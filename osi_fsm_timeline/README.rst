c1c6ba477806cb2eb2e6c8505b64ac6e3c521c0d
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-10-31 09:38:44 -0700

    [ADD]osi_fsm_timeline module has been added

---
+++ osi_fsm_timeline/README.rst
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+OSI Field Service Timeline
+==========================
+
+This module will support Field Services and Timeline view.
+
+Usage
+=====
+
+* Go to Field Services > Timeline view
+
+Credits
+=======
+
+* Sandip Mangukiya <smangukiya@opensourceintegrators.com>
+
+Contributors
+------------
+
+* Open Source Integrators <http://www.opensourceintegrators.com>
---
+++ osi_fsm_timeline/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OSI Field Service Timeline",
+    "summary": "Field Services and Timeline view support ",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Field Service",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["fieldservice", "web_timeline"],
+    "data": [
+        "views/field_timeline_view.xml",
+    ],
+    "installable": True,
+}
