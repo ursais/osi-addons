fc93b4a4cff32e46668c6277ec2f626fa842a3a6
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-11-05 22:49:52 -0800

    [IMP]12586: Code formatting pycharm

--- osi_fsm_timeline/models/person.py
+++ osi_fsm_timeline/models/person.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2012 - TODAY, Ursa Information Systems
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -11,13 +10,11 @@ class FSMPerson(models.Model):
     @api.multi
     def get_person_information(self, vals):
         # get person ids
-        person_ids = self.search([('id','!=',0),('active','=',True)])
+        person_ids = self.search([('id', '!=', 0), ('active', '=', True)])
         person_information_dict = []
         for person in person_ids:
             person_information_dict.append({
-                'id':person.id,
-                'name':person.name})
-        
+                'id': person.id,
+                'name': person.name})
+
         return person_information_dict
-
-
