af5a4c8e830e2b633b093f7bfd063f59759d0d6c
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-12-03 23:06:17 -0800

    [ADD]osi_website_slide_embeded module has been added to show download button on website slide

---
+++ osi_website_slide_embeded/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Slides improvement",
+    "summary": "Remove is_embedded condition from Download button visibility",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators",
+    "category": "Website",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["website_slides"],
+    "data": [
+        "views/website_slides_embed.xml",
+    ],
+    "installable": True,
+}
