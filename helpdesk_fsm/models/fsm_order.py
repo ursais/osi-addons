74f72da2a43574fa5e21e8e4cb5f01a051e320bc
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-07 09:43:42 -0600

    [FIX] module not found

--- helpdesk_fsm/models/fsm_order.py
+++ helpdesk_fsm/models/fsm_order.py
@@ -1,8 +1,16 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import logging
+
 from odoo import fields
-from odoo.addons.base_geoengine import geo_model
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from odoo.addons.base_geoengine import geo_model
+except ImportError:
+    _logger.info('base_geoengine module not found.')
 
 
 class FSMOrder(geo_model.GeoModel):
