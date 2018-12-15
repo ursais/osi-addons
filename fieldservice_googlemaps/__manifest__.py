fd56e63a96944c9a5d87b2c1cc40a9ed3e289cf0
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-14 18:01:04 -0600

    [IMP] Manifest

--- fieldservice_googlemaps/__manifest__.py
+++ fieldservice_googlemaps/__manifest__.py
@@ -2,21 +2,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Field Service Google Map Integration",
-    "version": "11.0.1.0.0",
-    "license": "AGPL-3",
-    "summary": "Auto-complete addresses with Google Maps",
-    "author": "Open Source Integrators",
-    "maintainer": "Open Source Integrators",
-    "website": "https://www.opensourceintegrators.com",
-    "depends": [
+    'name': 'Field Service - Google Maps',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'summary': 'Auto-complete addresses with Google Maps',
+    'author': 'Open Source Integrators',
+    'maintainer': 'Open Source Integrators',
+    'website': 'https://www.opensourceintegrators.com',
+    'depends': [
         'web_google_maps',
         'fieldservice',
     ],
-    "data": [
+    'data': [
         'views/fsm_location.xml',
         'views/fsm_order.xml',
     ],
-    "application": True,
-    "sequence": 50,
+    'development_status': 'Beta',
+    'maintainers': [
+        'osimallen',
+    ],
 }
