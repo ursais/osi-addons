9e9231fdee440c5d4e572539edebd6937509127c
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-20 12:42:16 -0600

    [FIX] helpdesk_stock

--- helpdesk_stock/__manifest__.py
+++ helpdesk_stock/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Helpdesk - Stock',
     'summary': 'Inventory and Stock Operations for Helpdesk Tickets',
     'version': '11.0.0.1.0',
     'category': 'Helpdesk',
-    'author': "Open Source Integrators",
+    'author': 'Open Source Integrators',
+    'website': 'https://github.com/ursais/osi-addons',
     'depends': [
         'helpdesk_timesheet',
         'stock',
@@ -17,7 +18,7 @@
         'security/ir.model.access.csv',
     ],
     'installable': True,
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'maintainers': [
         'osimallen',
         'wolfhall',
