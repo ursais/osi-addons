3d06413dfaa064905b83b2fd4450cf72bdc67e3b
osi-scampbell <scampbell@opensourceintegrators.com>
2018-12-20 11:57:53 -0700

    [ADD] Helpdesk_phone

---
+++ helpdesk_phone/README.rst
@@ -0,0 +1,28 @@
+.. image:: https://img.shields.io/badge/licence-LGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   :alt: License: LGPL-3
+
+==============
+Helpdesk Phone
+==============
+
+This module adds a phone number field on the helpdesk ticket related to the selected customer.  
+
+=====
+Usage
+=====
+
+* Create or edit a helpdesk ticket
+* Select a customer with a phone number
+* The phone number is displayed on the ticket
+
+=======
+Credits
+=======
+
+* Steven Campbell <scampbell@opensourceintegrators.com>
+
+Contributors
+------------
+
+* Open Source Integrators <contact@opensourceintegrators.com>
---
+++ helpdesk_phone/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Helpdesk Phone',
+    'summary': 'Display the customer phone number on Helpdesk ticket',
+    'version': '11.0.0.0.1',
+    'category': 'Helpdesk',
+    'author': "Open Source Integrators",
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'helpdesk',
+    ],
+    'data': [
+        'views/helpdesk_phone.xml'
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+        'max3903',
+    ],
+}
