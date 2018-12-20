fae985a1bcb87ac1e0a76b78e46123ef9c11d1ab
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-12-20 13:12:15 -0600

    [FIX] license

--- helpdesk_scope/models/__init__.py
+++ helpdesk_scope/models/__init__.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2018 - TODAY, Brian McMaster
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import (
     helpdesk_scope,
