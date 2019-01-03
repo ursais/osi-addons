b953f2710f458f162a99121cd304c1d808511b6d
osi-scampbell <scampbell@opensourceintegrators.com>
2019-01-03 14:12:24 -0700

    "Flake8 fix"

--- stock_production_lot_warranty/models/stock_production_lot.py
+++ stock_production_lot_warranty/models/stock_production_lot.py
@@ -4,7 +4,6 @@
 from odoo import fields, models, api
 from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
 
 class StockProductionLot(models.Model):
@@ -15,15 +14,23 @@ class StockProductionLot(models.Model):
     @api.onchange('product_id')
     def _onchange_product_id(self):
         if self.product_id:
-            if self.product_id.product_tmpl_id.warranty_type and self.product_id.product_tmpl_id.warranty:
+            if (self.product_id.product_tmpl_id.warranty_type and
+                    self.product_id.product_tmpl_id.warranty):
                 today_date = datetime.now()
                 if self.product_id.product_tmpl_id.warranty_type == 'day':
-                    time = (today_date + timedelta(days=self.product_id.product_tmpl_id.warranty))
+                    time = (today_date +
+                            timedelta(days=self.product_id.
+                                      product_tmpl_id.warranty))
                 elif self.product_id.product_tmpl_id.warranty_type == 'week':
-                    time = (today_date + timedelta(weeks=self.product_id.product_tmpl_id.warranty))
+                    time = (today_date +
+                            timedelta(weeks=self.product_id.
+                                      product_tmpl_id.warranty))
                 elif self.product_id.product_tmpl_id.warranty_type == 'month':
-                    time = (today_date + relativedelta(months=+self.product_id.product_tmpl_id.warranty))
+                    time = (today_date +
+                            relativedelta(months=+self.product_id.
+                                          product_tmpl_id.warranty))
                 elif self.product_id.product_tmpl_id.warranty_type == 'year':
-                    time = (today_date + relativedelta(years=+self.product_id.product_tmpl_id.warranty))
-
+                    time = (today_date +
+                            relativedelta(years=+self.product_id.
+                                          product_tmpl_id.warranty))
                 self.warranty_exp_date = time
