903e8ebe50f37763d9bcedb902c6bdd42f0488e3
osi-scampbell <scampbell@opensourceintegrators.com>
2019-01-08 15:04:21 -0700

    [ADD] agreement_sale_subscription

---
+++ agreement_sale_subscription/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    agreement,
+    sale_order,
+    subscription,
+)
