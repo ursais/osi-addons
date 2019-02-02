c893dd4a9cec845f46398c07bc56777502cb7d7e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-01 19:28:13 -0600

    [FIX] fieldservice_helpdesk_stock

--- fieldservice_helpdesk_stock/__manifest__.py
+++ fieldservice_helpdesk_stock/__manifest__.py
@@ -8,7 +8,7 @@
     'website': 'https://github.com/ursais/osi-addons',
     'category': 'Field Service',
     'license': 'LGPL-3',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'depends': [
         'fieldservice_stock',
         'helpdesk_fieldservice',
