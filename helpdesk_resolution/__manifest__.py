99a75200f1d6e79da2e3c6442ae93131c829698c
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-02-05 10:34:00 -0600

    [ADD] helpdesk_resolution

---
+++ helpdesk_resolution/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Helpdesk Resolution',
+    'summary': 'Keep the resolution of Helpdesk tickets',
+    'version': '11.0.0.0.1',
+    'category': 'Helpdesk',
+    'author': "Open Source Integrators",
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'helpdesk',
+    ],
+    'data': [
+        'views/helpdesk_ticket.xml'
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'max3903',
+    ],
+}
