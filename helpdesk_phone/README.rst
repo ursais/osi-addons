d467b1b495076370ed2b796ba534707b6afaf5b4
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-03-12 15:41:20 +0530

    [MIG][12.0] helpdesk_phone: Migrated.

--- helpdesk_phone/README.rst
+++ helpdesk_phone/README.rst
@@ -26,3 +26,4 @@ Contributors
 ------------
 
 * Open Source Integrators <contact@opensourceintegrators.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
--- helpdesk_phone/__manifest__.py
+++ helpdesk_phone/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Helpdesk Phone',
     'summary': 'Display the customer phone number on Helpdesk ticket',
-    'version': '11.0.0.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Helpdesk',
     'author': "Open Source Integrators",
     'website': 'https://github.com/ursais/osi-addons',
