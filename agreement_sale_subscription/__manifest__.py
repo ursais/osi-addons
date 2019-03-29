7c0127deecb413364d4f9e533487634d0586ca86
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-03-29 19:48:08 +0530

    [MIG][WIP][12.0] agreement_sale_subscription

--- agreement_sale_subscription/__manifest__.py
+++ agreement_sale_subscription/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Agreement - Sales Subscription',
     'summary': 'Link your sales subscriptions to an agreement',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'LGPL-3',
     'author': 'Open Source Integrators',
     'category': 'Agreement',
     'website': 'https://github.com/ursais/osi-addons',
     'depends': [
-        'agreement_sale',
+        'agreement_legal_sale',
         'sale_subscription'
     ],
     'data': [
