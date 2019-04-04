b468a8868ad1f24d95ce9313de32e4da59916f57
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-04 16:03:37 +0530

    [MIG][WIP][12.0] helpdesk_fieldservice

--- helpdesk_fieldservice/__manifest__.py
+++ helpdesk_fieldservice/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Helpdesk - Field Service',
     'summary': 'Create service requests from a ticket',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'LGPL-3',
     'author': 'Open Source Integrators',
     'category': 'Helpdesk',
--- helpdesk_fieldservice/models/fsm_order.py
+++ helpdesk_fieldservice/models/fsm_order.py
@@ -1,19 +1,14 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import logging
 
-from odoo import fields
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
-try:
-    from odoo.addons.base_geoengine import geo_model
-except ImportError:
-    _logger.info('base_geoengine module not found.')
 
-
-class FSMOrder(geo_model.GeoModel):
+class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
     ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket',
