bec8892e2663b230626fe33828dfb418697baf98
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-09 19:55:25 +0530

    [MIG][WIP][12.0] fieldservice_helpdesk_stock_v12

--- fieldservice_helpdesk_stock/models/__init__.py
+++ fieldservice_helpdesk_stock/models/__init__.py
@@ -1,9 +1,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
-    fsm_order,
-    fsm_order_line,
-    helpdesk_ticket,
-    helpdesk_ticket_line,
-    stock,
+    fsm_order
 )
--- fieldservice_helpdesk_stock/models/fsm_order.py
+++ fieldservice_helpdesk_stock/models/fsm_order.py
@@ -1,10 +1,10 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.base_geoengine import geo_model
+from odoo import api, models
 
 
-class FSMOrder(geo_model.GeoModel):
+class FSMOrder(models.Model):
     _inherit = 'fsm.order'
 
     def action_inventory_confirm(self):
@@ -13,24 +13,11 @@ class FSMOrder(geo_model.GeoModel):
             order.procurement_group_id.helpdesk_ticket_id = order.ticket_id
         return res
 
+    @api.multi
     def write(self, vals):
         res = super(FSMOrder, self).write(vals)
         if 'ticket_id' in vals:
-            for line in self.line_ids:
-                if line.state in ('draft', 'requested', 'cancelled'):
-                    if line.helpdesk_ticket_line_id:
-                        line.helpdesk_ticket_line_id.ticket_id = \
-                            vals['ticket_id']
-                    else:
-                        line.helpdesk_ticket_line_id = \
-                            self.env['helpdesk.ticket.line'].create({
-                                'product_id': line.product_id.id,
-                                'name': line.name,
-                                'qty_requested': line.qty_requested,
-                                'qty_ordered': line.qty_ordered,
-                                'product_uom_id': line.product_uom_id.id,
-                                'ticket_id': vals['ticket_id'],
-                                'fsm_order_line_id': line.id,
-                                'state': line.state,
-                            })
+            for line in self.stock_request_ids:
+                if line.state in ('draft', 'cancelled'):
+                    line.ticket_id = vals.get('ticket_id')
         return res
