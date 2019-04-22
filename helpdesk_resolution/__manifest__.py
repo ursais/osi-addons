98c5833b8fabc97dac2c02a40a4a5bc224a3be94
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-22 19:50:35 +0530

    [MIG][12.0] helpdesk_resolution

--- helpdesk_resolution/__manifest__.py
+++ helpdesk_resolution/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Helpdesk Resolution',
     'summary': 'Keep the resolution of Helpdesk tickets',
-    'version': '11.0.0.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Helpdesk',
     'author': "Open Source Integrators",
     'website': 'https://github.com/ursais/osi-addons',
