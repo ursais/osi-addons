6269491f4322c05f54e48e1a67c5c3ada5902e04
scampbell <scampbell@opensourceintegrators.com>
2019-06-04 11:06:48 -0700

    [IMP] Github comments

--- helpdesk_fieldservice/models/helpdesk_ticket.py
+++ helpdesk_fieldservice/models/helpdesk_ticket.py
@@ -72,9 +72,9 @@ class HelpdeskTicket(models.Model):
             'default_ticket_id': self.id,
             'default_priority': self.priority,
             'default_location_id': self.fsm_location_id.id,
-            'default_customer_id': self.partner_id.id
+            'default_customer_id': self.partner_id.id,
+            'default_origin': self.name
         }
         res = self.env.ref('fieldservice.fsm_order_form', False)
         result['views'] = [(res and res.id or False, 'form')]
-        result['context']['default_origin'] = self.name
         return result
