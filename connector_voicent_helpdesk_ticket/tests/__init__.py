b5e917f04a2684997a7d9ca8b0008ab5ecad13bd
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-06-19 11:58:28 -0500

    [FIX] connector_voicent_helpdesk_ticket

--- connector_voicent_helpdesk_ticket/tests/__init__.py
+++ connector_voicent_helpdesk_ticket/tests/__init__.py
@@ -1,3 +1,5 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2019 Open Source Integrators
+# <https://www.opensourceintegrators.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import test_helpdesk_ticket
