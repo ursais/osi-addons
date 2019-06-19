c528f0b1d04891b1a0b2eb50e92ef30a37f4b30d
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-06-19 11:58:28 -0500

    [FIX] connector_voicent_helpdesk_ticket

--- connector_voicent_helpdesk_ticket/models/helpdesk_ticket.py
+++ connector_voicent_helpdesk_ticket/models/helpdesk_ticket.py
@@ -1,9 +1,12 @@
+# Copyright (C) 2019 Open Source Integrators
+# <https://www.opensourceintegrators.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 import io
 import base64
 from odoo import api, models, _
 from ...queue_job.job import job
 from odoo.tools import pycompat
-from datetime import datetime, timedelta
 from ...connector_voicent.examples import voicent
 import tempfile
 import shutil
@@ -14,79 +17,76 @@ class HelpdeskTicket(models.Model):
 
     @job
     @api.multi
-    def check_status_job(self):
+    def check_status_job(self, voicent_campaign, helpdesk_ticket,
+                         call_line):
         for rec in self:
-            queue_job = self.env['queue.job'].search(
-                [('uuid', '=', self._context.get('job_uuid'))])
             voicent_obj = voicent.Voicent(
-                queue_job.call_line_id.backend_id.host,
-                str(queue_job.call_line_id.backend_id.port))
-            status = voicent_obj.checkStatus(queue_job.voicent_campaign)
-            queue_job.helpdesk_ticket_id.message_post(
-                body=_('Status Call on <b>%s</b>: <b>%s</b>' %
-                       (queue_job.call_line_id.backend_id.port,
-                        status)))
-            if status != 'OK':
-                next_time = datetime.now() + timedelta(minutes=15)
-                rec.with_delay(eta=next_time).check_status_job()
-            if queue_job.state == 'failed':
-                message = _("""Job has been failed, Please check the:
-                    <a href=# data-oe-model=queue.job data-oe-id=%d>%s</a> job
-                            and fix the issue and re-execute the job"""
-                            ) % (queue_job.id, queue_job.name)
-                queue_job.helpdesk_ticket_id.message_post(body=message)
+                call_line.backend_id.host,
+                str(call_line.backend_id.port),
+                call_line.backend_id.callerid,
+                str(call_line.backend_id.line))
+            status = voicent_obj.checkStatus(voicent_campaign)
+            message = _("""Status of campaign <b>%s</b> on <b>%s</b>:
+             %s""" % (voicent_campaign,
+                      call_line.backend_id.name,
+                      status))
+            helpdesk_ticket.message_post(body=message)
 
     @job
     @api.multi
-    def voicent_import_and_runcampaign(self):
+    def voicent_import_and_runcampaign(self, helpdesk_ticket, call_line):
         for rec in self:
-            queue_job = self.env['queue.job'].search(
-                [('uuid', '=', self._context.get('job_uuid'))])
-
-            if queue_job.call_line_id.helpdesk_ticket_stage_id.id \
-                    == queue_job.helpdesk_ticket_id.stage_id.id:
+            if call_line.helpdesk_ticket_stage_id.id \
+                    == helpdesk_ticket.stage_id.id:
+                # Generate the CSV file
                 fp = io.BytesIO()
                 writer = pycompat.csv_writer(fp, quoting=1)
-                writer.writerow(("name", "phone"))
+                writer.writerow(("Name", "Phone"))
                 writer.writerow(
-                    [queue_job.helpdesk_ticket_id.partner_id.name,
-                     queue_job.helpdesk_ticket_id.partner_id.phone])
-                self.env['ir.attachment'].create({
-                    'name': queue_job.name,
-                    'datas': base64.encodestring(fp.getvalue()),
-                    'datas_fname': "%s.csv" % (queue_job.name),
-                    'res_model': 'queue.job',
-                    'res_id': queue_job.id})
-                directory_name = tempfile.mkdtemp(suffix='tempimagenew')
-                file_name = directory_name + "/" + queue_job.name + ".csv"
+                    [helpdesk_ticket.partner_id.name,
+                     helpdesk_ticket.partner_id.phone])
+                directory = tempfile.mkdtemp(suffix='-helpdesk.ticket')
+                file_name = directory + "/" + rec.name + rec.stage_id.name +\
+                            ".csv"
                 write_file = open(file_name, 'wb')
                 write_file.write(fp.getvalue())
-                voicent_obj = voicent.Voicent(
-                    queue_job.call_line_id.backend_id.host,
-                    str(queue_job.call_line_id.backend_id.port))
-                imp_run_camp = voicent_obj.importAndRunCampaign(
-                    file_name, "tts",
-                    queue_job.call_line_id.voicent_app)
                 write_file.close()
-                shutil.rmtree(directory_name)
-                queue_job.helpdesk_ticket_id.message_post(
-                    body=_('Call has been sent to <b>%s</b>' %
-                           (queue_job.call_line_id.backend_id.host)))
-                new_job = rec.with_delay().check_status_job()
-                db_job = new_job.db_record()
-                if db_job:
-                    db_job.write(
-                        {'voicent_campaign': imp_run_camp.get('camp_id'),
-                         'helpdesk_ticket_id': queue_job.helpdesk_ticket_id.id,
-                         'call_line_id': queue_job.call_line_id.id})
+                # Contact Voicent
+                voicent_obj = voicent.Voicent(
+                    call_line.backend_id.host,
+                    str(call_line.backend_id.port),
+                    call_line.backend_id.callerid,
+                    str(call_line.backend_id.line))
+                res = voicent_obj.importAndRunCampaign(file_name, "tts",
+                                                       call_line.voicent_app)
+                # Attach the file to the ticket
+                self.env['ir.attachment'].create({
+                    'name': rec.name,
+                    'datas': base64.encodestring(fp.getvalue()),
+                    'datas_fname': "%s.csv" % (rec.name),
+                    'res_model': 'helpdesk.ticket',
+                    'res_id': rec.id})
+                # Delete the file on the filesystem
+                shutil.rmtree(dir)
+                if res.get('camp_id'):
+                    message = _("""Call has been sent to <b>%s</b>.
+                    The campaign ID is <b>%s</b> and the status is: %s""" %
+                                (call_line.backend_id.name,
+                                 res.get('camp_id'),
+                                 res.get('status')))
+                    rec.with_delay().check_status_job(
+                        res.get('camp_id'),
+                        helpdesk_ticket,
+                        call_line)
+                else:
+                    message = _("""Call has been sent to <b>%s</b> but failed
+                     with the following message: %s""" %
+                                (call_line.backend_id.name,
+                                 res))
             else:
-                queue_job.state = 'done'
-            if queue_job.state == 'failed':
-                message = _("""Job has been failed, Please check the:
-                    <a href=# data-oe-model=queue.job data-oe-id=%d>%s</a> job
-                            and fix the issue and re-execute the job."""
-                            ) % (queue_job.id, queue_job.name)
-                queue_job.helpdesk_ticket_id.message_post(body=message)
+                message = _("Call has been cancelled because the stage has "
+                            "changed.")
+            helpdesk_ticket.message_post(body=message)
 
     @api.multi
     def write(self, vals):
@@ -96,13 +96,11 @@ class HelpdeskTicket(models.Model):
                 call_lines = self.env['backend.voicent.call.line'].search(
                     [('helpdesk_ticket_stage_id', '=',
                       vals.get('stage_id')),
-                     ('backend_id.is_active', '=', True)])
+                     ('backend_id.active', '=', True)])
                 for line_rec in call_lines:
-                    q_job = rec.with_delay(
-                        eta=line_rec.backend_id.next_call). \
-                        voicent_import_and_runcampaign()
-                    db_job = q_job.db_record()
-                    if db_job:
-                        db_job.write({'helpdesk_ticket_id': rec.id,
-                                      'call_line_id': line_rec.id})
+                    if not (line_rec.has_parent is True and
+                            rec.parent_id is False):
+                        rec.with_delay(
+                            eta=line_rec.backend_id.next_call). \
+                            voicent_import_and_runcampaign(rec, line_rec)
         return super(HelpdeskTicket, self).write(vals)
