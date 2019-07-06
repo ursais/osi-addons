7139d6e714ce7783a5f0dd30f9136348325d51c5
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-05 21:22:46 -0500

    [FIX] fieldservice_helpdesk_stock: Add onchange_location_id()

--- fieldservice_helpdesk_stock/models/stock_request.py
+++ fieldservice_helpdesk_stock/models/stock_request.py
@@ -19,3 +19,17 @@ class StockRequest(models.Model):
         else:
             res = super().create(vals)
         return res
+
+    @api.onchange('direction', 'fsm_order_id', 'helpdesk_ticket_id')
+    def _onchange_location_id(self):
+        super()._onchange_location_id()
+        # FSM Order takes priority over Helpdesk Ticket
+        if self.fsm_order_id:
+            if self.direction == 'outbound':
+                # Inventory location of the FSM location of the order
+                self.location_id = \
+                    self.fsm_order_id.location_id.inventory_location_id.id
+            else:
+                # Otherwise the stock location of the warehouse
+                self.location_id = \
+                    self.fsm_order_id.warehouse_id.lot_stock_id.id
