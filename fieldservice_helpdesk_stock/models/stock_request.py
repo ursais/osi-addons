6439cf2434ba8c80f3a6224783c05bc577148ad6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-08 22:43:32 -0500

    [FIX] fieldservice_helpdesk_stock: Set the partner on the picking

--- fieldservice_helpdesk_stock/models/stock_request.py
+++ fieldservice_helpdesk_stock/models/stock_request.py
@@ -49,11 +49,18 @@ class StockRequest(models.Model):
 
     def _prepare_procurement_values(self, group_id=False):
         res = super()._prepare_procurement_values(group_id=group_id)
-        res.update({
-            'fsm_order_id': self.fsm_order_id.id,
-            'helpdesk_ticket_id': self.fsm_order_id.ticket_id.id or False,
-            'partner_id': self.fsm_order_id.location_id.partner_id.id,
-        })
+        if self.helpdesk_ticket_id:
+            res.update({
+                'helpdesk_ticket_id': self.helpdesk_ticket_id.id,
+                'partner_id':
+                    self.helpdesk_ticket_id.fsm_location_id.partner_id.id,
+            })
+        if self.fsm_order_id:
+            res.update({
+                'fsm_order_id': self.fsm_order_id.id,
+                'helpdesk_ticket_id': self.fsm_order_id.ticket_id.id or False,
+                'partner_id': self.fsm_order_id.location_id.partner_id.id,
+            })
         return res
 
     def _prepare_procurement_group_values(self):
