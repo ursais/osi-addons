d93b128204bf2f4eaa8fc0364a805d1edacf1b93
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-07-09 14:47:54 +0530

    [IMP] osi_vendor_reference

--- osi_vendor_reference/__manifest__.py
+++ osi_vendor_reference/__manifest__.py
@@ -1,23 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2012 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'OSI Vendor Reference',
     'version': '12.0.1.0.0',
     'author': 'Open Source Integrators',
-    'summary': 
-            '''
-             When vendor bill is created, copy the vendor reference info from the PO into the bill.
-             If the bill already has a reference, append to existing reference using ; [Bill can be for multiple PO's]
-            ''',
+    'summary': '''When vendor bill is created, copy the vendor reference info
+    from the PO into the bill. If the bill already has a reference, append
+    to existing reference using ; [Bill can be for multiple PO's]''',
+    'license': 'LGPL-3',
     'category': 'Customers',
     'maintainer': 'Open Source Integrators',
     'website': 'http://www.opensourceintegrators.com',
-    'depends': ['purchase', 'account'],
-    'data': [ 
-    ],
-    'qweb': [        
-    ],
+    'depends': ['purchase'],
     'installable': True,
 }
