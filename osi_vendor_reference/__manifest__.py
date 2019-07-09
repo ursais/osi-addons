b95d60f5a66a203f7f38f33cc71682d68b742849
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-09 12:44:00 -0500

    [ADD] osi_vendor_reference (#127)

---
+++ osi_vendor_reference/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'OSI Vendor Reference',
+    'version': '12.0.1.0.0',
+    'author': 'Open Source Integrators',
+    'summary': '''When vendor bill is created, copy the vendor reference info
+    from the PO into the bill. If the bill already has a reference, append
+    to existing reference using ; [Bill can be for multiple PO's]''',
+    'license': 'LGPL-3',
+    'category': 'Customers',
+    'maintainer': 'Open Source Integrators',
+    'website': 'http://www.opensourceintegrators.com',
+    'depends': ['purchase'],
+    'installable': True,
+}
