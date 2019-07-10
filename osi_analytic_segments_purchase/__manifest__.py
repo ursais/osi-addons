c8be46c380b4eb24aaae0c631f23b7a211f9c4bb
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-10 10:09:05 -0500

    [ADD] osi_analytic_segments_purchase (#124)

---
+++ osi_analytic_segments_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'OSI Analytic Segments Purchase',
+    'version': '12.0.1.0.0',
+    'license': 'LGPL-3',
+    'summary': 'Additional analytic segments in Purchase',
+    'author': 'Open Source Integrators',
+    'maintainer': 'Open Source Integrators',
+    'website': 'http://www.opensourceintegrators.com',
+    'category': 'Analytic Accounting',
+    'depends': [
+        'purchase',
+        'osi_analytic_segments',
+    ],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+}
