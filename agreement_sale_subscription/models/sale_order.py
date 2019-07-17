dfb611e33e5d3387027e76edb93f345e4653fd68
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-17 17:13:13 -0500

    [FIX] agreement_sale_subscription: Expected singleton: agreement()

--- agreement_sale_subscription/models/sale_order.py
+++ agreement_sale_subscription/models/sale_order.py
@@ -10,8 +10,9 @@ class SaleOrder(models.Model):
     @api.multi
     def create_subscriptions(self):
         res = super(SaleOrder, self).create_subscriptions()
-        for line in self.order_line:
-            if line.subscription_id:
-                self.agreement_id.subscription_id = line.subscription_id
-                line.subscription_id.agreement_id = self.agreement_id
+        for order in self:
+            for line in order.order_line:
+                if line.subscription_id:
+                    order.agreement_id.subscription_id = line.subscription_id
+                    line.subscription_id.agreement_id = order.agreement_id
         return res
