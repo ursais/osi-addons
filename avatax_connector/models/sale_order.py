812932ef390be94964c56f4363ef00e9c22d8cdc
Daniel Reis <dgreis@sapo.pt>
2019-07-18 16:25:34 +0100

    Add ship-from warehouse to Sale Order

--- avatax_connector/models/sale_order.py
+++ avatax_connector/models/sale_order.py
@@ -62,6 +62,7 @@ class SaleOrder(models.Model):
             'exemption_code': self.exemption_code or '',
             'exemption_code_id': self.exemption_code_id.id or False,
             'location_code': self.location_code or '',
+            'warehouse_id': self.warehouse_id.id or '',
             'tax_on_shipping_address': self.tax_add_shipping,
         })
         return invoice_vals
@@ -127,6 +128,7 @@ class SaleOrder(models.Model):
                                  compute='_compute_tax_id', store=True)
     tax_address = fields.Text('Tax Address Text')
     location_code = fields.Char('Location Code', help='Origin address location code')
+    warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse')
 
     @api.model
     def create_lines(self, order_lines):
@@ -150,7 +152,7 @@ class SaleOrder(models.Model):
     def compute_tax(self):
         """ Create and update tax amount for each and every order line and shipping line.
         @param order_line: send sub_total of each line and get tax amount
-        @param shiiping_line: send shipping amount of each ship line and get ship tax amount
+        @param shiping_line: send shipping amount of each ship line and get ship tax amount
         """
         if self.env.context.get('doing_compute_tax'):
             return False
@@ -172,14 +174,10 @@ class SaleOrder(models.Model):
         tax_amount = o_tax_amt = 0.0
 
         # ship from Address / Origin Address either warehouse or company if none
-        # warehouse_id might not be available if the Inventory app is not installed
-        if hasattr(self, 'warehouse_id') and self.warehouse_id.partner_id:
-            ship_from_address_id = self.warehouse_id.partner_id
-        else:
-            ship_from_address_id = self.company_id.partner_id
+        ship_from_address_id = self.warehouse_id.partner_id or self.company_id.partner_id
 
-        compute_taxes = self.env.context.get('avatax_recomputation') or avatax_config.enable_immediate_calculation
-        print(compute_taxes)
+        compute_taxes = (self.env.context.get('avatax_recomputation') or
+                         avatax_config.enable_immediate_calculation)
         if compute_taxes:
             ava_tax = account_tax_obj.search(
                 [('is_avatax', '=', True),
@@ -239,8 +237,9 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm(self):
+        res =  super(SaleOrder, self).action_confirm()
         self.with_context(avatax_recomputation=True).compute_tax()
-        return super(SaleOrder, self).action_confirm()
+        return res
 
 
 class SaleOrderLine(models.Model):
