e5657c62829f567066cec4543b0fd1ca78018106
scampbell <scampbell@opensourceintegrators.com>
2019-07-20 15:05:31 -0700

    [ADD] Sale Subscription Brand

---
+++ sale_subscription_brand/models/account_invoice.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountInvoice(models.Model):
+        _inherit = 'account.invoice'
+        
