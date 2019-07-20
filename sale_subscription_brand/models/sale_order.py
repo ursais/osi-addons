1ce53f79010ca7f051528d57d88f553eaec6e7ac
scampbell <scampbell@opensourceintegrators.com>
2019-07-20 15:05:31 -0700

    [ADD] Sale Subscription Brand

---
+++ sale_subscription_brand/models/sale_order.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def create_subscriptions(self):
+        res = super(SaleOrder, self).create_subscriptions()
+        for sub_id in res:
+            subscription = self.env['sale.subscription'].search([('id', '=', sub_id)])
+            subscription.brand = self.brand
+
