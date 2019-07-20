8a2d065e823a6f4af692cd2bd786db70315cf099
scampbell <scampbell@opensourceintegrators.com>
2019-07-20 15:05:31 -0700

    [ADD] sale_subscription_brand

---
+++ sale_subscription_brand/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2019 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Sale Subscription Brand",
+    "summary": "Brand your subscription invoices",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://github.com/ursais/osi-addons",
+    "author": "Open Source Integrators",
+    "license": "LGPL-3",
+    "depends": [ 
+        'sale_subscription',
+        'sale_brand',
+    ],
+    "data": [
+        "views/sale_subscription_views.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["osi-scampbell"],
+}
---
+++ sale_subscription_brand/models/sale_subscription.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2019 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SaleSubscription(models.Model):
+    _inherit = 'sale.subscription'
+
+    brand_id = fields.Many2one('res.partner', string='Brand',
+                               domain="[('type', '=', 'brand')]")
+
+    def _prepare_invoice_data(self):
+        res = super()._prepare_invoice_data()
+        if self.brand_id:
+            res.update({'brand_id': self.brand_id.id})
+        return res
