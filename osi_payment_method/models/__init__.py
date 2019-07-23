a3aea60f058ffbf0f9c0ef127bdbc22f220aa453
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-23 12:32:35 -0500

    [FIX] osi_payment_method: Use account.payment.method

--- osi_payment_method/models/__init__.py
+++ osi_payment_method/models/__init__.py
@@ -1,4 +1,9 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import custom_payment_method, res_partner,\
-    sale, purchase, account_invoice
+from . import (
+    res_partner,
+    sale,
+    purchase,
+    account_invoice,
+)
--- osi_payment_method/models/purchase.py
+++ osi_payment_method/models/purchase.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
@@ -7,18 +7,20 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    payment_method = fields.Many2one("custom.payment.method",
+    payment_method = fields.Many2one("account.payment.method",
                                      string="Payment Method")
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
+        res = super()._change_partner_id()
         self.payment_method = self.partner_id.payment_method
+        return res
 
     @api.multi
     def action_view_invoice(self):
-        result = super(PurchaseOrder, self).action_view_invoice()
-        result['context']['default_payment_method'] = self.payment_method.id
-        return result
+        res = super().action_view_invoice()
+        res['context']['default_payment_method'] = self.payment_method.id
+        return res
 
 
 class StockRule(models.Model):
@@ -27,7 +29,7 @@ class StockRule(models.Model):
     @api.multi
     def _prepare_purchase_order(self, product_id, product_qty, product_uom,
                                 origin, values, partner):
-        res = super(StockRule, self)._prepare_purchase_order(
+        res = super()._prepare_purchase_order(
             product_id, product_qty, product_uom, origin, values, partner)
         if res and 'payment_method' not in res:
             res.update({'payment_method': partner.payment_method and
