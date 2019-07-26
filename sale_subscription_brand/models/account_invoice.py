78a980011306f62f2467e5eb172721347bfcbbd9
scampbell <scampbell@opensourceintegrators.com>
2019-07-26 08:53:54 -0700

    [IMP] Flake8

--- sale_subscription_brand/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
-
-
-class AccountInvoice(models.Model):
-        _inherit = 'account.invoice'
-        
