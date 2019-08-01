9885e5780a4a99a8f729a6065ac631013d68d832
Jacob Barnard <jbarnard@amplex.net>
2019-08-01 15:09:27 -0400

    WIP Stashing

--- sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
+++ sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
@@ -7,94 +7,69 @@ from odoo.tools.translate import _
 
 
 class checkServiceSuspensionState(models.Model):
-    _inherit = 'ir.cron'
+    _inherit = 'sale.subscription'
+    # inherit = 'ir.cron'
+
+    passed_partner = fields.Char()
+
+    passed_partner_id = fields.Integer()
+
+    partner_subscriptions = fields.Many2one(
+        'sale.subscription',
+        domain=[
+            ('partner_id', '=', passed_partner)
+        ]
+    )
+
+    subscriptions = fields.Many2one(
+        'sale.subscription',
+        domain=[
+            '|',
+            ('stage_id', '=', 'In Progress'),
+            ('stage_id', '=', 'Suspended')
+        ]
+    )
+
+    subscription_partner = fields.Many2one(
+        'res.partner',
+        domain=[
+            ('id', '=', passed_partner_id)
+        ]
+    )
 
     @api.multi
     def _check_service_suspensions(self, partner=None):
         try:
-            # do individual match if a specific partner record is passed
+            # if a specific partner record is passed, do individual match
             if partner:
-                # do this like a many2one or use the domain in a search()?
-                # domain = [('id', '=', 'partner')]
-                # partner = res.partner.search(domain)
-                partner_subscriptions = fields.Many2one(
-                    'sale.subscription',
-                    domain=[
-                        ('partner_id', '=', partner)
-                    ]
-                )
-                for service in partner_subscriptions:
-                    # brian's extension should allow me to do this
-                    service.action_re_activate()
-                    # service.write({'stage_id': 'In Progress'})
-            # default to processing all partner records
+                self.passed_partner = partner
+                for service in self.partner_subscriptions:
+                    # need to call Brian's function as a super
+                    super(checkServiceSuspensionState, self).service.action_re_activate()
+            # else default to processing all partner records
             else:
                 # gather all valid, active subscriptions
-                subscriptions = fields.Many2one(
-                    'sale.subscription',
-                    domain=[
-                        '|',
-                        ('stage_id', '=', 'In Progress'),
-                        ('stage_id', '=', 'Suspended')
-                    ]
-                )
-                for service in subscriptions:
-                    # do this like a many2one or use the domain in a search()?
-                    # get the partner record for each of the services
-                    # domain = [('id', '=', 'service.partner_id')]
-                    # partner = res.partner.search(domain)
-                    # ...or...
-                    partner = fields.Many2one(
-                        'res.partner',
-                        domain=[
-                            ('id', '=', service.partner_id)
-                        ]
-                    )
-
-                    # compute their credit and credit limit by credit type
-                    # Can I just directly pull partner.credit_limit_type
-                    # instead of using a related?
-                    partner_credit_type = fields.Selection(
-                        related='partner.credit_limit_type'
-                    )
-                    partner_credit_limit = fields.Float(
-                        related='partner.credit_limit'
-                    )
-                    partner_credit = fields.Monetary(
-                        related='partner.credit'
-                    )
-
-                    # compute their credit and credit limit by credit type
-                    # ^^ how to compute their credit?  Var exists but not visbl
+                for service in self.subscriptions:
+                    # compute their credit and credit limit by credit type &
                     # compare the credit with their limit
-                    if partner_credit_type == 'fixed':
-                        # get the age of the oldest open invoice - how to view list
-
+                    if self.partner.credit_limit_type == 'fixed':
+                        self.passed_partner_id = service.partner_id
                         # if credit > credit limit or age > overdue limit then
-                        # move all the subscriptions of the customer to "Suspended"
-                        if partner_credit > partner_credit_limit:
-                            service.action_suspend()
-                            # service.write({'stage_id': 'Suspended'})
-                    elif partner_credit_type == 'subscription_based':
-                        # get the age of the oldest open invoice - how to view list
-                        partner_credit_limit_subscription_qty = fields.Int(
-                            related='partner.credit_limit_subscription_qty'
-                        )
-                        partner_credit_limit_subscription_uom = fields.Selection(
-                            related='partner.credit_limit_subscription_uom'
-                        )
-                        # if credit > credit limit or age > overdue limit then
-                        # move all the subscriptions of the customer to "Suspended"
-                        if ...:
-                            service.action_suspend()
-                            # service.write({'stage_id': 'Suspended'})
-
-                    # will need to be able to control exceptions
-                    # res.partner needs a field that prevents suspension
-                    # for eligible services (e.g. NWWSD, Luckey Farmers, etc)
-                    # suspend/reactivate service as the case demands
-                    # service.action_re_activate()
-                    # service.write({'stage_id': 'In Progress'})
+                        # suspend all partner subscriptions
+                        if self.partner.credit > self.partner.credit_limit:
+                            super(checkServiceSuspensionState, self).service.action_suspend()
+                        else:
+                            super(checkServiceSuspensionState, self).service.action_re_activate()
+                    elif self.partner.credit_limit_type == 'subscription_based':
+                        # get the age of the oldest open invoice
+                        self.partner.credit_limit_subscription_qty
+                        self.partner.credit_limit_subscription_uom
+                        if self.age > self.overdue_limit:
+                            # Suspend all partner subscriptions
+                            super(checkServiceSuspensionState, self).service.action_suspend()
+                        else:
+                            super(checkServiceSuspensionState, self).service.action_re_activate()
+                    # suspend or reactivate service as the case demands
         except RuntimeError as error:
             msg = _('Error Encountered:\n {} \n {}'.format(error, error.args))
             raise UserError(msg)
