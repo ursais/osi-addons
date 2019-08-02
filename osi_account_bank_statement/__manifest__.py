6cfebdc30be5a1aa0eb36d27c29d197d09f1c8ee
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-02 13:54:36 -0500

    [FIX] osi_account_bank_statement: AttributeError: 'account.journal' object has no attribute 'liability_account_id'

--- osi_account_bank_statement/__manifest__.py
+++ osi_account_bank_statement/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2019, Open Source Integrators
+# Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Account Bank Reconciliation Extended',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'LGPL-3',
     'author': 'Open Source Integrators',
     'summary': 'Bank Reconciliation Entries restricted to specific accounts',
@@ -10,7 +10,9 @@
     'maintainer': 'Open Source Integrators',
     'website': 'http://www.opensourceintegrators.com',
     'depends': [
-                'account_payment_credit_card',
+        'account_payment_credit_card',
     ],
     'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['mgosai'],
 }
