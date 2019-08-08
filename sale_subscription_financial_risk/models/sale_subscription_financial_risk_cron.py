a0d3fdd587ec7dc9f3682a88a415d0745f5b8ca2
Jacob Barnard <jbarnard@amplex.net>
2019-08-08 11:18:47 -0400

    So far so good...throwing it at the wall

--- sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
+++ sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
@@ -7,26 +7,6 @@ from odoo.tools.translate import _
 class SaleSubscription(models.Model):
     _inherit = 'sale.subscription'
 
-    # passed_partner = fields.Char()
-
-    # passed_partner_id = fields.Integer()
-
-    # subscriptions = fields.Many2one(
-    #     'sale.subscription',
-    #     domain=[
-    #         '|',
-    #         ('stage_id', '=', 'In Progress'),
-    #         ('stage_id', '=', 'Suspended')
-    #     ]
-    # )
-
-    # subscription_partner = fields.Many2one(
-    #     'res.partner',
-    #     domain=[
-    #         ('id', '=', passed_partner_id)
-    #     ]
-    # )
-
     @api.model
     def check_service_suspensions(self, partner=None):
         try:
@@ -44,25 +24,72 @@ class SaleSubscription(models.Model):
                     super(
                         SaleSubscription, self
                     ).service.action_re_activate()
-        #     # else default to processing all partner records
-        #     else:
-        #         # gather all valid, active subscriptions
-        #         for service in self.subscriptions:
-        #             # compute their credit and credit limit by credit type &
-        #             # compare the credit with their limit
-        #             if self.partner.credit_limit_type == 'fixed':
-        #                 self.passed_partner_id = service.partner_id
-        #                 # if credit > credit limit or age > overdue limit then
-        #                 # suspend all partner subscriptions
-        #                 if self.partner.credit > self.partner.credit_limit:
-        #                     super(
-        #                         SaleSubscription, self
-        #                     ).service.action_suspend()
-        #                 else:
-        #                     super(
-        #                         SaleSubscription, self
-        #                     ).service.action_re_activate()
-        #  ~~~ Below is definitely WIP ~~~ #
+            # else default to processing all partner records
+
+            # EVERYTHING BELOW NEEDS COMPLETELY REVIEW AND HASHED OUT
+
+            else:
+                # gather all valid, active subscriptions
+                subscriptions = fields.Many2one(
+                    'sale.subscription',
+                    domain=[
+                        '|',
+                        ('stage_id', '=', 'In Progress'),
+                        ('stage_id', '=', 'Suspended')
+                    ]
+                )
+                for service in subscriptions:
+                    # compute their credit and credit limit by credit type &
+                    # compare the credit with their limit
+                    partner = fields.Many2one(
+                        'res.partner',
+                        domain=[
+                            ('id', '=', service.partner_id)
+                        ]
+                    )
+
+                    partner_credit_type = fields.Selection(
+                        related='partner.credit_limit_type'
+                    )
+                    partner_credit_limit = fields.Float(
+                        related='partner.credit_limit'
+                    )
+                    partner_credit = fields.Monetary(
+                        related='partner.credit'
+                    )
+
+                    if partner_credit_type == 'fixed':
+                        self.passed_partner_id = service.partner_id
+                        # if credit > credit limit or age > overdue limit then
+                        # suspend all partner subscriptions
+                        if partner_credit > partner_credit_limit:
+                            super(
+                                SaleSubscription, self
+                            ).service.action_suspend()
+                        else:
+                            super(
+                                SaleSubscription, self
+                            ).service.action_re_activate()
+                    elif partner_credit_type == 'subscription_based':
+                        # get the age of the oldest open invoice
+                        # partner_credit_limit_subscription_qty = fields.Int(
+                        #     related='partner.credit_limit_subscription_qty'
+                        # )
+                        # partner_credit_limit_subscription_uom = fields.Selection(
+                        #     related='partner.credit_limit_subscription_uom'
+                        # )
+                        # if age > overdue limit then
+                        # suspend all partner subscriptions
+                        if self.age > self.overdue_limit:
+                            super(
+                                SaleSubscription, self
+                            ).service.action_suspend()
+                        else:
+                            super(
+                                SaleSubscription, self
+                            ).service.action_re_activate()
+                        # suspend or reactivate service as the case demands
+        #  ~~~ Below is definitely WIP backup ~~~ #
         # elif self.partner.credit_limit_type == 'subscription_based':
         #     calculate the age of the oldest open invoice
         #     self.partner.credit_limit_subscription_qty
