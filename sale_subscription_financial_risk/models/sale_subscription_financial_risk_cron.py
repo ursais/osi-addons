2cf8ff212864a5165a5930568815076330b357ce
Jacob Barnard <jbarnard@amplex.net>
2019-08-12 09:16:28 -0400

    Playing around

--- sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
+++ sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
@@ -2,6 +2,9 @@ from odoo import models, fields, api
 from datetime import date
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleSubscription(models.Model):
@@ -16,12 +19,45 @@ class SaleSubscription(models.Model):
         ]
     )
 
+    # These functions are in agreement_sale_subscription_suspension
+    def suspend_service_profile(self):
+        sp_ids = self.env['agreement.serviceprofile'].search([
+            ('agreement_id', '=', self.agreement_id.id)])
+        if sp_ids:
+            sp_ids.write({'state': 'suspended'})
+
+    def activate_service_profile(self):
+        sp_ids = self.env['agreement.serviceprofile'].search([
+            ('agreement_id', '=', self.agreement_id.id)])
+        if sp_ids:
+            sp_ids.write({'state': 'active'})
+
+    def action_suspend(self):
+        self.suspend_service_profile()
+        return super().action_suspend()
+
+    def action_re_activate(self):
+        self.activate_service_profile()
+        return super().action_re_activate()
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'stage_id' in vals:
+            stage_id = self.env['sale.subscription.stage'].search(
+                [('id', '=', vals['stage_id'])])
+            if stage_id.name == 'Suspended':
+                self.suspend_service_profile()
+            elif stage_id.name == "In Progress":
+                self.activate_service_profile()
+        return res
+
     @api.model
     def check_service_suspensions(self, partner=None):
+        # accepts opitonal passed partner_id to match subscriptions on
         try:
-            # if a specific partner record is passed, do individual match
+            # if a specific partner record is passed, do an individual match
             if partner:
-                self.passed_partner = partner
+                _logger.info('Jacob passed a partner {}'.format(partner))
                 partner_subscriptions = fields.Many2one(
                     'sale.subscription',
                     domain=[
@@ -35,18 +71,18 @@ class SaleSubscription(models.Model):
                     ).service.action_re_activate()
             # else default to processing all partner records
             else:
-                # I think it is angry about using subscriptions in this manner
-                # gather all valid, active subscriptions
+                _logger.info('Jacob did not pass a partner')
                 for service in self.subscriptions:
                     # compute their credit and credit limit by credit type &
                     # compare the credit with their limit
+                    _logger.info('Jacob in service in subscription loop')
                     partner = fields.Many2one(
                         'res.partner',
                         domain=[
                             ('id', '=', service.partner_id)
                         ]
                     )
-
+                    _logger.info('Jacob partner matched {}'.format(partner))
                     partner_credit_type = fields.Selection(
                         related='partner.credit_limit_type'
                     )
@@ -58,6 +94,7 @@ class SaleSubscription(models.Model):
                     )
 
                     if partner_credit_type == 'fixed':
+                        _logger.info('Jacob partner credit type {}'.format(partner_credit_type))
                         self.passed_partner_id = service.partner_id
                         # if credit > credit limit or age > overdue limit then
                         # suspend all partner subscriptions
@@ -70,6 +107,7 @@ class SaleSubscription(models.Model):
                                 SaleSubscription, self
                             ).service.action_re_activate()
                     elif partner_credit_type == 'subscription_based':
+                        _logger.info('Jacob partner credit type {}'.format(partner_credit_type))
                         # get the age of the oldest open invoice
                         # partner_credit_limit_subscription_qty = fields.Int(
                         #     related='partner.credit_limit_subscription_qty'
@@ -106,3 +144,39 @@ class SaleSubscription(models.Model):
         except RuntimeError as error:
             msg = _('Error Encountered:\n {} \n {}'.format(error, error.args))
             raise UserError(msg)
+
+
+# Ken's Code
+# class SaleSubscriptionSuspension(models.Model):
+#     _inherit = 'sale.subscription'
+
+#     def suspend_service_profile(self):
+#         sp_ids = self.env['agreement.serviceprofile'].search([
+#             ('agreement_id', '=', self.agreement_id.id)])
+#         if sp_ids:
+#             sp_ids.write({'state': 'suspended'})
+
+#     def activate_service_profile(self):
+#         sp_ids = self.env['agreement.serviceprofile'].search([
+#             ('agreement_id', '=', self.agreement_id.id)])
+#         if sp_ids:
+#             sp_ids.write({'state': 'active'})
+
+#     def action_suspend(self):
+#         self.suspend_service_profile()
+#         return super().action_suspend()
+
+#     def action_re_activate(self):
+#         self.activate_service_profile()
+#         return super().action_re_activate()
+
+#     def write(self, vals):
+#         res = super().write(vals)
+#         if 'stage_id' in vals:
+#             stage_id = self.env['sale.subscription.stage'].search(
+#                 [('id', '=', vals['stage_id'])])
+#             if stage_id.name == 'Suspended':
+#                 self.suspend_service_profile()
+#             elif stage_id.name == "In Progress":
+#                 self.activate_service_profile()
+#         return res
