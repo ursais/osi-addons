ced0664c72fd04e3d255dd3a6beac9cce096849d
Jacob Barnard <jbarnard@amplex.net>
2019-08-15 14:43:11 -0400

    Simple

--- sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
+++ sale_subscription_financial_risk/models/sale_subscription_financial_risk_cron.py
@@ -40,36 +40,38 @@ class SaleSubscription(models.Model):
 
     # These functions are in agreement_sale_subscription_suspension Ken did
     # It's just not merged into osi-addons yet
-    def suspend_service_profile(self):
-        sp_ids = self.env['agreement.serviceprofile'].search([
-            ('agreement_id', '=', self.agreement_id.id)])
-        if sp_ids:
-            sp_ids.write({'state': 'suspended'})
 
-    def activate_service_profile(self):
-        sp_ids = self.env['agreement.serviceprofile'].search([
-            ('agreement_id', '=', self.agreement_id.id)])
-        if sp_ids:
-            sp_ids.write({'state': 'active'})
+    # def suspend_service_profile(self):
+    #     sp_ids = self.env['agreement.serviceprofile'].search([
+    #         ('agreement_id', '=', self.agreement_id.id)])
+    #     if sp_ids:
+    #         sp_ids.write({'state': 'suspended'})
 
-    def action_suspend(self):
-        self.suspend_service_profile()
-        return super().action_suspend()
+    # def activate_service_profile(self):
+    #     sp_ids = self.env['agreement.serviceprofile'].search([
+    #         ('agreement_id', '=', self.agreement_id.id)])
+    #     if sp_ids:
+    #         sp_ids.write({'state': 'active'})
 
-    def action_re_activate(self):
-        self.activate_service_profile()
-        return super().action_re_activate()
+    # def action_suspend(self):
+    #     self.suspend_service_profile()
+    #     return super().action_suspend()
 
-    def write(self, vals):
-        res = super().write(vals)
-        if 'stage_id' in vals:
-            stage_id = self.env['sale.subscription.stage'].search(
-                [('id', '=', vals['stage_id'])])
-            if stage_id.name == 'Suspended':
-                self.suspend_service_profile()
-            elif stage_id.name == "In Progress":
-                self.activate_service_profile()
-        return res
+    # def action_re_activate(self):
+    #     self.activate_service_profile()
+    #     return super().action_re_activate()
+
+    # def write(self, vals):
+    #     res = super().write(vals)
+    #     if 'stage_id' in vals:
+    #         stage_id = self.env['sale.subscription.stage'].search(
+    #             [('id', '=', vals['stage_id'])])
+    #         if stage_id.name == 'Suspended':
+    #             self.suspend_service_profile()
+    #         elif stage_id.name == "In Progress":
+    #             self.activate_service_profile()
+    #     return res
+
     # End unnecessary function duplication from Ken's unloaded module
 
     # CRON will loop through all records in the model
@@ -150,13 +152,13 @@ class SaleSubscription(models.Model):
                     )
                     if self.partner_id.credit > self.partner_id.credit_limit:
                         _logger.info('Jacob suspending!')
-                        self.action_suspend()
+                        # self.action_suspend()
                     elif oldest_invoice[0].date_due + delta_qty > cur_date:
                         _logger.info('Jacob suspending!')
-                        self.action_suspend()
+                        # self.action_suspend()
                     else:
                         _logger.info('Jacob activating!')
-                        self.action_re_activate()
+                        # self.action_re_activate()
                 # Same thing, but for subscription-based credit limits
                 elif self.partner_id.credit_limit_type == 'subscription_based':
                     _logger.info('Jacob partner credit type {}'.format(
