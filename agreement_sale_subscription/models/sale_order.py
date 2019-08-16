89347ec9f02004cdfed741c80a02c76ca8af9002
scampbell <scampbell@opensourceintegrators.com>
2019-08-16 16:03:44 -0700

    [FIX] Move to _action_confirm()

--- agreement_sale_subscription/models/sale_order.py
+++ agreement_sale_subscription/models/sale_order.py
@@ -7,15 +7,13 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.multi
-    def create_subscriptions(self):
-        res = super().create_subscriptions()
+
+    def _action_confirm(self):
+        res = super()._action_confirm()
         for order in self:
             for line in order.order_line:
                 if line.subscription_id and order.agreement_id:
-                    # Set the subscription on the agreement
                     order.agreement_id.subscription_id = \
                         line.subscription_id.id
-                    # Set the agreement on the subscription
-                    line.subscription_id.agreement_id = order.agreement_id.id
+                    line.subscription_id.agreement_id = order.agreement_id
         return res
