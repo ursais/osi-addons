6740be88248e74d28d2798c7b788020d11a5fa47
Steve Campbell <scampbell@opensourceintegrators.com>
2019-08-29 10:33:08 -0700

    [FIX] agreement_sale_subscription: Agreement Not Set on Subscription From Sale Order (#185)

--- agreement_sale_subscription/models/sale_order.py
+++ agreement_sale_subscription/models/sale_order.py
@@ -8,14 +8,10 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.multi
-    def create_subscriptions(self):
-        res = super().create_subscriptions()
-        for order in self:
-            for line in order.order_line:
-                if line.subscription_id and order.agreement_id:
-                    # Set the subscription on the agreement
-                    order.agreement_id.subscription_id = \
-                        line.subscription_id.id
-                    # Set the agreement on the subscription
-                    line.subscription_id.agreement_id = order.agreement_id.id
+    def _action_confirm(self):
+        res = super()._action_confirm()
+        for order in self.filtered(lambda x: x.agreement_id):
+            for line in order.order_line.filtered(lambda l: l.subscription_id):
+                order.agreement_id.subscription_id = line.subscription_id.id
+                line.subscription_id.agreement_id = order.agreement_id.id
         return res
