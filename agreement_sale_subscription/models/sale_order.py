8393f614e5e7c109f8303cf3a4e3302960c84b2a
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-08-29 22:38:40 +0530

    [IMP] module code and correct README.rst file

--- agreement_sale_subscription/models/sale_order.py
+++ agreement_sale_subscription/models/sale_order.py
@@ -1,19 +1,17 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.multi
     def _action_confirm(self):
         res = super()._action_confirm()
-        for order in self:
-            for line in order.order_line:
-                if line.subscription_id and order.agreement_id:
-                    order.agreement_id.subscription_id = \
-                        line.subscription_id.id
-                    line.subscription_id.agreement_id = \
-                        order.agreement_id.id
+        for order in self.filtered(lambda x: x.agreement_id):
+            for line in order.order_line.filtered(lambda l: l.subscription_id):
+                order.agreement_id.subscription_id = line.subscription_id.id
+                line.subscription_id.agreement_id = order.agreement_id.id
         return res
