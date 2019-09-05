166a0422b68116ed4dad6964d58d5ebb07e968a0
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-09-05 11:25:54 -0500

    [IMP] sale_subscription_financial_risk

--- sale_subscription_financial_risk/models/res_partner.py
+++ sale_subscription_financial_risk/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Amplex
 # Copyright (C) 2019 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
@@ -13,41 +13,31 @@ class ResPartner(models.Model):
         help='If one invoice is due for more than the duration \
         specified here, all the subscriptions of the customer will \
         be suspended.',
-        default=15
-    )
+        default=15)
 
     # Overdue limit unit of measurement
     overdue_limit_uom = fields.Selection(
         string='Unit of Measure',
-        selection=[
-            ('days', 'Days'),
-            ('weeks', 'Weeks'),
-            ('months', 'Months')
-        ]
-    )
+        selection=[('days', 'Days'),
+                   ('weeks', 'Weeks'),
+                   ('months', 'Months')],
+        default='days', required=True)
 
     # Hide the credit_limit field if "Credit Limit Type" is not "Fixed"
     credit_limit_type = fields.Selection(
         string='Credit Limit Type',
-        selection=[
-            ('fixed', 'Fixed Amount'),
-            ('subscription_based', 'Based on Subscription')
-        ]
-    )
+        selection=[('fixed', 'Fixed Amount'),
+                   ('subscription_based', 'Based on Subscriptions')])
 
     # Hide these last 2 fields only if the type is not "Based on Subscriptions"
     credit_limit_subscription_qty = fields.Integer(
-        string='Using the last',
-        default=4
-    )
+        string='Using the last', default=4)
 
     # Credit limit unit of measurement
     credit_limit_subscription_uom = fields.Selection(
         string='Unit of Measure',
-        selection=[
-            ('weeks', 'Weeks'),
-            ('months', 'Months'),
-            ('quarters', 'Quarters'),
-            ('years', 'Years')
-        ]
-    )
+        selection=[('weeks', 'Weeks'),
+                   ('months', 'Months'),
+                   ('quarters', 'Quarters'),
+                   ('years', 'Years')],
+        default='weeks', required=True)
