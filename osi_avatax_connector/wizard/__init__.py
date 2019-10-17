d3c7720a04d3e35e132aaa871328543929f7c3b6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-17 17:03:13 -0500

    [DEL] osi_avatax_connector: Move to https://github.com/sodexis/avatax_connector

--- osi_avatax_connector/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2019 Odoo
-# Copyright (C) 2019 Open Source Integrators
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from . import (
-    avalara_salestax_ping,
-    avalara_salestax_address_validate,
-    sale_advance_payment_inv,
-)
