202e528d7968efa135911aeb0f7f9ce5c8410f74
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-11-06 15:47:18 -0600

    Bump version

--- helpdesk_fieldservice/__manifest__.py
+++ helpdesk_fieldservice/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Helpdesk - Field Service',
     'summary': 'Create service requests from a ticket',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'license': 'LGPL-3',
     'author': 'Open Source Integrators',
     'category': 'Helpdesk',
