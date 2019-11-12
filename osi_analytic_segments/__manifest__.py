c2130ab61bae67920650e75b7906754b4c3a2951
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-11-11 22:11:57 -0600

    [FIX] osi_analytic_segments: KeyError: 0 (#213)

--- osi_analytic_segments/__manifest__.py
+++ osi_analytic_segments/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "OSI Analytic Segments",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "license": "LGPL-3",
     "summary": "Additional segments for analytic accounts",
     "author": "Open Source Integrators",
