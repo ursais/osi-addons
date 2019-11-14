97a8a99cdee7f773e670cbfdd36b97270ffaaa19
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2019-11-14 10:55:15 -0600

    [UPD] Reviewer Suggestions
    Reviewer suggestions.

--- helpdesk_fieldservice/models/fsm_order.py
+++ helpdesk_fieldservice/models/fsm_order.py
@@ -14,6 +14,37 @@ class FSMOrder(models.Model):
     ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket',
                                 track_visibility='onchange')
 
+    def action_complete(self):
+        if not self.date_end:
+            raise ValidationError(_("Cannot move to Complete " +
+                                    "until 'Actual End' is filled in"))
+        if not self.resolution:
+            raise ValidationError(_("Cannot move to Complete " +
+                                    "until 'Resolution' is filled in"))
+        if self.ticket_id:
+            if self.ticket_id.stage_id.is_close is True:
+                return self.write({'stage_id': self.env.ref(
+                    'fieldservice.fsm_stage_completed').id})
+            else:
+                self.write({'stage_id': self.env.ref(
+                    'fieldservice.fsm_stage_completed').id})
+                view_id = self.env.ref(
+                    'helpdesk_fieldservice.fsm_order_close_wizard_view_form').id
+                return {
+                    'view_id': view_id,
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'fsm.order.close.wizard',
+                    'type': 'ir.actions.act_window',
+                    'target': 'new',
+                    'context': {'default_ticket_id': self.ticket_id.id,
+                                'default_team_id': self.ticket_id.team_id.id,
+                                'default_resolution': self.resolution}
+                }
+        else:
+            return self.write({'stage_id': self.env.ref(
+                'fieldservice.fsm_stage_completed').id})
+
     @api.multi
     def action_view_order(self):
         '''
--- helpdesk_fieldservice/wizard/fsm_order_close_wizard.py
+++ helpdesk_fieldservice/wizard/fsm_order_close_wizard.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2019 Pavlov Media
-# License Proprietary. Do not copy, share nor distribute.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
@@ -17,44 +17,10 @@ class FSMOrderCloseWizard(models.TransientModel):
     @api.multi
     def action_close_ticket(self):
         for record in self:
-            if record.ticket_id.stage_id.is_close is False:
+            if not record.ticket_id.stage_id.is_close:
                 record.ticket_id.write({'resolution': record.resolution,
                                         'stage_id': record.stage_id.id})
         return {
             'type': 'ir.actions.client',
             'tag': 'reload',
         }
-
-class FSMOrder(models.Model):
-    _inherit = 'fsm.order'
-
-    def action_complete(self):
-        if not self.date_end:
-            raise ValidationError(_("Cannot move to Complete " +
-                                    "until 'Actual End' is filled in"))
-        if not self.resolution:
-            raise ValidationError(_("Cannot move to Complete " +
-                                    "until 'Resolution' is filled in"))
-        if self.ticket_id:
-            if self.ticket_id.stage_id.is_close is True:
-                return self.write({'stage_id': self.env.ref(
-                    'fieldservice.fsm_stage_completed').id})
-            else:
-                self.write({'stage_id': self.env.ref(
-                    'fieldservice.fsm_stage_completed').id})
-                view_id = self.env.ref(
-                    'helpdesk_fieldservice.fsm_order_close_wizard_view_form').id
-                return {
-                    'view_id': view_id,
-                    'view_type': 'form',
-                    'view_mode': 'form',
-                    'res_model': 'fsm.order.close.wizard',
-                    'type': 'ir.actions.act_window',
-                    'target': 'new',
-                    'context': {'default_ticket_id': self.ticket_id.id,
-                                'default_team_id': self.ticket_id.team_id.id,
-                                'default_resolution': self.resolution}
-                }
-        else:
-            return self.write({'stage_id': self.env.ref(
-                'fieldservice.fsm_stage_completed').id})
