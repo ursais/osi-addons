6346a5a0de06f67ca0f7cbb7008f1ab59a497687
scampbell <scampbell@opensourceintegrators.com>
2019-11-26 16:11:14 -0800

    [IMP] Ticket -> FSO: Agreement Context

---
+++ fieldservice_agreement_helpdesk/models/helpdesk_ticket.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    @api.multi
+    def action_create_order(self):
+        import pdb; pdb.set_trace()
+        res = super().action_create_order()
+        '''
+        This function returns an action that displays a full FSM Order
+        form when creating an FSM Order from a ticket.
+        '''
+        # override the context to get rid of the default filtering
+        res['context'] = {
+            'default_agreement_id': self.agreement_id.id,
+            'default_serviceprofile_id': self.serviceprofile_id.id,
+        }
+        return res
