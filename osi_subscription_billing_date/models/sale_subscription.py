a2fbbcf1e38cb3d8c6fe45cd684d63abcd153ed5
Daniel Reis <dreis@opensourceintegrators.com>
2020-01-02 15:55:26 +0000

    Add osi_subscription_billing_date

---
+++ osi_subscription_billing_date/models/sale_subscription.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2019 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+import calendar
+from dateutil.relativedelta import relativedelta
+
+
+DAY = relativedelta(days=1)  # one day, to add to dates
+
+
+class SaleSubscription(models.Model):
+    _inherit = 'sale.subscription'
+
+    def _get_add_periods(self, current_date=None, periods=1):
+        self.ensure_one()
+        current_date = current_date or self.recurring_next_date
+        period_map = {
+            'daily': 'days',
+            'weekly': 'weeks',
+            'monthly': 'months',
+            'yearly': 'years'}
+        period = period_map[self.recurring_rule_type]
+        interval = self.recurring_interval * periods
+        new_date = current_date + relativedelta(**{period: interval})
+        return new_date
+
+    def _prepare_invoice(self):
+        """
+        Detect if the Billing Day has changed.
+        if so, adjust the invoiced quantity to the interrim period
+        until the next regular billing date.
+
+        The recurring_next_date is the date we creating the invoice
+        for the next period. If the billing day was changed,
+        we create a prorated invoice and adjust the next date.
+
+        This happens during the regular _recurring_create_invoice()
+        process.
+
+        After it runs, one period will be incremented to the
+        next invoice date (recurring_next_date).
+
+        So we also need to set the current "next date" to be one period
+        before the next intended invoicing date.
+        """
+        def date_set_day(my_date, day):
+            day = 31 if day == 'eom' else int(day)
+            last_day = calendar.monthrange(my_date.year, my_date.month)[1]
+            return my_date.replace(day=min(day, last_day))
+
+        values = super()._prepare_invoice()
+        adb = self.partner_id.authoritative_bill_date
+        if not adb:
+            return values
+        # Find billing date, according to current customer billing day
+        invoiced_date = self.recurring_next_date
+        expected_date = date_set_day(invoiced_date, adb)
+        # If billing date not the current one, prorate current invoice
+        if expected_date != invoiced_date:
+            # find the base date to increment, for the next invoicing date
+            # and set as next date to be incremented after invoice generation
+            if expected_date < invoiced_date:
+                base_date = expected_date
+            else:
+                base_date = self._get_add_periods(expected_date, -1)
+            self.recurring_next_date = base_date
+            # Prorate the invoice line
+            period_to = self._get_add_periods(base_date) - DAY
+            days, ratio = self._prorate_compute_ratio(
+                date_from=invoiced_date,
+                period_from=base_date,
+                period_to=period_to)
+            for a, b, line in values.get('invoice_line_ids', []):
+                line['quantity'] *= ratio
+                line['name'] += " (%d days)" % days
+        return values
--- osi_subscription_prorate/models/sale_subscription.py
+++ osi_subscription_prorate/models/sale_subscription.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2019 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 import datetime
 from odoo import api, fields, models, _
 from datetime import date
@@ -8,163 +9,90 @@ import calendar
 from odoo.exceptions import UserError
 
 
+DAY = datetime.timedelta(days=1)  # one day, to add to dates
+
+
 class SaleSubscription(models.Model):
     _inherit = 'sale.subscription'
 
-    def prorate_note_line(self, account_invoice, option_line, ratio):
-        """ Add an invoice line on the sales order for the specified option and add a discount
-        to take the partial recurring period into account """
-        inv_line_obj = self.env['account.invoice.line']
-        values = {
-            'invoice_id': account_invoice.id,
-            'product_id': option_line.product_id.id,
-            'subscription_id': self.id,
-            'quantity': option_line.quantity * ratio,
-            'uom_id': option_line.uom_id.id,
-            'price_unit': self.pricelist_id.with_context({'uom': option_line.uom_id.id}).get_product_price(option_line.product_id, 1, False),
-            'name': option_line.name,
-            'account_id': account_invoice.partner_id.property_account_receivable_id.id,
-            'account_analytic_id': self.analytic_account_id.id,
-        }
-        return inv_line_obj.create(values)
+    def _prorate_compute_ratio(self, date_from=None, date_to=None,
+                               period_from=None, period_to=None):
+        """
+        Compute the quantity to bill or refund.
+        Use date_from to bill, or date_to to refund.
 
-    @api.multi
-    def create_prorated_note(self, ratio, days_diff):
-        fpos_id = self.env['account.fiscal.position'].get_fiscal_position(self.partner_id.id)
-        invoice_obj = self.env['account.invoice']
-        team = self.env['crm.team']._get_default_team_id(user_id=self.user_id.id)
-        new_inv_vals = {
-            'partner_id': self.partner_id.id,
-            'team_id': team and team.id,
-            'fiscal_position_id': fpos_id,
-            'origin': self.code,
-            'type': 'out_refund',
-            'name': 'Credit For ' + str(days_diff) + ' Unused Days'
-        }
-        # we don't override the default if no payment terms has been set on the customer
-        if self.partner_id.property_payment_term_id:
-            new_inv_vals['payment_term_id'] = self.partner_id.property_payment_term_id.id
-        inv = invoice_obj.create(new_inv_vals)
-        for line_id in self.recurring_invoice_line_ids:
-            self.prorate_note_line(inv, line_id, ratio)
-        inv.action_invoice_open()
-        return {
-            "type": "ir.actions.act_window",
-            "res_model": "account.invoice",
-            "views": [[False, "form"]],
-            "res_id": inv.id,
-        }
+        Returns a signed number of days to bill,
+        and a signed ratio to apply.
+        """
+        self.ensure_one()
+        period_from = (
+            period_from or
+            self.recurring_last_date or
+            self.date_start)
+        period_to = period_to or (self.recurring_next_date - DAY)
+        period_period = period_to - period_from + DAY
+        if date_from:
+            bill_period = period_to - date_from + DAY
+        else:
+            bill_period = -(period_to - date_to)
+        bill_days = float(bill_period.days)
+        ratio = bill_days / period_period.days
+        return bill_days, ratio
+
+    def _prorate_invoice_line(self, line, fiscal_position, ratio, days):
+        """
+        Prepares the values for a prorated invoice line
+        """
+        values = self._prepare_invoice_line(line, fiscal_position)
+        values['quantity'] *= ratio
+        values['name'] += " (%d days)" % days
+        return values
+
+    def _prorate_invoice(self, lines, date_from=None, date_to=None):
+        """
+        Prepares the values for a prorated invoice
+        """
+        self.ensure_one()
+        self = self.with_context(lang=self.partner_id.lang)
+        invoice = self._prepare_invoice_data()
+        fiscal_position = self.env['account.fiscal.position'].browse(
+            invoice['fiscal_position'])
+        days, ratio = self._prorate_compute_ratio(
+            date_from=date_from, date_to=date_to)
+        if days < 0:
+            invoice['comment'] = _('Credit for %d days') % abs(days)
+            invoice['type'] = 'out_refund'
+        # Prepare line
+        line_values = [
+            (6, 0, self._prorate_invoice_line(
+                line, fiscal_position, ratio, days))
+            for line in lines]
+        # Add to invoice values
+        invoice['invoice_line_ids'] = line_values
+        return invoice
+
+    def _prorate_create_invoice(self, lines, date_from=None, date_to=None):
+        """
+        Creates a new Invoice or Credit Note with the prorated amount to bill
+        """
+        self.ensure_one()
+        values = self._prorate_invoice(lines, date_from, date_to)
+        new_invoice = self.env['account.invoice'].create(values)
+        return new_invoice
+
 
     @api.onchange('stage_id')
     def onchange_stage_id(self):
         res = super().onchange_stage_id()
         if self.stage_id.name == 'Closed':
-            # Count the number of days each line has been active this billing cycle
-            next_date = self.recurring_next_date
-            today = date.today()
-            billing_cycle = calendar.monthrange(today.year, today.month)[1]
-            delta = (next_date - today)
-            days_diff = delta.days
-            ratio = float(days_diff/billing_cycle)
-            self.create_prorated_note(ratio, days_diff)
-        if self.stage_id.name == 'In Progress':
-            # Generate Invoice
-            fpos_id = self.env['account.fiscal.position'].get_fiscal_position(self.partner_id.id)
-            invoice_obj = self.env['account.invoice']
-            team = self.env['crm.team']._get_default_team_id(user_id=self.user_id.id)
-            new_inv_vals = {
-                'partner_id': self.partner_id.id,
-                'team_id': team and team.id,
-                'fiscal_position_id': fpos_id,
-                'origin': self.code,
-                'type': 'out_invoice',
-            }
-            if self.partner_id.property_payment_term_id:
-                new_inv_vals['payment_term_id'] = self.partner_id.property_payment_term_id.id
-            inv = invoice_obj.create(new_inv_vals)
-            for line_id in self.recurring_invoice_line_ids:
-                inv_line_obj = self.env['account.invoice.line']
-                values = {
-                    'invoice_id': inv.id,
-                    'product_id': line_id.product_id.id,
-                    'subscription_id': self.id,
-                    'quantity': line_id.quantity,
-                    'uom_id': line_id.uom_id.id,
-                    'price_unit': self.pricelist_id.with_context({'uom': line_id.uom_id.id}).get_product_price(line_id.product_id, 1, False),
-                    'name': line_id.name,
-                    'account_id': inv.partner_id.property_account_receivable_id.id,
-                    'account_analytic_id': self.analytic_account_id.id,
-                    'last_invoice_date': date.today()
-                }
-                inv_line_obj.create(values)
-            inv.action_invoice_open()
+            # TODO Check proper dependency adding recurring_last_date field!
+            today = fields.Date.today()
+            lines = self.recurring_invoice_line_ids
+            self._prorate_create_invoice(lines, to_date=today)
         return res
 
-    def prorate_abd_invoice_line(self, account_invoice, option_line, ratio):
-        """ Add an invoice line on the sales order for the specified option and add a discount
-        to take the partial recurring period into account """
-        inv_line_obj = self.env['account.invoice.line']
-        values = {
-            'invoice_id': account_invoice.id,
-            'product_id': option_line.product_id.id,
-            'subscription_id': self.id,
-            'quantity': option_line.quantity * ratio,
-            'uom_id': option_line.uom_id.id,
-            'price_unit': self.pricelist_id.with_context({'uom': option_line.uom_id.id}).get_product_price(option_line.product_id, 1, False),
-            'name': option_line.name,
-            'account_id': account_invoice.partner_id.property_account_receivable_id.id,
-            'account_analytic_id': self.analytic_account_id.id,
-        }
-        return inv_line_obj.create(values)
-
-    @api.multi
-    def create_prorated_abd_invoice(self, ratio, days_diff):
-        fpos_id = self.env['account.fiscal.position'].get_fiscal_position(self.partner_id.id)
-        invoice_obj = self.env['account.invoice']
-        team = self.env['crm.team']._get_default_team_id(user_id=self.user_id.id)
-        new_inv_vals = {
-            'partner_id': self.partner_id.id,
-            'team_id': team and team.id,
-            'fiscal_position_id': fpos_id,
-            'origin': self.code,
-            'type': 'out_invoice',
-            'name': 'Invoiced For ' + str(days_diff) + ' Days'
-        }
-        # we don't override the default if no payment terms has been set on the customer
-        if self.partner_id.property_payment_term_id:
-            new_inv_vals['payment_term_id'] = self.partner_id.property_payment_term_id.id
-        inv = invoice_obj.create(new_inv_vals)
-        for line in self.recurring_invoice_line_ids:
-            self.prorate_abd_invoice_line(inv, line, ratio)
-            # Update Last Invoice Date when generating invoices
-            line.last_invoice_date = date.today()
-        inv.action_invoice_open()
-
-    def wizard_change_recurring_next_date(self, next_month):
-        # Save the previous Next Bill Date
-        old_date = self.recurring_next_date
-        # Change to the new New Bill Date
-        res = super().wizard_change_recurring_next_date(next_month)
-        # Bill the number of days between the two dates
-        new_date = self.recurring_next_date
-        billing_cycle = calendar.monthrange(old_date.year, old_date.month)[1]
-        delta = (new_date - old_date)
-        days_diff = delta.days
-        ratio = float(days_diff/billing_cycle)
-        self.create_prorated_abd_invoice(ratio, days_diff)
-        return res
-
-    def recurring_invoice(self):
-        res = super().recurring_invoice()
-        for line_id in self.recurring_invoice_line_ids:
-            line_id.last_invoice_date = date.today()
-        return res
-
-    def write(self, vals):
-        if vals.get('stage_id', False):
-            if self.stage_id.name == 'Closed':
-                raise UserError(_('Closed Subscriptions Cannot Be Reopened'))
-        return super().write(vals)
+    # TODO def set_close(self):
+    #    return super().set_close()
 
 
 class SaleSubscriptionLine(models.Model):
@@ -173,106 +101,22 @@ class SaleSubscriptionLine(models.Model):
     @api.model
     def create(self, vals):
         """ Adding a new line creates an immediate prorated invoice for it """
-        res = super().create(vals)
-        if res.analytic_account_id.stage_id.name == 'In Progress':
-            # They are going to be billed from now until Next Bill Date.
-            next_date = res.analytic_account_id.recurring_next_date
-            today = date.today()
-            billing_cycle = calendar.monthrange(today.year, today.month)[1]
-            # TODO we want to be able to select the start date
-            delta = (next_date - today)
-            days_diff = delta.days
-            ratio = float(days_diff/billing_cycle)
-            # TODO single point for the Invoice creation
-            self.create_new_service_invoice(res.analytic_account_id, ratio, days_diff, res)
-            # Update Last Invoice Date when generating invoices
-            res.last_invoice_date = date.today()
-        return res
-
-    def prorate_new_service_invoice_line(self, subscription_id, account_invoice, ratio, line_id):
-        """ Add an invoice line on the sales order for the specified option and add a discount
-        to take the partial recurring period into account """
-        inv_line_obj = self.env['account.invoice.line']
-        values = {
-            'invoice_id': account_invoice.id,
-            'product_id': line_id.product_id.id,
-            'subscription_id': subscription_id.id,
-            'quantity': line_id.quantity * ratio,
-            'uom_id': line_id.uom_id.id,
-            'price_unit': subscription_id.pricelist_id.with_context({'uom': line_id.uom_id.id}).get_product_price(line_id.product_id, 1, False),
-            'name': line_id.name,
-            'account_id': account_invoice.partner_id.property_account_receivable_id.id,
-            'account_analytic_id': subscription_id.analytic_account_id.id,
-        }
-        return inv_line_obj.create(values)
-
-    @api.multi
-    def create_new_service_invoice(self, subscription_id, ratio, days_diff, line_id):
-        fpos_id = self.env['account.fiscal.position'].get_fiscal_position(subscription_id.partner_id.id)
-        invoice_obj = self.env['account.invoice']
-        team = self.env['crm.team']._get_default_team_id(user_id=self.env.user.id)
-        new_inv_vals = {
-            'partner_id': subscription_id.partner_id.id,
-            'team_id': team and team.id,
-            'fiscal_position_id': fpos_id,
-            'origin': subscription_id.code,
-            'type': 'out_invoice',
-            'name': 'Invoiced For ' + str(days_diff) + ' Days'
-        }
-        # we don't override the default if no payment terms has been set on the customer
-        if subscription_id.partner_id.property_payment_term_id:
-            new_inv_vals['payment_term_id'] = subscription_id.partner_id.property_payment_term_id.id
-        inv = invoice_obj.create(new_inv_vals)
-        self.prorate_new_service_invoice_line(subscription_id, inv, ratio, line_id)
-        inv.action_invoice_open()
+        line = super().create(vals)
+        subscription = line.account_analytic_id
+        if subscription.stage_id.in_progress:
+            today = fields.Date.today()
+            subscription._prorate_create_invoice(line, date_from=today)
+        return line
 
     @api.multi
     def unlink(self):
-        for line_id in self:
-            if line_id.analytic_account_id.stage_id.name == 'In Progress':
-                # Count the number of days this line has been active this billing cycle
-                next_date = line_id.analytic_account_id.recurring_next_date
-                today = date.today()
-                billing_cycle = calendar.monthrange(today.year, today.month)[1]
-                delta = (next_date - today)
-                days_diff = delta.days
-                ratio = float(days_diff/billing_cycle)
-                self.create_remove_service_note(line_id.analytic_account_id, ratio, days_diff, line_id)
+        """
+        When a Subscription Line is deleted,
+        create Credit Note for the unused service days.
+        """
+        for line in self:
+            subscription = line.account_analytic_id
+            if subscription.stage_id.in_progress:
+                today = fields.Date.today()
+                subscription._prorate_create_invoice(line, date_to=today)
         return super().unlink()
-
-    def remove_service_note_line(self, subscription_id, account_invoice, ratio, line_id):
-        """ Add an invoice line on the sales order for the specified option and add a discount
-        to take the partial recurring period into account """
-        inv_line_obj = self.env['account.invoice.line']
-        values = {
-            'invoice_id': account_invoice.id,
-            'product_id': line_id.product_id.id,
-            'subscription_id': subscription_id.id,
-            'quantity': line_id.quantity * ratio,
-            'uom_id': line_id.uom_id.id,
-            'price_unit': subscription_id.pricelist_id.with_context({'uom': line_id.uom_id.id}).get_product_price(line_id.product_id, 1, False),
-            'name': line_id.name,
-            'account_id': account_invoice.partner_id.property_account_receivable_id.id,
-            'account_analytic_id': subscription_id.analytic_account_id.id,
-        }
-        return inv_line_obj.create(values)
-
-    @api.multi
-    def create_remove_service_note(self, subscription_id, ratio, days_diff, line_id):
-        fpos_id = self.env['account.fiscal.position'].get_fiscal_position(subscription_id.partner_id.id)
-        invoice_obj = self.env['account.invoice']
-        team = self.env['crm.team']._get_default_team_id(user_id=self.env.user.id)
-        new_inv_vals = {
-            'partner_id': subscription_id.partner_id.id,
-            'team_id': team and team.id,
-            'fiscal_position_id': fpos_id,
-            'origin': subscription_id.code,
-            'type': 'out_refund',
-            'name': 'Credit For ' + str(days_diff) + ' Days'
-        }
-        # we don't override the default if no payment terms has been set on the customer
-        if subscription_id.partner_id.property_payment_term_id:
-            new_inv_vals['payment_term_id'] = subscription_id.partner_id.property_payment_term_id.id
-        inv = invoice_obj.create(new_inv_vals)
-        self.remove_service_note_line(subscription_id, inv, ratio, line_id)
-        inv.action_invoice_open()
