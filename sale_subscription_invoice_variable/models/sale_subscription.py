b8b9f8ab608f76880b7f32b974eb5f204b85eae6
Daniel Reis <dreis@opensourceintegrators.com>
2020-01-03 12:51:02 +0000

    Lint

--- sale_subscription_invoice_variable/models/sale_subscription.py
+++ sale_subscription_invoice_variable/models/sale_subscription.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2019, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from datetime import timedelta
-from odoo import models
+from datetime import date, timedelta
+from odoo import api, fields, models
 
 
 class SaleSubscription(models.Model):
