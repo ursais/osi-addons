d771bd8a7ad7df39a2eb855046fb135cacbb24a5
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-03-23 17:02:31 -0600

    [IMP] helpdesk_fieldservice

--- helpdesk_fieldservice/__manifest__.py
+++ helpdesk_fieldservice/__manifest__.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 {
     'name': 'Helpdesk - Field Service',
     'summary': 'Create service requests from a ticket',
@@ -10,10 +9,11 @@
     'category': 'Helpdesk',
     'website': 'https://github.com/ursais/osi-addons',
     'depends': [
-        'helpdesk_timesheet',
+        'helpdesk',
         'fieldservice',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/helpdesk_ticket_views.xml',
         'views/fsm_location_views.xml',
         'views/fsm_order_views.xml',
