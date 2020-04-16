55025bea31033608a0c31640ca1032ddd9926a94
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-04-16 09:49:25 -0500

    [IMP] osi_analytic_segments_purchase: Default value for segment one

--- osi_analytic_segments_purchase/models/purchase_order_line.py
+++ osi_analytic_segments_purchase/models/purchase_order_line.py
@@ -1,15 +1,18 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2019 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 from odoo import fields, models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
+    def _get_default_segment_one(self):
+        return self.env["analytic.segment.one"].get_default_segment_one()
+
     analytic_segment_one_id = fields.Many2one(
         'analytic.segment.one',
         string='Analytic Segment One',
+        default=_get_default_segment_one,
         copy=False)
     analytic_segment_two_id = fields.Many2one(
         'analytic.segment.two',
