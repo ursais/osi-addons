1f1f54e677cf2fdc1606edb37a4cddcbd86b44db
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-04-16 09:49:25 -0500

    [IMP] osi_analytic_segments_purchase: Default value for segment one

--- osi_analytic_segments_purchase/models/__init__.py
+++ osi_analytic_segments_purchase/models/__init__.py
@@ -1,4 +1,5 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import purchase_order
-from . import account_invoice
+from . import (
+    purchase_order_line,
+    account_invoice,
+)
