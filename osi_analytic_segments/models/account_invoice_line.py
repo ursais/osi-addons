fecba792cf8ba5860bbea46f78ef39fe5f47223c
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-04-16 09:45:50 -0500

    [IMP] osi_analytic_segments: Default value for segment one

--- osi_analytic_segments/models/account_invoice_line.py
+++ osi_analytic_segments/models/account_invoice_line.py
@@ -1,11 +1,10 @@
 # Copyright (C) 2019 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 from odoo import fields, models
 
 
-class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
 
     def _get_default_segment_one(self):
         return self.env["analytic.segment.one"].get_default_segment_one()
