60e193a6e6da698e26433c76859a7446d3ad75b7
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-04-16 09:51:28 -0500

    [IMP] osi_analytic_segments_sales: Default value for segment one

--- osi_analytic_segments_sales/models/__init__.py
+++ osi_analytic_segments_sales/models/__init__.py
@@ -1,4 +1,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import sale_order
-from . import account_invoice
+from . import (
+    sale_order,
+    sale_order_line,
+    account_invoice,
+)
--- osi_analytic_segments_sales/models/sale_order.py
+++ osi_analytic_segments_sales/models/sale_order.py
@@ -1,45 +1,20 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    def _get_default_segment_one(self):
+        return self.env["analytic.segment.one"].get_default_segment_one()
+
     analytic_segment_one_id = fields.Many2one(
         'analytic.segment.one',
         string='Analytic Segment One',
+        default=_get_default_segment_one,
         copy=False)
     analytic_segment_two_id = fields.Many2one(
         'analytic.segment.two',
         string='Analytic Segment Two',
         copy=False)
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    analytic_account_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account',
-        help="The analytic account related to a sales order Line.",
-        copy=False)
-    analytic_segment_one_id = fields.Many2one(
-        'analytic.segment.one',
-        string='Analytic Segment One',
-        copy=False)
-    analytic_segment_two_id = fields.Many2one(
-        'analytic.segment.two',
-        string='Analytic Segment Two',
-        copy=False)
-
-    @api.multi
-    def _prepare_invoice_line(self, qty):
-        res = super()._prepare_invoice_line(qty)
-        res.update({
-            'analytic_segment_one_id':
-                self.order_id.analytic_segment_one_id.id,
-            'analytic_segment_two_id':
-                self.order_id.analytic_segment_two_id.id})
-        return res
