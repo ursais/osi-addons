35c4489672a732f4233458a3b63f0256308bb20f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-04-20 11:36:23 -0500

    Update osi_analytic_segments_defaults/models/account_invoice_line.py
    Co-Authored-By: Bhavesh Odedra <bodedra@opensourceintegrators.com>

--- osi_analytic_segments_defaults/models/account_invoice_line.py
+++ osi_analytic_segments_defaults/models/account_invoice_line.py
@@ -21,7 +21,7 @@ class AccountInvoiceLine(models.Model):
     def _set_additional_fields(self, invoice):
         rec = self.env['account.analytic.default'].account_get(
             self.product_id.id, self.invoice_id.partner_id.id,
-            self.env.uid, fields.Date.today(), company_id=self.company_id.id)
+            self.env.uid, fields.Date.today(), company_id=invoice.company_id.id)
         if rec and not self.analytic_segment_one_id:
             self.analytic_segment_one_id = rec.analytic_segment_one_id.id
         if rec and not self.analytic_segment_two_id:
