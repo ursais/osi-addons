98f29085c3f03a8f47a5fd7a485abd3dd63bc16b
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2020-04-28 15:54:44 -0700

    [ADD]12 #2260 osi_bank_reconciled_report_Extended

---
+++ osi_bank_reconciled_report_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Bank Reconciliation Report Extended",
+    "summary": "Provide Custom Date FROM and DATE TO range",
+    "version": "12.0.1.0.0",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "http://www.opensourceintegrators.com",
+    "category": "Inventory",
+    "license": "LGPL-3",
+    "depends": [
+        "osi_bank_reconciled_report",
+    ],
+    "data": [
+    ],
+    "application": False,
+}
