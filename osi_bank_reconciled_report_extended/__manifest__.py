1923fe549fe956202d950aadc4767798f07800a1
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2020-05-04 20:46:57 -0700

    [IMP]Attendeds MC comments for PR #263

--- osi_bank_reconciled_report_extended/__manifest__.py
+++ osi_bank_reconciled_report_extended/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Bank Reconciliation Report Extended",
+    "name": "Bank Reconciled Report Extended",
     "summary": "Provide Custom Date FROM and DATE TO range",
     "version": "12.0.1.0.0",
     "author": "Open Source Integrators",
