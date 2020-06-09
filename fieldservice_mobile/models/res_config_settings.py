e7c562c7c3548b7c0532b61a073b6905797d06b9
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-06-09 21:59:51 +0530

    [WIP][ADD][12.0] fieldservice_mobile

---
+++ fieldservice_mobile/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_fieldservice_isp_flow = fields.Boolean(
+        string='Enable Field Service ISP Workflow')
