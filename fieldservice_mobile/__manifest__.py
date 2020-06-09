0b7fe54b0330881ad75621a41dfa54560375c76e
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-06-09 21:59:51 +0530

    [WIP][ADD][12.0] fieldservice_mobile

---
+++ fieldservice_mobile/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service Mobile',
+    'summary': 'This module is manage FSM mobile stages based on'
+    ' configuration.',
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators',
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'fieldservice',
+    ],
+    'data': [
+        'views/res_config_settings.xml',
+        'views/fsm_stage_view.xml',
+    ],
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+    ],
+}
