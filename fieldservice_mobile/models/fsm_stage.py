0455c8260e963e936916b2ab6a008b10b3aa7242
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-06-09 21:59:51 +0530

    [WIP][ADD][12.0] fieldservice_mobile

---
+++ fieldservice_mobile/models/fsm_stage.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FSMStage(models.Model):
+    _inherit = 'fsm.stage'
+
+    is_display_on_mobile = fields.Boolean('Is Display on Mobile?')
