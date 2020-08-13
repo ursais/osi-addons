d370e25764cd0ccda43ec83d10b93d8158c682d4
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-08-13 23:10:50 +0530

    [13.0][MIG] sale_subscription_suspend

--- sale_subscription_suspend/__manifest__.py
+++ sale_subscription_suspend/__manifest__.py
@@ -3,19 +3,19 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Subscription Suspensions',
-    'summary': 'Suspend and Re-Activate your sales subscriptions',
-    'version': '12.0.1.0.0',
-    'license': 'LGPL-3',
-    'author': 'Open Source Integrators, Brian McMaster',
-    'category': 'Sales',
-    'website': 'https://github.com/ursais/osi-addons',
-    'depends': [
-        'sale_subscription',
+    "name": "Subscription Suspensions",
+    "summary": "Suspend and Re-Activate your sales subscriptions",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators, Brian McMaster",
+    "category": "Sales",
+    "website": "https://github.com/ursais/osi-addons",
+    "depends": [
+        "sale_subscription",
     ],
-    'data': [
-        'data/sale_subscription_data.xml',
-        'views/sale_subscription.xml',
+    "data": [
+        "data/sale_subscription_data.xml",
+        "views/sale_subscription.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
