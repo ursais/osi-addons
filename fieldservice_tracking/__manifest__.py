82696283c58d0da2dbeecbf2838160c02a5dd696
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-08-15 00:01:44 +0530

    [WIP][12.0][ADD] fieldservice_tracking

---
+++ fieldservice_tracking/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Field Service Tracking',
+    'summary': 'This module will add tracking functionality in Fieldservice.',
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators',
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'fieldservice_substatus',
+        'uom',
+    ],
+    'data': [
+        'security/fsm_tracking_group.xml',
+        'security/ir.model.access.csv',
+        'views/fsm_person_view.xml',
+        'views/fsm_stage_view.xml',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+    ],
+}
