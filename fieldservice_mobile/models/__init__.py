0e819edef802df2da769c8d62e6f87b7a429fa9e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-08-17 16:46:46 -0500

    [IMP] fieldservice_mobile

--- fieldservice_mobile/models/__init__.py
+++ fieldservice_mobile/models/__init__.py
@@ -1,6 +1,6 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_config_settings
 from . import fsm_stage
 from . import fsm_order
 from . import fsm_stage_history
