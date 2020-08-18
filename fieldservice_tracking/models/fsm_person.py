aa3c70ac39b2b4f327c1f352560ac1462cf53a49
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-08-18 16:40:10 +0530

    [FIX Travis

--- fieldservice_tracking/models/fsm_person.py
+++ fieldservice_tracking/models/fsm_person.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 Open Source Integrators
 # Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FsmPerson(models.Model):
@@ -11,7 +11,8 @@ class FsmPerson(models.Model):
     track_lat = fields.Float('Last known latitude', digits=(16, 5))
     track_long = fields.Float('Last known longitude', digits=(16, 5))
     # track_shape = fields.Char('Last known location')
-    loc_rad = fields.Float('Last known location accuracy radius', digits=(16, 5))
+    loc_rad = fields.Float('Last known location accuracy radius',
+                           digits=(16, 5))
     loc_time = fields.Datetime('Last location ping')
     rad_uom = fields.Many2one('uom.uom', 'Unit of measure')
 
@@ -20,6 +21,8 @@ class FsmPerson(models.Model):
     track_intv_coarse = fields.Integer('Time in seconds')
     fence_rad = fields.Integer('Distance from destination')
     route_stage = fields.Selection([
-        ('out', 'Out'), ('checkin', 'Check-in'), ('enroute', 'Enroute'), ('jobstart', 'Job Start'),
-        ('working', 'Working'), ('jobstop', 'Job Stop'), ('checkout', 'Check-out'), ('break', 'Break')
+        ('out', 'Out'), ('checkin', 'Check-in'),
+        ('enroute', 'Enroute'), ('jobstart', 'Job Start'),
+        ('working', 'Working'), ('jobstop', 'Job Stop'),
+        ('checkout', 'Check-out'), ('break', 'Break')
     ], string='Route Stage')
