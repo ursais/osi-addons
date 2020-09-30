fbea06732559e0000b9de5ef9fbe4f5ea89e30ec
Murtuza Saleh(SerpentCS) <m.saleh.serpentcs@gmail.com>
2020-09-30 23:29:08 +0530

    [FIX] fieldservice_sign: Add security file in manifest (#293)

--- fieldservice_sign/__manifest__.py
+++ fieldservice_sign/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Field Service Signature',
     'summary': 'This module will provide signature link from FSM order.',
     'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Field Service',
     'author': 'Open Source Integrators',
     'website': 'https://github.com/ursais/osi-addons',
@@ -15,6 +15,7 @@
         'sign',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/fsm_order.xml',
         'views/sign_request.xml',
         'wizard/sign_send_request_views.xml',
