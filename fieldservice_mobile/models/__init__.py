8415666dd085433a86aceba0018526e49eb915c9
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-10-16 00:07:00 +0530

    [IMP] created method for getting portal fields

--- fieldservice_mobile/models/__init__.py
+++ fieldservice_mobile/models/__init__.py
@@ -4,3 +4,4 @@ from . import fsm_stage
 from . import fsm_order
 from . import fsm_stage_history
 from . import res_config_settings
+from . import res_users
--- fieldservice_mobile/models/res_config_settings.py
+++ fieldservice_mobile/models/res_config_settings.py
@@ -2,45 +2,19 @@
 # Copyright (C) 2020 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     fsm_allow_portal_view_move_qty = fields.Boolean(
-        string='Allow portal user to view stock move quantities')
+        string='Allow portal user to view stock move quantities',
+        config_parameter='fieldservice_mobile.fsm_allow_portal_view_move_qty')
     fsm_allow_portal_update_move_qty = fields.Boolean(
-        string='Allow portal user update of stock move quantities')
+        string='Allow portal user update of stock move quantities',
+        config_parameter='fieldservice_mobile.fsm_allow_portal_update_move_qty'
+    )
     fsm_allow_portal_validate_move_qty = fields.Boolean(
-        string='Allow portal user validation of stock move quantities')
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        res.update(
-            fsm_allow_portal_view_move_qty=bool(params.get_param(
-                'fieldservice_mobile.fsm_allow_portal_view_move_qty')
-            ) or False,
-            fsm_allow_portal_update_move_qty=bool(params.get_param(
-                'fieldservice_mobile.fsm_allow_portal_update_move_qty')
-            ) or False,
-            fsm_allow_portal_validate_move_qty=bool(params.get_param(
-                'fieldservice_mobile.fsm_allow_portal_validate_move_qty')
-            ) or False,
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param(
-            'fieldservice_mobile.fsm_allow_portal_view_move_qty',
-            self.fsm_allow_portal_view_move_qty)
-        self.env['ir.config_parameter'].sudo().set_param(
-            'fieldservice_mobile.fsm_allow_portal_update_move_qty',
-            self.fsm_allow_portal_update_move_qty)
-        self.env['ir.config_parameter'].sudo().set_param(
-            'fieldservice_mobile.fsm_allow_portal_validate_move_qty',
-            self.fsm_allow_portal_validate_move_qty)
+        string='Allow portal user validation of stock move quantities',
+        config_parameter='fieldservice_mobile.fsm_allow_portal_validate_move_qty')
