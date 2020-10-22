b31b3855c1192c3ad379e5655da3f16b3778e64d
scampbell <scampbell@opensourceintegrators.com>
2020-10-22 10:42:28 -0700

    [IMP] Do Not Repeat Job on Same Day

---
+++ connector_voicent_helpdesk_ticket/models/queue_job.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, models
+import time
+
+
+class QueueJob(models.Model):
+    _inherit = 'queue.job'
+
+    @api.model
+    def create(self, vals):
+        found = False
+        if vals.get('model_name') == 'helpdesk.ticket' and vals.get('name') == 'helpdesk.ticket.voicent_start_campaign':
+            record_ids = vals.get('record_ids')
+            job_ids = self.env['queue.job'].\
+                search([('model_name', '=', 'helpdesk.ticket'),
+                        ('name', '=', 'helpdesk.ticket.voicent_start_campaign'),
+                        ('date_created', '>', time.strftime('%Y-%m-%d 00:00:00'))])
+            for job_id in job_ids:
+                if record_ids == job_id.record_ids:
+                    found = True
+        if not found:
+            return super().create(vals)
