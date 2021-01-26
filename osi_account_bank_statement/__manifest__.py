d50be0a00e94681f13e6302f4d963184756d6f80
Daniel Reis <dreis@opensourceintegrators.com>
2021-01-26 09:50:59 +0000

    [IMP] osi_account_bank_statement: black, isort, prettier

--- osi_account_bank_statement/__manifest__.py
+++ osi_account_bank_statement/__manifest__.py
@@ -1,18 +1,16 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Bank Reconciliation Extended',
-    'version': '12.0.1.0.1',
-    'license': 'LGPL-3',
-    'author': 'Open Source Integrators',
-    'summary': 'Bank Reconciliation Entries restricted to specific accounts',
-    'category': 'Accounting & Finance',
-    'maintainer': 'Open Source Integrators',
-    'website': 'http://www.opensourceintegrators.com',
-    'depends': [
-        'account_payment_credit_card',
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['mgosai'],
+    "name": "Account Bank Reconciliation Extended",
+    "version": "13.0.1.0.1",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators",
+    "summary": "Bank Reconciliation Entries restricted to specific accounts",
+    "category": "Accounting & Finance",
+    "maintainer": "Open Source Integrators",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["account_payment_credit_card"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["mgosai"],
 }
