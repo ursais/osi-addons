7a6edc8844b52a78a99680e3f0ccaf39a5d18ffa
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-01-28 17:44:39 -0600

    [ADD] osi_fifo_serialized_fix

---
+++ osi_fifo_serialized_fix/models/stock_move.py
@@ -0,0 +1,93 @@
+# Copyright (C) 2021, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _action_done(self, cancel_backorder=False):
+        res = super()._action_done(cancel_backorder)
+        for move in res:
+            if (
+                move.product_id.tracking in ["serial", "lot"]
+                and move.picking_id.picking_type_id.code == "outgoing"
+            ):
+                svl_ids = []
+                test_vals = self.env["stock.valuation.layer"].search(
+                    [("product_id", "=", move.product_id.id)]
+                )
+                for line_id in move.move_line_ids:
+                    for valuation in test_vals:
+                        if line_id.lot_id in valuation.lot_ids and valuation.value > 0:
+                            if not self.check_found_vals(valuation.id, svl_ids):
+                                svl_ids.append((valuation.id, 1, [line_id.lot_id.id]))
+                                break
+                            else:
+                                self.increment_qty(
+                                    valuation.id, svl_ids, line_id.lot_id.id
+                                )
+                if len(svl_ids) > 1:
+                    final_layers = []
+                    for layer in move.stock_valuation_layer_ids:
+                        layer.sudo().unlink()
+                    for svl_id in svl_ids:
+                        val = move.product_id._prepare_out_svl_vals(
+                            svl_id[1], self.env.user.company_id
+                        )
+                        val_obj = self.env["stock.valuation.layer"].browse(svl_id[0])
+                        val["unit_cost"] = val_obj.unit_cost
+                        val["value"] = val_obj.unit_cost * svl_id[1] * -1
+                        val["company_id"] = self.env.user.company_id.id
+                        val["lot_ids"] = [(6, 0, svl_id[2])]
+                        final_layers.append(val)
+                    final_layers = self.env["stock.valuation.layer"].create(
+                        final_layers
+                    )
+                    move.write(
+                        {"stock_valuation_layer_ids": [(6, 0, final_layers.ids)]}
+                    )
+                else:
+                    valuation_layer = self.env["stock.valuation.layer"].browse(
+                        svl_ids[0][0]
+                    )
+                    if len(move.stock_valuation_layer_ids.ids) > 1:
+                        val = move.stock_valuation_layer_ids[0]
+                        val.unit_cost = -1 * valuation_layer.unit_cost
+                        for layer in move.stock_valuation_layer_ids:
+                            layer.sudo().unlink()
+                        move.write({"stock_valuation_layer_ids": [6, 0, val.id]})
+                    else:
+                        move.stock_valuation_layer_ids.unit_cost = (
+                            -1 * valuation_layer.unit_cost
+                        )
+                        move.stock_valuation_layer_ids.value = (
+                            -1
+                            * move.stock_valuation_layer_ids.quantity
+                            * move.stock_valuation_layer_ids.unit_cost
+                        )
+        return res
+
+    def increment_qty(self, id, svl_ids, lot_id):
+        index = 0
+        for svl_id in svl_ids:
+            if svl_id[0] == id:
+                if lot_id not in svl_id[2]:
+                    svl_id[2].append(lot_id)
+                svl_ids[index] = (svl_id[0], svl_id[1] + 1, svl_id[2])
+            index += 1
+
+    def check_found_vals(self, id, svl_ids):
+        for svl_id in svl_ids:
+            if id == svl_id[0]:
+                return True
+        return False
+
+    def _create_in_svl(self, forced_quantity=None):
+        res = super()._create_in_svl(forced_quantity)
+        for move in self:
+            for layer in res:
+                if layer.stock_move_id.id == move.id:
+                    layer.lot_ids = [(6, 0, move.lot_ids.ids)]
+        return res
