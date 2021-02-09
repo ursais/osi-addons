37b2a3c2ecbd2929b946669e6edd78ce7a2ee424
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2021-02-09 13:27:17 +0530

    [MIG] Module migrated in v14.

--- sale_subscription_openshift/__manifest__.py
+++ sale_subscription_openshift/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Sale Subscription Openshift",
     "summary": """Sale Subscription Openshift""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "Open Source Integrators",
     "website": "http://www.opensourceintegrators.com",
