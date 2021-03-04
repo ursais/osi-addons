908b6845ba8e85cdd4b9d4e7fc2e8ce447e8ce00
Sandip Mangukiya <smangukiya@opensourceintegrators.com>
2021-03-04 11:49:40 -0700

    [IMP][12.0]fieldservice_mobile: portal user access to update qty from app

--- fieldservice_mobile/__manifest__.py
+++ fieldservice_mobile/__manifest__.py
@@ -10,7 +10,7 @@
     "category": "Field Service",
     "author": "Open Source Integrators",
     "website": "https://github.com/ursais/osi-addons",
-    "depends": ["fieldservice_stage_server_action"],
+    "depends": ["fieldservice_stage_server_action", "stock"],
     "data": [
         "security/ir_rule.xml",
         "security/ir.model.access.csv",
