898758b5a5ab31b2bf837594001d3c57f784011a
AmmarOSI <aofficewala@opensourceintegrators.com>
2021-04-20 16:33:11 +0530

    [FIX] Fixed a travis.

--- fieldservice_mobile/models/fsm_stage_history.py
+++ fieldservice_mobile/models/fsm_stage_history.py
@@ -12,5 +12,9 @@ class FsmStageHistory(models.Model):
     order_id = fields.Many2one("fsm.order", string="FSM Order")
     start_datetime = fields.Datetime("Start Date&time")
     stage_id = fields.Many2one("fsm.stage", string="Stage")
-    duration = fields.Float(string="Duration",)
-    total_duration = fields.Float(string="Total Duration",)
+    duration = fields.Float(
+        string="Duration",
+    )
+    total_duration = fields.Float(
+        string="Total Duration",
+    )
