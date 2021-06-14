cbd59cd1d1bb18fd0e4e802768bf4c7f973a4285
Freni-OSI <fpatel@opensourceintegrators.com>
2021-06-14 19:12:52 +0530

    [14.0][Fix] Travis

--- sale_subscription_brand/models/account_invoice.py
+++ sale_subscription_brand/models/account_invoice.py
@@ -1,9 +1,8 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import models
 
 
 class AccountInvoice(models.Model):
-        _inherit = 'account.invoice'
-        
+    _inherit = "account.invoice"
--- sale_subscription_brand/models/sale_subscription.py
+++ sale_subscription_brand/models/sale_subscription.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class SaleSubscription(models.Model):
-    _inherit = 'sale.subscription'
+    _inherit = "sale.subscription"
 
-    brand = fields.Many2one('res.partner', 'Brand',
-                            domain="[('type', '=', 'brand')]")
+    brand = fields.Many2one("res.partner", "Brand", domain="[('type', '=', 'brand')]")
