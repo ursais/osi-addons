1b1750d04c50458b31c289e83dae33f2cb285399
Freni-OSI <fpatel@opensourceintegrators.com>
2021-06-29 13:21:03 +0530

    [14.0][FIX] Travis

--- helpdesk_operating_unit/models/helpdesk_ticket.py
+++ helpdesk_operating_unit/models/helpdesk_ticket.py
@@ -5,11 +5,12 @@ from odoo import fields, models
 
 
 class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+    _inherit = "helpdesk.ticket"
 
     operating_unit_id = fields.Many2one(
-        comodel_name='operating.unit',
-        string='Operating Unit',
-        default=lambda self: self.env['res.users'].operating_unit_default_get(
-            self._uid)
+        comodel_name="operating.unit",
+        string="Operating Unit",
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
     )
