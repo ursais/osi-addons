331ba5e231438c183e41634a7d9b2f0c7ccb15aa
Freni-OSI <fpatel@opensourceintegrators.com>
2021-07-01 16:52:06 +0530

    [14.0][FIX] Travis

--- helpdesk_timesheet_fieldservice/__manifest__.py
+++ helpdesk_timesheet_fieldservice/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2020 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Helpdesk Timesheet - Field Service',
-    'summary': 'Create FSM Orders and Timesheet from Helpdesk Ticket',
-    'version': '12.0.1.1.0',
-    'license': 'LGPL-3',
-    'author': 'Open Source Integrators',
-    'category': 'Helpdesk',
-    'website': 'https://github.com/ursais/osi-addons',
-    'depends': [
-        'helpdesk_timesheet',
-        'helpdesk_fieldservice',
+    "name": "Helpdesk Timesheet - Field Service",
+    "summary": "Create FSM Orders and Timesheet from Helpdesk Ticket",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators",
+    "category": "Helpdesk",
+    "website": "https://github.com/ursais/osi-addons",
+    "depends": [
+        "helpdesk_timesheet",
+        "helpdesk_fieldservice",
     ],
-    'data': [
-        'views/helpdesk_ticket.xml',
+    "data": [
+        "views/helpdesk_ticket.xml",
     ],
-    'auto_install': True,
+    "auto_install": True,
 }
