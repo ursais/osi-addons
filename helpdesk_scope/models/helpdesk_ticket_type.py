24cff23322a338dce9f34e95b26f78d48713b314
Freni-OSI <fpatel@opensourceintegrators.com>
2021-07-01 18:06:07 +0530

    [14.0][MIG] helpdesk_scope

--- helpdesk_scope/models/helpdesk_ticket_type.py
+++ helpdesk_scope/models/helpdesk_ticket_type.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
 
 class HelpdeskTicketType(models.Model):
-    _inherit = 'helpdesk.ticket.type'
+    _inherit = "helpdesk.ticket.type"
 
-    scope_ids = fields.Many2many('helpdesk.scope', string="Scopes")
+    scope_ids = fields.Many2many("helpdesk.scope", string="Scopes")
