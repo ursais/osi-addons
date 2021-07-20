e68e597a87fc382c693ceff162c603f73c092b14
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-20 17:50:46 +0530

    [14.0][MIG]connector_voicent_helpdesk_ticket: Travis

--- connector_voicent_helpdesk_ticket/models/backend_voicent_call_line.py
+++ connector_voicent_helpdesk_ticket/models/backend_voicent_call_line.py
@@ -6,14 +6,16 @@ from odoo import fields, models
 
 
 class BackendVoicentCallLine(models.Model):
-    _inherit = 'backend.voicent.call.line'
+    _inherit = "backend.voicent.call.line"
 
     applies_on = fields.Selection(
-        selection_add=[('helpdesk.ticket', 'Helpdesk Ticket')])
+        selection_add=[("helpdesk.ticket", "Helpdesk Ticket")]
+    )
     helpdesk_ticket_stage_id = fields.Many2one(
-        'helpdesk.stage',
-        string='Helpdesk Ticket Stage')
+        "helpdesk.stage", string="Helpdesk Ticket Stage"
+    )
     has_parent = fields.Boolean(
-        string='Must Have a Parent',
+        string="Must Have a Parent",
         help="""Determine if the call is made based on whether the parent of
-        the ticket is set or not""")
+        the ticket is set or not""",
+    )
