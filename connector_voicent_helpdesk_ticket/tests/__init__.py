faa34c135e7b710739f26822f6615c9f14e8fb27
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-22 13:08:37 +0530

    [14.0][MIG]connector_voicent_helpdesk_ticket: migrated to v14

--- connector_voicent_helpdesk_ticket/tests/__init__.py
+++ connector_voicent_helpdesk_ticket/tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # <https://www.opensourceintegrators.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
