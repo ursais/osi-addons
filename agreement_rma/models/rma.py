13ff2d2e54aa02f6477627345c55842b070dc798
HemanginiPatel <h.patel.serpentcs@gmail.com>
2021-07-23 17:40:07 +0530

    [MIG]Migrated the agreement_rma in v14.

--- agreement_rma/models/rma.py
+++ agreement_rma/models/rma.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -7,14 +7,13 @@ from odoo import api, fields, models
 class RMAOrderLine(models.Model):
     _inherit = "rma.order.line"
 
-    agreement_id = fields.Many2one('agreement', 'Agreement')
-    serviceprofile_id = fields.Many2one('agreement.serviceprofile',
-                                        'Service Profile')
+    agreement_id = fields.Many2one("agreement", "Agreement")
+    serviceprofile_id = fields.Many2one("agreement.serviceprofile", "Service Profile")
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def _onchange_partner(self):
         self.agreement_id = False
 
-    @api.onchange('agreement_id')
+    @api.onchange("agreement_id")
     def _onchange_agreement(self):
         self.serviceprofile_id = False
