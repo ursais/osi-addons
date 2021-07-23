b35aaaf34785777ebf1984119c7ab9f969319acb
HemanginiPatel <h.patel.serpentcs@gmail.com>
2021-07-23 19:06:27 +0530

    [MIG]Migrated the module helpdesk_resolution in v14.

--- helpdesk_resolution/models/helpdesk_ticket.py
+++ helpdesk_resolution/models/helpdesk_ticket.py
@@ -1,11 +1,12 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
 
 class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+    _inherit = "helpdesk.ticket"
 
-    resolution = fields.Text(string="Resolution",
-                             placeholder="Resolution of the ticket...")
+    resolution = fields.Text(
+        string="Resolution", placeholder="Resolution of the ticket..."
+    )
