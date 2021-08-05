717d723912c1532bbb31988ad3e77874f35cb2aa
HemanginiPatel <h.patel.serpentcs@gmail.com>
2021-08-05 19:13:38 +0530

    [MIG]Migrated the osi_analytic_segments_purchase_subscription in v14.

--- osi_analytic_segments_purchase_subscription/models/__init__.py
+++ osi_analytic_segments_purchase_subscription/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from . import (
     purchase_subscription,
--- osi_analytic_segments_purchase_subscription/models/purchase_subscription.py
+++ osi_analytic_segments_purchase_subscription/models/purchase_subscription.py
@@ -1,30 +1,30 @@
-# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PurchaseSubscription(models.Model):
-    _inherit = 'purchase.subscription'
+    _inherit = "purchase.subscription"
 
     def _get_default_segment_one(self):
         return self.env["analytic.segment.one"].get_default_segment_one()
 
     analytic_segment_one_id = fields.Many2one(
-        'analytic.segment.one',
-        string='Analytic Segment One',
+        "analytic.segment.one",
+        string="Analytic Segment One",
         default=_get_default_segment_one,
-        copy=False)
+        copy=False,
+    )
     analytic_segment_two_id = fields.Many2one(
-        'analytic.segment.two',
-        string='Analytic Segment Two',
-        copy=False)
+        "analytic.segment.two", string="Analytic Segment Two", copy=False
+    )
 
-    @api.multi
     def _prepare_invoice_line(self, line, fiscal_position):
         res = super()._prepare_invoice_line(line, fiscal_position)
-        res.update({
-            'analytic_segment_one_id':
-                line.analytic_segment_one_id.id,
-            'analytic_segment_two_id':
-                line.analytic_segment_two_id.id})
+        res.update(
+            {
+                "analytic_segment_one_id": line.analytic_segment_one_id.id,
+                "analytic_segment_two_id": line.analytic_segment_two_id.id,
+            }
+        )
         return res
