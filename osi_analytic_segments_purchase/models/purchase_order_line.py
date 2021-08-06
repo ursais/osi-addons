3c6c385ad70dec21ad88008de6a39774ade8e11e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-08-06 18:05:33 -0500

    [FIX] osi_analytic_segments_purchase

--- osi_analytic_segments_purchase/models/purchase_order_line.py
+++ osi_analytic_segments_purchase/models/purchase_order_line.py
@@ -4,24 +4,27 @@ from odoo import fields, models
 
 
 class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
+    _inherit = "purchase.order.line"
 
     def _get_default_segment_one(self):
         return self.env["analytic.segment.one"].get_default_segment_one()
 
     analytic_segment_one_id = fields.Many2one(
-        'analytic.segment.one',
-        string='Analytic Segment One',
+        "analytic.segment.one",
+        string="Analytic Segment One",
         default=_get_default_segment_one,
-        copy=False)
+        copy=False,
+    )
     analytic_segment_two_id = fields.Many2one(
-        'analytic.segment.two',
-        string='Analytic Segment Two',
-        copy=False)
+        "analytic.segment.two", string="Analytic Segment Two", copy=False
+    )
 
     def _prepare_account_move_line(self, move=False):
         res = super()._prepare_account_move_line(move)
-        res.update({
-            'analytic_segment_one_id': self.analytic_segment_one_id.id,
-            'analytic_segment_two_id': self.analytic_segment_two_id.id})
+        res.update(
+            {
+                "analytic_segment_one_id": self.analytic_segment_one_id.id,
+                "analytic_segment_two_id": self.analytic_segment_two_id.id,
+            }
+        )
         return res
