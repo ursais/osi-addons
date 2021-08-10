5b8a1f8fa67f870557ebb4bf0ada6f8dd7f54621
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-08-10 16:08:43 +0530

    [14.0][MIG]agreement_sale_subscription: migrated into v14

--- agreement_sale_subscription/__manifest__.py
+++ agreement_sale_subscription/__manifest__.py
@@ -1,21 +1,16 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Agreement - Sales Subscription',
-    'summary': 'Link your sales subscriptions to an agreement',
-    'version': '12.0.1.0.1',
-    'license': 'LGPL-3',
-    'author': 'Open Source Integrators',
-    'category': 'Agreement',
-    'website': 'https://github.com/ursais/osi-addons',
-    'depends': [
-        'agreement_legal_sale',
-        'sale_subscription'
-    ],
-    'data': [
-        'views/agreement.xml',
-        'views/subscription.xml'
-    ],
-    'installable': True,
+    "name": "Agreement - Sales Subscription",
+    "summary": "Link your sales subscriptions to an agreement",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators",
+    "category": "Agreement",
+    "website": "https://github.com/ursais/osi-addons",
+    "depends": ["agreement_legal_sale", "sale_subscription"],
+    "data": ["views/agreement.xml", "views/subscription.xml"],
+    "installable": True,
+    "maintainers": ["Open Source Integrators"],
 }
--- agreement_sale_subscription/models/agreement.py
+++ agreement_sale_subscription/models/agreement.py
@@ -1,11 +1,10 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
 
 class Agreement(models.Model):
-    _inherit = 'agreement'
+    _inherit = "agreement"
 
-    subscription_id = fields.Many2one('sale.subscription',
-                                      string="Subscription")
+    subscription_id = fields.Many2one("sale.subscription", string="Subscription")
