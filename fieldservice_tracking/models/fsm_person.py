4d231bc421e77a83b4c48275db89b2b642de3b08
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2021-09-07 13:31:42 +0530

    [14.0][MIG] fieldservice_tracking

---
+++ fieldservice_tracking/models/fsm_person.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2021 Open Source Integrators
+# Copyright (C) 2021 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class FsmPerson(models.Model):
+    _inherit = "fsm.person"
+
+    worker_allow_tracking = fields.Boolean("Worker Allow Tracking")
+    track_lat = fields.Float("Last known latitude", digits=(16, 5))
+    track_long = fields.Float("Last known longitude", digits=(16, 5))
+    # track_shape = fields.Char('Last known location')
+    loc_rad = fields.Float("Last known location accuracy radius", digits=(16, 5))
+    loc_time = fields.Datetime("Last location ping")
+    rad_uom = fields.Many2one("uom.uom", "Unit of measure")
+
+    # only visible to dispatchers
+    match_rad = fields.Integer("Max allowed distance")
+    track_intv_coarse = fields.Integer("Time in seconds")
+    fence_rad = fields.Integer("Distance to Destination")
+    route_stage = fields.Selection(
+        [
+            ("out", "Out"),
+            ("checkin", "Check-in"),
+            ("enroute", "Enroute"),
+            ("jobstart", "Job Start"),
+            ("working", "Working"),
+            ("jobstop", "Job Stop"),
+            ("checkout", "Check-out"),
+            ("break", "Break"),
+        ],
+        string="Route Stage",
+    )
