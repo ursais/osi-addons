6d186e0dcd19074d4d227f367f0b1329c916a02e
Balaji Kannan <bkannan@ursainfosystems.com>
2021-10-11 19:32:25 -0700

    [FIX] Account Bank Statement Report corrected to include additional fields [Layout change to landscape is needed]

--- account_bank_statement_extend/report/account_bank_statement.py
+++ account_bank_statement_extend/report/account_bank_statement.py
@@ -8,148 +8,99 @@ from odoo.tools.misc import format_date
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
-    def get_base_reconcile_entries(self):
+    def get_statement_entries(self):
         lines_ids_journal = []
-        for move_line in self.line_ids:
-            rl = []
-            for lines in move_line:
-                lines = lines.move_id.line_ids
-                if move_line.amount < 0.0:
-                    if lines.full_reconcile_id:
-                        move_line = self.env["account.move.line"].search(
-                            [
-                                ("full_reconcile_id", "=", lines.full_reconcile_id.id,),
-                                ("debit", "=", 0.0),
-                            ]
-                        )
-                        for move in move_line:
-                            move_line = self.env["account.move.line"].browse(
-                                move_line._reconciled_lines()
-                            )
+        rl = []
+        for stmt_line in self.line_ids:
+            reconcile_ids = stmt_line.move_id.line_ids.full_reconcile_id
+            if not stmt_line.move_id.line_ids.full_reconcile_id:
+                rl.append(
+                    {
+                        "date": format_date(self.env, stmt_line.date),
+                        "account_id": "      ",
+                        "partner_id": stmt_line.partner_id.name,
+                        "partner_bank_id": stmt_line.partner_bank_id,
+                        "statement_id": stmt_line.name,
+                        "move_id": "      ",
+                        "ref": stmt_line.payment_ref,
+                        "note": stmt_line.narration,
+                        "matching_number": "      ",
+                        "debit": stmt_line.amount > 0 and abs(stmt_line.amount) or 0.0,
+                        "credit": stmt_line.amount < 0 and abs(stmt_line.amount) or 0.0,
+                    }
+                )
+                continue
+            else:
+                suspense_line = self.env["account.move.line"].search(
+                    [
+                        ("full_reconcile_id", "=", reconcile_ids.id),
+                        ("id", "not in", stmt_line.move_id.line_ids.ids),
+                    ]
+                )
+                payment_line = suspense_line.move_id.line_ids.filtered(
+                    lambda l: l.id != suspense_line.id
+                    and l.account_id.internal_type in ("receivable", "payable")
+                )
 
-                            for line in move_line:
-                                lines = line.move_id.line_ids._reconciled_lines()
-                                line_rec = self.env["account.move.line"].browse(lines)
-                                for l in line_rec:
-                                    if (
-                                        l.account_id.user_type_id.type
-                                        in ["payable", "receivable"]
-                                        and l.move_id.move_type
-                                        in ["in_invoice", "out_invoice", "entry"]
-                                        and len(rl) < 1
-                                    ):
-                                        bank_state_dict = self.get_bank_statement_lines_dict(
-                                            l,
-                                            l.account_id.user_type_id.type,
-                                            lines_ids_journal,
-                                            b_state,
-                                        )
-                                        if bank_state_dict:
-                                            rl.append(bank_state_dict)
-                                            lines_ids_journal.append(l.id)
-                                        rl.append(
-                                            {
-                                                "date": format_date(self.env, l.date),
-                                                "account_id": l.account_id.name,
-                                                "partner_id": l.partner_id.name,
-                                                "statement_id": self.name,
-                                                "move_id": l.move_id.name,
-                                                "ref": l.ref,
-                                                "matching_number": l.full_reconcile_id.name,
-                                                "debit": l.debit,
-                                                "credit": l.credit,
-                                            }
-                                        )
-                                    else:
-                                        lines = l.move_id.line_ids._reconciled_lines()
-                                        line_rec = self.env["account.move.line"].browse(
-                                            lines
-                                        )
-                                        for line in line_rec:
-                                            if (
-                                                line.account_id.user_type_id.type
-                                                in ["payable", "receivable",]
-                                                and line.move_id.move_type
-                                                in ["in_invoice", "out_invoice", "entry"]
-                                                and len(rl) < 1
-                                            ):
-                                                rl.append(
-                                                    {
-                                                        "date": format_date(
-                                                            self.env, l.date
-                                                        ),
-                                                        "account_id": line.account_id.name,
-                                                        "partner_id": line.partner_id.name,
-                                                        "statement_id": self.name,
-                                                        "move_id": line.move_id.name,
-                                                        "ref": line.ref,
-                                                        "matching_number": line.full_reconcile_id.name,
-                                                        "debit": line.debit,
-                                                        "credit": line.credit,
-                                                    }
-                                                )
-                else:
-                    if lines.full_reconcile_id:
-                        move_line = self.env["account.move.line"].search(
-                            [
-                                ("full_reconcile_id", "=", lines.full_reconcile_id.id,),
-                                ("debit", "!=", 0.0),
-                            ]
-                        )
-                        for move in move_line:
-                            move_line = self.env["account.move.line"].browse(
-                                move_line._reconciled_lines()
-                            )
-                            for line in move_line:
-                                lines = line.move_id.line_ids._reconciled_lines()
-                                line_rec = self.env["account.move.line"].browse(lines)
-                                for l in line_rec:
-                                    if (
-                                        l.account_id.user_type_id.type
-                                        in ["payable", "receivable",]
-                                        and l.move_id.move_type
-                                        in ["in_invoice", "out_invoice", "entry"]
-                                        and len(rl) < 1
-                                    ):
-                                        rl.append(
-                                            {
-                                                "date": format_date(self.env, l.date),
-                                                "account_id": l.account_id.name,
-                                                "partner_id": l.partner_id.name,
-                                                "statement_id": self.name,
-                                                "move_id": l.move_id.name,
-                                                "ref": l.ref,
-                                                "matching_number": l.full_reconcile_id.name,
-                                                "debit": l.debit,
-                                                "credit": l.credit,
-                                            }
-                                        )
-                                    else:
-                                        lines = l.move_id.line_ids._reconciled_lines()
-                                        line_rec = self.env["account.move.line"].browse(
-                                            lines
-                                        )
-                                        for line in line_rec:
-                                            if (
-                                                line.account_id.user_type_id.type
-                                                in ["payable", "receivable",]
-                                                and line.move_id.move_type
-                                                in ["in_invoice", "out_invoice", "entry"]
-                                                and len(rl) < 1
-                                            ):
-                                                rl.append(
-                                                    {
-                                                        "date": format_date(
-                                                            self.env, l.date
-                                                        ),
-                                                        "account_id": line.account_id.name,
-                                                        "partner_id": line.partner_id.name,
-                                                        "statement_id": self.name,
-                                                        "move_id": line.move_id.name,
-                                                        "ref": line.ref,
-                                                        "matching_number": line.full_reconcile_id.name,
-                                                        "debit": line.debit,
-                                                        "credit": line.credit,
-                                                    }
-                                                )
+                for payment in payment_line:
+                    rl.append(
+                        {
+                            "date": format_date(self.env, stmt_line.date),
+                            "account_id": payment.account_id.name,
+                            "partner_id": payment.partner_id.name,
+                            "partner_bank_id": "      ",
+                            "statement_id": stmt_line.name,
+                            "move_id": payment.move_id.name,
+                            "ref": payment.ref,
+                            "note": "      ",
+                            "matching_number": payment.full_reconcile_id.name,
+                            "debit": stmt_line.amount > 0
+                            and stmt_line.amount
+                            or payment.debit,
+                            "credit": stmt_line.amount < 0
+                            and stmt_line.amount
+                            or payment.credit,
+                        }
+                    )
+
+                outstanding_line = suspense_line.move_id.line_ids.filtered(
+                    lambda l: l.id != suspense_line.id
+                    and l.account_id.internal_type not in ("receivable", "payable")
+                )
+                reconcile_ids = outstanding_line.full_reconcile_id
+                partner_line = self.env["account.move.line"].search(
+                    [
+                        ("full_reconcile_id", "=", reconcile_ids.id),
+                        ("id", "not in", outstanding_line.ids),
+                    ]
+                )
+                payment_line = partner_line.move_id.line_ids.filtered(
+                    lambda l: l.id != partner_line.id
+                    and l.account_id.internal_type in ("receivable", "payable")
+                )
+
+                for payment in payment_line:
+                    rl.append(
+                        {
+                            "date": format_date(self.env, stmt_line.date),
+                            "account_id": payment.account_id.name,
+                            "partner_id": payment.partner_id.name,
+                            "partner_bank_id": "      ",
+                            "statement_id": stmt_line.name,
+                            "move_id": payment.move_id.name,
+                            "ref": payment.ref,
+                            "note": "      ",
+                            "matching_number": payment.full_reconcile_id.name,
+                            "debit": stmt_line.amount > 0
+                            and stmt_line.amount
+                            or payment.debit,
+                            "credit": stmt_line.amount < 0
+                            and stmt_line.amount
+                            or payment.credit,
+                        }
+                    )
+                outstanding_line = outstanding_line.move_id.line_ids.filtered(
+                    lambda l: l.id != outstanding_line.id
+                    and l.account_id.internal_type not in ("receivable", "payable")
+                )
         return rl
