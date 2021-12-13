74a1dfdfdd88b4a459b8665c350d2e77a261dcf2
Vandan-OSI <vpandeji@opensourceintegrators.com>
2021-12-13 15:41:33 +0530

    IMP Sale order onchange.

--- osi_multiple_salespersons/models/sale_order.py
+++ osi_multiple_salespersons/models/sale_order.py
@@ -9,11 +9,16 @@ class SaleOrder(models.Model):
     @api.onchange("partner_id")
     def onchange_partner_id(self):
         res = super().onchange_partner_id()
-        self.sale_user_ids = (
-            self.partner_id.sale_user_ids.ids
-            or self.partner_id.commercial_partner_id.sale_user_ids.ids
-            or False
-        )
+        if self.env.context.get(
+            "default_partner_id"
+        ) == self.partner_id.id and self.env.context.get("default_sale_user_ids"):
+            self.sale_user_ids = self.env.context.get("default_sale_user_ids")
+        else:
+            self.sale_user_ids = (
+                self.partner_id.sale_user_ids.ids
+                or self.partner_id.commercial_partner_id.sale_user_ids.ids
+                or False
+            )
         return res
 
     def _prepare_invoice(self):
