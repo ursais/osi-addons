f6358794ba3a7a7b4adee4fb362377a3a5739879
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-12-20 15:55:51 +0530

    [14.0][FIX] Procument Group error.

--- osi_helpdesk_stock/models/stock_request_order.py
+++ osi_helpdesk_stock/models/stock_request_order.py
@@ -44,7 +44,8 @@ class StockRequestOrder(models.Model):
     def action_confirm(self):
         if self.helpdesk_ticket_id and not self.procurement_group_id:
             group = self.env["procurement.group"].search(
-                [("helpdesk_ticket_id", "=", self.helpdesk_ticket_id.id)]
+                [("helpdesk_ticket_id", "=", self.helpdesk_ticket_id.id)],
+                limit=1, order="id desc"
             )
             if not group:
                 values = self._prepare_procurement_group_values_helpdesk()
