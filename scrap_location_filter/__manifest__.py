0d7446122265e544bcf8bd7b7e57eb65518569de
Freni Patel <fpatel@opensourceintegrators.com>
2022-02-21 18:24:10 +0530

    [ADD] scrap_location_filter

---
+++ scrap_location_filter/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Scrap Location Filter",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "description": """
+    Filters scrap location
+ """,
+    "author": "Open Source Integrators",
+    "website": "https://github.com/ursais/osi-addons",
+    "data": ["views/stock_views.xml"],
+    "installable": True,
+    "application": False,
+}
