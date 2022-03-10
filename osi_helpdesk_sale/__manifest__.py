e8ac87e16d89d2a2452d6527c5ba86f4b91a533e
Freni Patel <fpatel@opensourceintegrators.com>
2022-03-10 21:04:43 +0530

    [Fix] osi_helpdesk_sale

--- osi_helpdesk_sale/__manifest__.py
+++ osi_helpdesk_sale/__manifest__.py
@@ -14,6 +14,7 @@
         "helpdesk_sale",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "views/helpdesk_team_view.xml",
         "views/helpdesk_ticket_view.xml",
         "views/sale_order_view.xml",
