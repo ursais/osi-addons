dcd7849dd661b325480cd5ee4eec4619108a9c4d
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2022-04-25 16:46:53 -0500

    fix

--- purchase_budget_validation/models/purchase_order.py
+++ purchase_budget_validation/models/purchase_order.py
@@ -18,19 +18,20 @@ class PurchaseOrder(models.Model):
 
     def check_budget(self, notify=False):
         message = ""
+        users = None
         over_budget = False
         for po_line in self.order_line:
             budget_lines = po_line._get_budget_lines()
             for b_line in budget_lines:
                 if b_line.over_budget:
+                    users += b_line.crossovered_budget_id.user_id
                     message += _(
-                        "<br/>This order is over budget."
-                        "<br/>Budget Name: %s"
-                        "<br/>Budget Line: %s"
+                        "This order is over budget %s / %s (%s):"
                         "<br/>%s + %s + %s = %s > %s.<br/>"
                         % (
                             b_line.crossovered_budget_id.name,
                             b_line.name,
+                            b_line.crossovered_budget_id.user_id.name,
                             abs(b_line.practical_amount),
                             abs(b_line.committed_amount),
                             abs(b_line.uncommitted_amount),
@@ -43,10 +44,8 @@ class PurchaseOrder(models.Model):
         if message:
             over_budget = True
         if notify:
-            group = self.env.ref("purchase.group_purchase_manager")
-            po_user = self.env["res.users"].search([("groups_id", "in", group.ids)])
-            if po_user:
-                self.message_subscribe(partner_ids=po_user.partner_id.ids)
+            for user in users:
+                self.message_subscribe(partner_ids=user.partner_id.ids)
             self.message_post(body=message)
         return over_budget
 
