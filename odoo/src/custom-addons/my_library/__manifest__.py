cb15b8fde6a5f73fc01994342f519a0b70c3aeed
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-19 16:37:12 -0700

    [UDPATe] changed manifest file

---
+++ odoo/src/custom-addons/my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    
+'name':'My Library',
+'summary':'Manage books easily',
+'description' : """Manage Library 
+=======================================
+Description related to library
+""",
+'author ' : 'Richard',
+'website' : 'richard.ursasys.com',
+'category' : 'Uncategorized',
+'version':'15.0',
+'depends' : ['base'],
+# 'data':['views/views.xml'],
+# 'demo' : ['demo.xml']
+}
