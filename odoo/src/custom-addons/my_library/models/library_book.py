68889c30ae312177178b615dabfcc269fafd60c2
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-20 19:47:37 -0700

    [FIX] fixed casing error

--- odoo/src/custom-addons/my_library/models/library_book.py
+++ odoo/src/custom-addons/my_library/models/library_book.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = "library.book"
-    name = fields.char("Title", required=True)
+    name = fields.Char("Title", required=True)
     date_release = fields.Date("Release Date")
     author_ids = fields.Many2many(
         "res,partner",
