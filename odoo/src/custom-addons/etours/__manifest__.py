02b45b37a9ca3b6df84681888469789ead5a4c9e
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-22 12:30:33 -0700

    [ADD] added cost price to model and view

--- odoo/src/custom-addons/etours/__manifest__.py
+++ odoo/src/custom-addons/etours/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "etours",
+    'name': "Etours",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Bakersophic Solutions",
+    'website': "http://www.bakersophicsolutions.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
--- odoo/src/custom-addons/my_library/models/library_book.py
+++ odoo/src/custom-addons/my_library/models/library_book.py
@@ -3,8 +3,34 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
-    name = fields.Char('Title', required=True)
+    _description = 'Library book'
+
+    # sorts the books from oldest
+    _order = 'date_release desc, name'
+
+    # to use the shortname field as the record representation
+    _rec_name = 'short_name'
+    short_name = fields.Char('Short Title', required=True)
+
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    cost_price = fields.Float(
+        'Book Cost', digits='Book Price')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision decimals,
+    )
+
+    name = fields.Char('Title', required=True)
     author_ids = fields.Many2many(
         'res,partner',
         string='Authors'
