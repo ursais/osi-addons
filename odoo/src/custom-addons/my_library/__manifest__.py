036c8af3dfbd8920bbb6479688138760ce607e51
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-22 09:33:41 -0700

    [UPDATE] updated files to add menus and views to module

--- odoo/src/custom-addons/my_library/__manifest__.py
+++ odoo/src/custom-addons/my_library/__manifest__.py
@@ -1,15 +1,15 @@
-{   
-'name':'My Library',
-'summary':'Manage books easily',
-'description' : """Manage Library 
+{
+    'name': 'My Library',
+    'summary': 'Manage books easily',
+    'description': """Manage Library
 =======================================
 Description related to library
 """,
-'author ' : 'Richard',
-'website' : 'richard.ursasys.com',
-'category' : 'Uncategorized',
-'version':'15.0',
-'depends' : ['base'],
-'data':['views/views.xml'],
-'demo' : ['demo.xml']
+    'author ': 'Richard',
+    'website': 'richard.ursasys.com',
+    'category': 'Uncategorized',
+    'version': '15.0',
+    'depends': ['base'],
+    'data': ['views/library_book.xml'],
+    'demo': ['demo.xml']
 }
