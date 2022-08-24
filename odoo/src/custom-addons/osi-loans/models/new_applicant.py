0620c1f842ce3a37d5041f8aeefc6f596f1ecdba
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-24 10:25:10 -0700

    [FIX] removed unused record

--- odoo/src/custom-addons/osi-loans/models/new_applicant.py
+++ odoo/src/custom-addons/osi-loans/models/new_applicant.py
@@ -19,14 +19,13 @@ class NewApplicant(models.Model):
         ('MS', 'MS'), ('MO', 'MO'), ('MT', 'MT'), ('NE', 'NE'), ('NV', 'NV'), ('NH', 'NH'), ('NJ', 'NJ'), ('NM', 'NM'),
         ('NY', 'NY'), ('NC', 'NC'), ('ND', 'ND'), ('OH', 'OH'), ('OK', 'OK'), ('OR', 'OR'), ('PA', 'PA'), ('RI', 'RI'),
         ('SC', 'SC'), ('SD', 'SD'), ('TN', 'TN'), ('TX', 'TX'), ('UT', 'UT'), ('VT', 'VT'), ('VA', 'VA'), ('WA', 'WA'),
-        ('WV', 'WV'), ('WI', 'WI'), ('WY', 'WY')
-    ], string="State")
+        ('WV', 'WV'), ('WI', 'WI'), ('WY', 'WY')], string="State")
 
     zip = fields.Integer('Zip code')
     ssn = fields.Integer('SSN #')
     employment_status = fields.Selection([('Employed', 'Employed'), ('Unemployed', 'Unemployed'),
-                                          ('Fixed', 'Fixed Income (SSI, Retirement Income)')
-                                          ], string='Employment Status')
+                                          ('Fixed', 'Fixed Income (SSI, Retirement Income)')],
+                                           string='Employment Status')
     monthly_income = fields.Integer('Monthly income')
     employer = fields.Char('Employer')
     employer_number = fields.Char('Employer contact number')
