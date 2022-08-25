0eed5f2ea72a4058bbf402f55baf486fe21f2916
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-25 15:38:42 -0700

    [FIX] fixed applicant view

--- odoo/src/custom-addons/osi-loans/models/new_application.py
+++ odoo/src/custom-addons/osi-loans/models/new_application.py
@@ -5,11 +5,11 @@ class NewApplication(models.Model):
     _name = 'new.application'
     _description = 'create a new loan application in the system'
 
-    applicant = fields.Many2one('new.applicant',string='Applicant')
-    loan_type = fields.Many2one('loan.options',string='Loan type')
+    applicant = fields.Many2one('new.applicant', 'email', string='Applicant')
+    loan_type = fields.Many2one('loan.options', 'name', string='Loan type')
 
     loan_term = fields.Selection([('2', '2 years'), ('5', '5 years'),
-                                  ('10', '10 years')],string='Loan term')
+                                  ('10', '10 years')], string='Loan term')
 
     state = fields.Selection([('Draft', 'Draft'), ('Submitted', 'Submitted'),
                               ('Reviewed', 'Reviewed')], required=True, default='Draft')
