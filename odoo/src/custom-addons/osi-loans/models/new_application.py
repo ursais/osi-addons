7a2df98ea933f5ab7badc6fbf54c738ed237a353
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-25 15:27:36 -0700

    [UPDATE]  updated new application view

--- odoo/src/custom-addons/osi-loans/models/new_application.py
+++ odoo/src/custom-addons/osi-loans/models/new_application.py
@@ -5,11 +5,11 @@ class NewApplication(models.Model):
     _name = 'new.application'
     _description = 'create a new loan application in the system'
 
-    applicant = fields.One2many(comodel_name='new.applicant', inverse_name='email')
-    loan_type = fields.One2many(comodel_name='loan.options', inverse_name='loan_name')
+    applicant = fields.Many2one('new.applicant',string='Applicant')
+    loan_type = fields.Many2one('loan.options',string='Loan type')
 
     loan_term = fields.Selection([('2', '2 years'), ('5', '5 years'),
-                                  ('10', '10 years')])
+                                  ('10', '10 years')],string='Loan term')
 
     state = fields.Selection([('Draft', 'Draft'), ('Submitted', 'Submitted'),
                               ('Reviewed', 'Reviewed')], required=True, default='Draft')
