86abb11fe7748dbfd176ae106d4ccd6e158aa525
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-29 13:35:49 -0700

    [ADD] added logic for approving and denying applications

--- odoo/src/custom-addons/osi-loans/models/new_application.py
+++ odoo/src/custom-addons/osi-loans/models/new_application.py
@@ -1,3 +1,5 @@
+import random
+
 from odoo import models, fields
 
 
@@ -16,13 +18,25 @@ class NewApplication(models.Model):
                                        ('rejected', 'Rejected')
                                        ], default='dft')
 
+    determined_status = fields.Char('Determination')
+
     def button_submit(self):
         self.progress_state = 'submit'
+
+        get_min_credit = fields.Many2one('loan.options')
+        # simulate using credit API to get credit score
+        applicant_credit = random.randint(300, 850)
+
+        # get credit requirements from loan options model
+        credit_score = fields.Integer(related='get_min_credit.minimum_credit')
+
+        if credit_score < applicant_credit:
+            determined_status = "Approved"
+        else:
+            determined_status = "Rejected"
 
     def button_approve(self):
         self.progress_state = 'approved'
 
     def button_reject(self):
         self.progress_state = 'rejected'
-
-
