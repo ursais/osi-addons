5bf47ebd4116da80203627ff8a5e2db804e521d5
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-08-31 11:39:16 -0700

    [FIX] corrected _rec_name

--- odoo/src/custom-addons/osi-loans/models/loan_options.py
+++ odoo/src/custom-addons/osi-loans/models/loan_options.py
@@ -4,10 +4,11 @@ from odoo import models, fields
 class LoanOptions(models.Model):
     _name = 'loan.options'
     _description = 'create a new loan option in the system'
-    _rec_name = 'loan_name'
 
     loan_name = fields.Char('Loan Name/Type')
     loan_apr = fields.Float('APR', digits=(2, 2))
     minimum_credit = fields.Integer('Minimum Credit')
+
+    _rec_name = loan_name
 
 
