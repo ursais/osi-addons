f62d99a8e6c687084e811a27129bf9b1dff10c1c
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-09-01 21:25:45 -0700

    [ADD] added user groups

--- odoo/src/custom-addons/osi-loans/models/contact_override.py
+++ odoo/src/custom-addons/osi-loans/models/contact_override.py
@@ -1,6 +1,6 @@
-from odoo import models
-
-
-class ContactOverride(models.Model):
-    _name = 'contact.override'
-    _inherit = 'res.partner'
+# from odoo import models
+#
+#
+# class ContactOverride(models.Model):
+#     _name = 'contact.override'
+#     _inherit = 'res.partner'
