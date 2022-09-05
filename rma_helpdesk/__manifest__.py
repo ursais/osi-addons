226cce2725d65eadc8f29be4f7cc01b842e8e182
Freni Patel <fpatel@opensourceintegrators.com>
2022-09-05 15:21:13 +0530

    [14.0][ADD] rma_helpdesk
    Added small adjustments

---
+++ rma_helpdesk/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2022 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "RMA - Helpdesk",
+    "summary": "Link Return Merchandise Authorization with Helpdesk Tickets",
+    "version": "14.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "RMA",
+    "website": "https://github.com/ursais/osi-addons",
+    "author": "Open Source Integrators",
+    "maintainers": ["opensourceintegrators"],
+    "license": "AGPL-3",
+    "depends": [
+        "rma",
+        "helpdesk",
+        "helpdesk_stock",
+    ],
+    "data": [
+        "views/rma_views.xml",
+        "views/helpdesk_views.xml",
+    ],
+}
