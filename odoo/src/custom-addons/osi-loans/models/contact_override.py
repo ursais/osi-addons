2e0ad00f37ff2224f553930d04816aa80d8ebea7
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-09-08 12:33:54 -0700

    [ADD] added user groups

--- odoo/src/custom-addons/osi-loans/models/contact_override.py
+++ odoo/src/custom-addons/osi-loans/models/contact_override.py
@@ -2,7 +2,7 @@ from odoo import models, api, fields
 
 
 
-class ContactOverride(models.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
     employment_status = fields.Char(string="Employment Status")
 
