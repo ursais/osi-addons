efa493cb11759cc77f9b6aff39cd420ca9631880
Richard Baker <88118947+richard-estar@users.noreply.github.com>
2022-09-08 12:30:35 -0700

    [ADD] added user groups

--- odoo/src/custom-addons/osi-loans/models/contact_override.py
+++ odoo/src/custom-addons/osi-loans/models/contact_override.py
@@ -1,6 +1,9 @@
-# from odoo import models
-#
-#
-# class ContactOverride(models.Model):
-#     _name = 'contact.override'
-#     _inherit = 'res.partner'
+from odoo import models, api, fields
+
+
+
+class ContactOverride(models.Model):
+    _inherit = 'res.partner'
+    employment_status = fields.Char(string="Employment Status")
+
+
--- odoo/src/custom-addons/osi-loans/models/new_applicant.py
+++ odoo/src/custom-addons/osi-loans/models/new_applicant.py
@@ -19,14 +19,8 @@ class NewApplicant(models.Model):
     street_address = fields.Text('Street Address', required=True)
     street_address2 = fields.Text('Street 2', required=False)
     city = fields.Char('City', required=True)
-    state = fields.Selection([
-        ('AL', 'AL'), ('AK', 'AK'), ('AZ', 'AZ'), ('AR', 'AR'), ('CA', 'CA'), ('CO', 'CO'), ('CT', 'CT'), ('DE', 'DE'),
-        ('DC', 'DC'), ('FL', 'FL'), ('GA', 'GA'), ('HI', 'HI'), ('ID', 'ID'), ('IL', 'IL'), ('IN', 'IN'), ('IA', 'IA'),
-        ('KS', 'KS'), ('KY', 'KY'), ('LA', 'LA'), ('ME', 'ME'), ('MD', 'MD'), ('MA', 'MA'), ('MI', 'MI'), ('MN', 'MN'),
-        ('MS', 'MS'), ('MO', 'MO'), ('MT', 'MT'), ('NE', 'NE'), ('NV', 'NV'), ('NH', 'NH'), ('NJ', 'NJ'), ('NM', 'NM'),
-        ('NY', 'NY'), ('NC', 'NC'), ('ND', 'ND'), ('OH', 'OH'), ('OK', 'OK'), ('OR', 'OR'), ('PA', 'PA'), ('RI', 'RI'),
-        ('SC', 'SC'), ('SD', 'SD'), ('TN', 'TN'), ('TX', 'TX'), ('UT', 'UT'), ('VT', 'VT'), ('VA', 'VA'), ('WA', 'WA'),
-        ('WV', 'WV'), ('WI', 'WI'), ('WY', 'WY')], string="State")
+    state = fields.Many2one('res.country.state',string="State")
+    country = fields.Many2one('res.country',string="Country")
 
     zip = fields.Integer('Zip code')
     ssn = fields.Integer('SSN #')
