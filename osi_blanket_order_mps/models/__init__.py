1af14cfba778ca0be1a539bfc2709bff243028a0
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2022-09-19 19:13:31 +0530

    [14.0][ADD] osi_blanket_order_mps
    [IMP] osi_blanket_order_mrp (Added blank order creation ability)
    
    Added improvements and fixes
    Changed to allow the ability to create blank orders but still track ordered items on the blanket order.
    
    Improvements
    
    [IMP] osi_blanket_order_mps (user usage improvements)
    
    [IMP] Additional Improvements
    
    pre-commit
    
    [FIX] Small Fixes and Adjustments

---
+++ osi_blanket_order_mps/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2022 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import blanket_orders
+from . import mrp_mps
+from . import sale_orders
