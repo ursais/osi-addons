b80365dff06f0ac7df6bcbeebd4c3265f10b55a5
Antonio Rivera <arivera@opensourceintegrators.com>
2022-10-03 15:54:01 -0500

    [ADD] Stock Bin Location
    # Conflicts:
    #	stock_bin_location/models/stock_move_line.py
    #	stock_bin_location/views/stock_production_lot.xml

---
+++ stock_bin_location/models/stock_move_line.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2022 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    bin_location = fields.Char(string="Bin Location")
+
+    def _action_done(self):
+        result = super()._action_done()
+        for record in self.filtered(lambda m: m.exists() and m.bin_location and m.lot_id):
+            record.lot_id.bin_location = record.bin_location
+        return result
---
+++ stock_bin_location/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_bin_location
