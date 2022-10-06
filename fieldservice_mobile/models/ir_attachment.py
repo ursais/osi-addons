bdd9ae30e0f203be45f44e0c2a1fa7492fe8cf4f
Murtuza Saleh <msaleh@opensourceintegrators.com>
2022-10-06 15:28:55 +0530

    [FIX] Fixed code.

--- fieldservice_mobile/models/ir_attachment.py
+++ fieldservice_mobile/models/ir_attachment.py
@@ -34,7 +34,7 @@ class IrAttachment(models.Model):
                   FROM ir_attachment WHERE id IN %s""",
                 [tuple(self.ids)],
             )
-            for res_model, res_id, public, res_field in self._cr.fetchall():
+            for res_model, res_id, create_uid, public, res_field in self._cr.fetchall():
                 if not self.env.is_system() and res_field:
                     raise AccessError(
                         _("Sorry, you are not allowed to access this document.")
