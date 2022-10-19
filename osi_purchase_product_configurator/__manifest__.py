4e661614a724bff63ed79571679899bcb19d37ff
Chanakya Soni <csoni@opensourceintegrators.com>
2022-10-19 11:45:05 +0530

    [14.0][ADD]osi_purchase_product_configurator

---
+++ osi_purchase_product_configurator/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OSI Purchase Product Configurator",
+    "summary": "OSI Purchase Product Configurator",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "author": "Open Source Integrators",
+    "license": "AGPL-3",
+    "website": "https://www.opensourceintegrators.com",
+    "depends": [
+        "sale_product_configurator",
+        "purchase"
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+        "views/assets.xml",
+    ],
+    "installable": True,
+}
---
+++ osi_purchase_product_configurator/models/__init__.py
@@ -0,0 +1,2 @@
+from . import purchase_order_line
+from . import product_attribute_custom_value
