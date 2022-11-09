9ccba0d9a1edcec8efb39ebe68b63b000bca5d98
cbeddies <cbeddies@opensourceintegrators.com>
2022-11-09 06:05:01 -0800

    [14.0][UPD] add domain to purchase_template_id

--- osi_purchase_product_configurator/models/purchase_order_line.py
+++ osi_purchase_product_configurator/models/purchase_order_line.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    product_template_id = fields.Many2one('product.template', 'Product')
+    product_template_id = fields.Many2one('product.template', 'Product', domain=[("purchase_ok", "=", True)])
     is_configurable_product = fields.Boolean('Is the product configurable?', related="product_template_id.has_configurable_attributes")
     product_template_attribute_value_ids = fields.Many2many(related='product_id.product_template_attribute_value_ids', readonly=True)
     product_custom_attribute_value_ids = fields.One2many('product.attribute.custom.value', 'purchase_order_line_id',
