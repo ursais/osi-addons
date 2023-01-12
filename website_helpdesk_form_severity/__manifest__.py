20d84234aa3c15f549bf96595bcb5abb69011b74
Urvisha@OSI <udesai@opensourceintegrators.com>
2023-01-12 13:17:09 +0530

    [MIG] website_helpdesk_form_severity: Migration to 16.0

--- website_helpdesk_form_severity/__manifest__.py
+++ website_helpdesk_form_severity/__manifest__.py
@@ -1,22 +1,19 @@
-# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2023 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Helpdesk Portal add Severity field",
     "summary": """Add severity field to Helpdesk Portal form.""",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": """Open Source Integrators, Serpent Consulting Services""",
     "category": "Helpdesk",
     "maintainers": ["Open Source Integrators"],
-    "website": "http://www.opensourceintegrators.com",
+    "website": "https://github.com/ursais/osi-addons",
     "depends": [
         "helpdesk_severity",
-        "website_helpdesk_form",
+        "website_helpdesk",
     ],
-    "data": [
-        "data/website_helpdesk.xml",
-        "views/helpdesk_template.xml"
-    ],
+    "data": ["data/website_helpdesk.xml", "views/helpdesk_template.xml"],
     "installable": True,
 }
