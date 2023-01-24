42c073677e4332d5091c9b01b0bf3925e4780c1c
Antonio Yamuta <ayamuta@opensourceintegrators.com>
2023-01-24 10:12:24 -0800

    [ADD] Modue to allow existing users in Odoo to login through OAuth2 Provider. Without this module OTB Odoo Reset Password Link is needed to login thru "Login with Google"

---
+++ osi_auth_oauth/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OAuth2 Authentication Correction",
+    "summary": "Allow existing users in Odoo to login through OAuth2 Provider.",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "depends": ["base", "web", "auth_signup", "auth_oauth"],
+    "data": [],
+}
