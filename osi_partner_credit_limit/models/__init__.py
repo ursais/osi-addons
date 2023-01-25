877b95eec5d85148064b73b8b6ea90b45f44438b
Chanakya-OSI <csoni@opensourceintegrators.com>
2023-01-25 16:05:23 +0530

    [16.0][MIG] Migrated osi_partner_credit_limit (V15 to V16)

---
+++ osi_partner_credit_limit/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2019 - 2023, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_partner
+from . import sale_order
+from . import stock_picking
