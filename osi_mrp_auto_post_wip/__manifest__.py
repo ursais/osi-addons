5ab201c5f7c57f8a546e17b8f132640ac1220a13
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2023-03-29 15:32:24 +0530

    [16.0][ADD] osi_mrp_auto_post_wip

---
+++ osi_mrp_auto_post_wip/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2023 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Manufacturing Accounting Analytic WIP",
+    "summary": "Add feture to Post WIP from tree view and schedule action",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/ursais/osi-addons",
+    "depends": ["mrp_account_analytic_wip"],
+    "data": ["data/ir_cron_data.xml", "views/mrp.xml"],
+}
