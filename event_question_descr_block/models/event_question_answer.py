e422b343356b57a08d1b9c3806ef159711747236
tirth-osi <tpatel@opensourceintegrators.com>
2023-04-24 17:34:56 -0700

    [FIX] Using constrains instead of Write and create check.

--- event_question_descr_block/models/event_question_answer.py
+++ event_question_descr_block/models/event_question_answer.py
@@ -1,4 +1,4 @@
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -8,28 +8,20 @@ class EventQuestionAnswer(models.Model):
     is_blocking = fields.Boolean(string="Is Blocking?")
 
 
-class EventRegistrationAnswer(models.Model):
+class EventAnswer(models.Model):
     _inherit = "event.registration.answer"
 
-    def check_answer_blocking(self, answer_id):
-        answer = self.env["event.question.answer"].browse(answer_id)
-        if answer and answer.is_blocking:
+    @api.constrains("value_answer_id")
+    def check_answer_block(self):
+        if self.value_answer_id.is_blocking:
             raise UserError(
-                _(" Based on your Answers you cannot register for this event.")
+                _(
+                    "The answer you entered for "
+                    + str(self.question_id.title)
+                    + " is not valid. Unfortunately you cannot "
+                    "register for the event."
+                )
             )
-
-    def write(self, vals):
-        if vals.get("value_answer_id"):
-            self.check_answer_blocking(vals.get("value_answer_id"))
-        res = super().write(vals)
-        return res
-
-    def create(self, vals):
-        for val in vals:
-            if val.get("value_answer_id"):
-                self.check_answer_blocking(val.get("value_answer_id"))
-        res = super().create(vals)
-        return res
 
 
 class EventQuestion(models.Model):
