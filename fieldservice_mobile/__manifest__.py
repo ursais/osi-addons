51cf6337f63bef11eaab7c2eb08f0a2c2116a1bb
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2023-05-10 11:54:10 -0600

    [IMP] fieldservice_mobile

--- fieldservice_mobile/__manifest__.py
+++ fieldservice_mobile/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Field Service Mobile",
+    "name": "Field Service - Mobile",
     "summary": "Field Service Mobile Backend Support.",
     "license": "AGPL-3",
     "version": "15.0.1.0.0",
