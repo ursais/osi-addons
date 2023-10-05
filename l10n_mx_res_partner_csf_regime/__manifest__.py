fa6ae0f803e9cfaae4a606e42d6746ebd3235ad8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2023-10-05 08:10:12 -0600

    Update __manifest__.py

--- l10n_mx_res_partner_csf_regime/__manifest__.py
+++ l10n_mx_res_partner_csf_regime/__manifest__.py
@@ -1,18 +1,16 @@
 # Copyright (C) 2017 Open Source Integrators (https://www.opensourceintegrators.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Mexican Localization Fiscal Regime Scan From CSF",
     "version": "16.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": "Mexican Localization Fiscal Regime Scan From CSF",
     "author": "Open Source Integrators",
     "maintainer": "Open Source Integrators",
     "website": "https://github.com/ursais/osi-addons",
     "depends": [
-        # Odoo + Enterprise
         "contacts",
         "l10n_mx_edi_40",
-        # OCA
         "l10n_mx_res_partner_csf",
     ],
     "external_dependencies": {
