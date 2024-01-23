f37cf491c675072f5251876362d58031d320a94a
RLeeOSI <rlee@opensourceintegrators.com>
2024-01-23 21:06:15 +0000

    [ADD] osi_account_commission

---
+++ osi_account_commission/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2022 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import product_template
+from . import commission
+from . import commission_mixin
+from . import commission_settlement
+from . import account_move
