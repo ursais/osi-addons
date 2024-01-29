9787317abbf619d30831fa4fdc64329e5afd57fe
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-01-29 11:24:55 -0600

    [IMP] helpdesk_ticket_type_team

--- helpdesk_ticket_type_team/models/helpdesk_ticket.py
+++ helpdesk_ticket_type_team/models/helpdesk_ticket.py
@@ -7,20 +7,9 @@ class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     @api.onchange("team_id")
-    def _onchange_ticket_type_id(self):
+    def _onchange_team_id(self):
         self.ticket_type_id = False
         if not self.team_id or not self.team_id.ticket_type_ids:
             return {"domain": {"ticket_type_id": [("team_id", "=", False)]}}
         else:
-            ticket_types = self.env["helpdesk.ticket.type"].search(
-                [("team_id", "=", self.team_id.id)]
-            )
-            if len(ticket_types) == 1:
-                self.ticket_type_id = ticket_types[0].id
-                return {
-                    "domain": {"ticket_type_id": [("team_id", "=", self.team_id.id)]}
-                }
-            elif len(ticket_types) > 1:
-                return {
-                    "domain": {"ticket_type_id": [("team_id", "=", self.team_id.id)]}
-                }
+            return {"domain": {"ticket_type_id": [("team_id", "=", self.team_id.id)]}}
