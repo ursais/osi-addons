6912ca3e0550bfa2d1a7a478d735dd5010630204
cbeddies <cbeddies@opensourceintegrators.com>
2024-02-15 13:31:18 -0500

    [17.0][MIG] osi_partner_credit_limit: migrate from 14.0 to 17.0

--- osi_partner_credit_limit/__manifest__.py
+++ osi_partner_credit_limit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "OSI Partner Credit Limit",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators",
     "category": "Sales",
