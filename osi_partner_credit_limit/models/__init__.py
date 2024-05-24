3210a1eece4590c20cd8ff37bc61c34e65e600bd
Chanakya Soni <csoni@opensourceintegrators.com>
2024-05-24 19:46:30 -0400

    [Remove] Removed duplicate module from CSN repo

--- osi_partner_credit_limit/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2019 - 2021, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import sale_order
-from . import stock_picking
