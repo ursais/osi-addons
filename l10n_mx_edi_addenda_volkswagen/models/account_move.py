cf38c6fdbc4d9a8907343ee05a52f455b2f8c302
EdgarRetes <edgar.retes24@gmail.com>
2024-05-29 11:55:34 -0600

    # This is a combination of 2 commits. # This is the 1st commit message:
    [ADD] l10n_mx_edi_addenda_volkswagen

---
+++ l10n_mx_edi_addenda_volkswagen/models/account_move.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    vw_division = fields.Char(string="Division")
+    vw_applicant_name = fields.Char(string="Applicant Name")
+    vw_file = fields.Char(string="File")
+    vw_applicant_email = fields.Char(string="Applicant email")
+    vw_reference = fields.Char(string="Reference")
+    vw_flag = fields.Boolean(compute="_compute_flag")
+
+    @api.depends("partner_id.l10n_mx_edi_addenda")
+    def _compute_flag(self):
+        for record in self:
+            record.vw_flag = (
+                record.partner_id.l10n_mx_edi_addenda_name == "Addenda Volkswagen"
+            )
+
