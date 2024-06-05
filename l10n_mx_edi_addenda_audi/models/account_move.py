7f30f703869eb26c0dad9cbfcb866b713b70286a
EdgarRetes <edgar.retes24@gmail.com>
2024-06-05 10:43:57 -0600

    [FIX] 17.0_l10n_mx_edi_addenda_audi

--- l10n_mx_edi_addenda_audi/models/account_move.py
+++ l10n_mx_edi_addenda_audi/models/account_move.py
@@ -1,14 +1,15 @@
 from odoo import api, fields, models
 
+
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     audi_business_unit = fields.Char(string="Business Unit")
     audi_applicant_email = fields.Char(string="Applicant email")
-    audi_flag = fields.Boolean(compute="_compute_flag")
+    audi_flag = fields.Boolean(compute="_compute_audi_flag", store=True)
 
     @api.depends("partner_id.l10n_mx_edi_addenda")
-    def _compute_flag(self):
+    def _compute_audi_flag(self):
         for record in self:
             record.audi_flag = (
                 record.partner_id.l10n_mx_edi_addenda_name == "Addenda Audi"
