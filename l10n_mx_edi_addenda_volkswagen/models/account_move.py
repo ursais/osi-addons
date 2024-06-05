ad8696d8c4fd6dc24d0c267a02d7ef04a7d4bc0e
EdgarRetes <edgar.retes24@gmail.com>
2024-06-05 10:47:11 -0600

    [FIX] 17.0_l10n_mx_edi_addenda_volkswagen

--- l10n_mx_edi_addenda_volkswagen/models/account_move.py
+++ l10n_mx_edi_addenda_volkswagen/models/account_move.py
@@ -12,7 +12,7 @@ class AccountMove(models.Model):
     vw_flag = fields.Boolean(compute="_compute_vw_flag", store=True)
 
     @api.depends("partner_id.l10n_mx_edi_addenda")
-    def _compute_flag(self):
+    def _compute_vw_flag(self):
         for record in self:
             record.vw_flag = (
                 record.partner_id.l10n_mx_edi_addenda_name == "Addenda Volkswagen"
