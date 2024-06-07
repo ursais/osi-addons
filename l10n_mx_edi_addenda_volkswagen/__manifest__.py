66e992c12949b627bbe051909c28e016f0b7c993
Edgar Martínez Retes <113946434+EdgarRetes@users.noreply.github.com>
2024-06-07 16:39:52 -0600

    [ADD] l10n_mx_edi_addenda_volkswagen (#626)

---
+++ l10n_mx_edi_addenda_volkswagen/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2023 Open Source Integrators (https://www.opensourceintegrators.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Mexican Addendum For Invoices For Volkswagen",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "summary": "Mexican Localization Addendum For Volkswagen",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/ursais/osi-addons",
+    "depends": ["account", "l10n_mx_edi"],
+    "data": [
+        "views/account_move_views.xml",
+        "views/l10n_mx_edi_addenda_volkswagen_views.xml",
+    ],
+    "application": False,
+}
