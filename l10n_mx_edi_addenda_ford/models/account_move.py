f6a85fb5cf556d3291f8d06fa5b12268f2f0877a
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2024-06-08 14:31:02 -0600

    [FIX] l10n_mx_edi_addenda_ford

--- l10n_mx_edi_addenda_ford/models/account_move.py
+++ l10n_mx_edi_addenda_ford/models/account_move.py
@@ -5,7 +5,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     # New Fields
-    ford_ref = fields.Char(string="Reference", default="NA")
+    ford_ref = fields.Char(string="Ford Reference", default="NA")
     ford_flag = fields.Boolean(compute="_compute_ford_flag", store=True)
 
     @api.depends("partner_id.l10n_mx_edi_addenda")
