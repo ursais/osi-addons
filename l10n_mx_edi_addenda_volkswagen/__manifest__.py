6aafcb423ca1e3523f060693229e0efe96adcca8
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2024-06-08 14:30:21 -0600

    [FIX] l10n_mx_edi_addenda_volkswagen

--- l10n_mx_edi_addenda_volkswagen/__manifest__.py
+++ l10n_mx_edi_addenda_volkswagen/__manifest__.py
@@ -10,8 +10,9 @@
     "website": "https://github.com/ursais/osi-addons",
     "depends": ["account", "l10n_mx_edi"],
     "data": [
-        "views/account_move_views.xml",
-        "views/l10n_mx_edi_addenda_volkswagen_views.xml",
+        "views/product_template.xml",
+        "views/account_move.xml",
+        "views/l10n_mx_edi_addenda_volkswagen.xml",
     ],
     "application": False,
 }
