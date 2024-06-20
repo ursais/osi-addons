77c73a2fb283cd1c44531d7ca75ba26ac7840c5b
Patrick Wilson <patrick.wilson@onlogic.com>
2024-06-20 15:57:01 -0700

    [FIX] multiplier

--- ol_product_ship_multiplier/models/product_template.py
+++ ol_product_ship_multiplier/models/product_template.py
@@ -1,5 +1,6 @@
 # Import Odoo libs
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
@@ -10,30 +11,36 @@ class ProductTemplate(models.Model):
 
     _inherit = "product.template"
 
-    # Method #######
-    def _compute_default_shipping_cost(self):
-        for rec in self:
-            default_shipping_cost = 1
-            company = self.env.company
-            if rec.carrier_multiplier_id:
-                default_shipping_cost = (
-                    rec.weight * rec.carrier_multiplier_id.multiplier
-                )
-            rec.with_company(company).default_shipping_cost = default_shipping_cost
-
-    # END #######
-
     # COLUMNS ###
 
     carrier_multiplier_id = fields.Many2one(
-        comodel_name="delivery.carrier.multiplier",
+        "delivery.carrier.multiplier",
         string="Inbound Ship Method",
         company_dependent=True,
+        check_company=True,
     )
     default_shipping_cost = fields.Float(
         string="Cost Multiplier",
         compute="_compute_default_shipping_cost",
-        company_dependent=True,
     )
+
+    # END #######
+
+    # Methods #######
+
+    @api.depends(
+        "weight",
+        "carrier_multiplier_id",
+        "carrier_multiplier_id.multiplier",
+    )
+    @api.depends_context("company")
+    def _compute_default_shipping_cost(self):
+        for rec in self:
+            default_shipping_cost = 0.0
+            if rec.carrier_multiplier_id:
+                default_shipping_cost = (
+                    rec.weight * rec.carrier_multiplier_id.multiplier
+                )
+            rec.default_shipping_cost = default_shipping_cost
 
     # END #######
