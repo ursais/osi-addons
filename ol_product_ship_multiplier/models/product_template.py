e2f0a9a4f9d0895f975f30fc87180185e524ac54
Patrick Wilson <patrick.wilson@onlogic.com>
2024-06-20 14:37:21 -0700

    [FIX] ship_multip

--- ol_product_ship_multiplier/models/product_template.py
+++ ol_product_ship_multiplier/models/product_template.py
@@ -14,21 +14,26 @@ class ProductTemplate(models.Model):
     def _compute_default_shipping_cost(self):
         for rec in self:
             default_shipping_cost = 1
+            company = self.env.company
             if rec.carrier_multiplier_id:
                 default_shipping_cost = (
                     rec.weight * rec.carrier_multiplier_id.multiplier
                 )
-            rec.default_shipping_cost = default_shipping_cost
+            rec.with_company(company).default_shipping_cost = default_shipping_cost
 
     # END #######
 
     # COLUMNS ###
 
     carrier_multiplier_id = fields.Many2one(
-        comodel_name="delivery.carrier.multiplier", string="Inbound Ship Method"
+        comodel_name="delivery.carrier.multiplier",
+        string="Inbound Ship Method",
+        company_dependent=True,
     )
     default_shipping_cost = fields.Float(
-        string="Cost Multiplier", compute="_compute_default_shipping_cost"
+        string="Cost Multiplier",
+        compute="_compute_default_shipping_cost",
+        company_dependent=True,
     )
 
     # END #######
