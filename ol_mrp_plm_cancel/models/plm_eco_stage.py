835c7047e8bf03b3ea10c71e90a459b3c63257f2
Patrick Wilson <patrick.wilson@onlogic.com>
2024-06-20 16:33:06 -0700

    [FIX] ol_mrp_plm

--- ol_mrp_plm_cancel/models/plm_eco_stage.py
+++ ol_mrp_plm_cancel/models/plm_eco_stage.py
@@ -6,15 +6,18 @@ class PlmEcoStage(models.Model):
     """
     Add field to PLM ECO Stage
     """
+
     _inherit = "mrp.eco.stage"
 
     # COLUMNS #####
     cancel_stage = fields.Boolean(
         string="Cancel Stage",
+        help="When the Cancel button is pressed, the ECO will move to this stage"
+        " and the product's stage will be reset to it's original stage.",
     )
     # END #########
 
     @api.onchange("cancel_stage")
     def onchange_cancel_stage(self):
         if self.cancel_stage:
-            self.state = 'cancel'
+            self.state = "cancel"
