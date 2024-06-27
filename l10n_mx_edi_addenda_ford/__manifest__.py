bef95125ef410bc26170595562e2731de3cb433b
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2024-06-27 10:49:02 -0600

    [ADD] 17.0-ams

--- l10n_mx_edi_addenda_ford/__manifest__.py
+++ l10n_mx_edi_addenda_ford/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Mexican Addendum For Invoices For Ford",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "license": "LGPL-3",
     "summary": "Mexican Localization Addendum For Ford",
     "author": "Open Source Integrators",
