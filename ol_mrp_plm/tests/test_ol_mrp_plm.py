fcfc53f12acebe149fedb6c37c1ef6a6b6840c57
Nikul-OSI <nchaudhary@opensourceintegrators.com>
2024-07-02 13:00:48 +0530

    [FIX] Fixed UT

--- ol_mrp_plm/tests/test_ol_mrp_plm.py
+++ ol_mrp_plm/tests/test_ol_mrp_plm.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo.tests import common, tagged
-
+from odoo.exceptions import ValidationError
 from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 
 
@@ -13,14 +13,16 @@ class TestPLMProductState(common.TransactionCase):
         cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
 
         cls.ecostage_new = cls.env.ref("mrp_plm.ecostage_new")
-        product_state_draft = cls.env.ref("product_state.product_state_draft")
-        cls.ecostage_new.write({"product_state_id": product_state_draft.id})
+        cls.product_state_draft = cls.env.ref("product_state.product_state_draft")
+        cls.ecostage_new.write({"product_state_id": cls.product_state_draft.id})
 
         cls.ecostage_progress = cls.env.ref("mrp_plm.ecostage_progress")
-        product_state_sellable = cls.env.ref("product_state.product_state_sellable")
-        cls.ecostage_progress.write({"product_state_id": product_state_sellable.id})
+        cls.product_state_sellable = cls.env.ref("product_state.product_state_sellable")
+        cls.ecostage_progress.write({"product_state_id": cls.product_state_sellable.id, "allow_bom_edits": False})
 
         cls.ecotype0 = cls.env.ref("mrp_plm.ecotype0")
+        cls.test_user_demo = cls.env.ref("base.user_demo")
+        cls.test_user_admin = cls.env.ref("base.user_admin")
 
         cls.product = cls.env["product.template"].create(
             [
@@ -30,6 +32,30 @@ class TestPLMProductState(common.TransactionCase):
                 }
             ]
         )
+        # Create BOM
+        product_to_build = cls.env['product.product'].create({
+            'name': 'Laptop',
+            'type': 'product',
+            "product_state_id": cls.product_state_draft.id,
+        })
+        product_to_use_1 = cls.env['product.product'].create({
+            'name': 'Body',
+            'type': 'product',
+        })
+        product_to_use_2 = cls.env['product.product'].create({
+            'name': 'display',
+            'type': 'product',
+        })
+        cls.bom_1 = cls.env['mrp.bom'].create({
+            'product_id': product_to_build.id,
+            'product_tmpl_id': product_to_build.product_tmpl_id.id,
+            'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
+            'product_qty': 3.0,
+            'type': 'normal',
+            'bom_line_ids': [
+                (0, 0, {'product_id': product_to_use_2.id, 'product_qty': 1}),
+                (0, 0, {'product_id': product_to_use_1.id, 'product_qty': 1})
+            ]})
 
     def test_action_new_revision_ol(self):
         mrp_eco = self.env["mrp.eco"].create(
@@ -45,3 +71,39 @@ class TestPLMProductState(common.TransactionCase):
         mrp_eco.action_new_revision()
         mrp_eco.write({"stage_id": self.ecostage_new.id})
         mrp_eco.write({"stage_id": self.ecostage_progress.id})
+
+    def test_product_allow_modification(self):
+        "The invoice_policy field of a product can only be modified by an admin user."
+        with self.assertRaises(ValidationError):
+            self.product.with_user(self.test_user_demo).write({"invoice_policy": "delivery"})
+        self.assertNotEqual(self.product.invoice_policy, "delivery")
+
+        self.product.with_user(self.test_user_admin).write({"invoice_policy": "delivery"})
+        self.assertEqual(self.product.invoice_policy, "delivery")
+
+    def test_bom_allow_modification(self):
+        "The product_qty field of a product can only be modified by an admin user."
+        self.assertEqual(self.bom_1.product_id.product_state_id, self.product_state_draft)
+        with self.assertRaises(ValidationError):
+            self.bom_1.with_user(self.test_user_demo).write({"product_qty": 5.0})
+        self.assertNotEqual(self.bom_1.product_qty, 5.0)
+
+        self.bom_1.with_user(self.test_user_admin).write({"product_qty": 5.0})
+        self.assertEqual(self.bom_1.product_qty, 5.0)
+
+    def test_change_product_stages(self):
+        "The product_state_id of a product can be changed by writing to the stage_id field of ECO."
+        mrp_eco = self.env["mrp.eco"].create(
+            [
+                {
+                    "name": "Test MRP ECO",
+                    "type_id": self.ecotype0.id,
+                    "type": "product",
+                    "product_tmpl_id": self.product.id,
+                    "stage_id": self.ecostage_new.id,
+                }
+            ]
+        )
+        self.assertNotEqual(self.product.product_state_id, self.ecostage_progress.product_state_id)
+        mrp_eco.write({"stage_id": self.ecostage_progress.id})
+        self.assertEqual(self.product.product_state_id, self.ecostage_progress.product_state_id)
