94c20f6fb99b679753c01b116317114ce7573531
Santiago Rodriguez <santiromur@gmail.com>
2024-07-22 11:20:26 -0600

    [FIX] l10n_mx_edi_addenda_audi: Fix mail and number

--- l10n_mx_edi_addenda_audi/models/account_move.py
+++ l10n_mx_edi_addenda_audi/models/account_move.py
@@ -10,8 +10,6 @@ class AccountMove(models.Model):
     audi_tax_code = fields.Char(string="Tax Code")
     audi_fiscal_document_type = fields.Char(string="Fiscal Document Type")
     audi_document_type = fields.Char(string="Document Type")
-    audi_supplier_email = fields.Char(string="Supplier Email")
-    audi_supplier_number = fields.Char(string="Supplier Number")
 
     @api.depends("partner_id.l10n_mx_edi_addenda")
     def _compute_audi_flag(self):
