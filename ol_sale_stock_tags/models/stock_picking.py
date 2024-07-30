e866415894cd5c72cfff101650360f4efc6c732b
Patrick Wilson <patrick.wilson@onlogic.com>
2024-07-30 10:13:20 -0700

    Added comments and formatting

--- ol_sale_stock_tags/models/stock_picking.py
+++ ol_sale_stock_tags/models/stock_picking.py
@@ -3,10 +3,18 @@ from odoo import fields, models
 
 
 class StockPicking(models.Model):
+    """
+    Add new Sale Tags field to Stock Picking
+    """
+
     _inherit = "stock.picking"
+
+    # COLUMNS #####
 
     tag_ids = fields.Many2many(
         related="sale_id.tag_ids",
         string="Tags",
         store=False,
     )
+
+    # END #########
