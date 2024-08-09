ad7fca14b22238450b58c930095a24f7c9635191
Vandan Pandeji <vpandeji@opensourceintegrators.com>
2024-08-09 15:42:16 +0530

    [ADD]ol_product_configurator_stock

---
+++ ol_product_configurator_stock/models/product_attribute.py
@@ -0,0 +1,41 @@
+# Import Odoo libs
+from odoo import models
+
+
+class ProductAttributeValue(models.Model):
+    """Inherit Product Attribute Value for Method Overriding."""
+
+    _inherit = "product.attribute.value"
+
+    def _compute_display_name(self):
+        # Call the parent class's _compute_display_name method to ensure any existing
+        # logic is preserved.
+        super()._compute_display_name()
+
+        # Check if the context includes the "show_price_extra" key and its value is True.
+        if self._context.get("show_price_extra"):
+            for rec in self:
+                # Determine the quantity available for the associated product.
+                # If there's no product associated, qty_available defaults to 0.
+                qty_available = rec.product_id and rec.product_id.qty_available or 0
+
+                # Calculate the on-hand quantity after subtracting outgoing quantities.
+                # If there's no product, outgoing_qty defaults to 0.0.
+                outgoing_qty = (
+                    rec.product_id
+                    and qty_available - rec.product_id.outgoing_qty
+                    or 0.0
+                )
+
+                # Retrieve the product's state name if available, otherwise,
+                # default to an empty string.
+                product_state_string = (
+                    rec.product_id and rec.product_id.product_state_id.name or ""
+                )
+
+                # Create a new display name with the format:
+                # "Original Display Name (A:<qty_available>/OH:<outgoing_qty>) (<product_state_string>)"
+                new_name = f"{rec.display_name} (A:{qty_available}/OH:{outgoing_qty}) ({product_state_string})"
+
+                # Update the record's display name only if a product is associated with it.
+                rec.display_name = rec.product_id and new_name or rec.display_name
---
+++ ol_product_configurator_stock/tests/test_product_confi_stock.py
@@ -0,0 +1,72 @@
+from odoo.tests import common, tagged
+
+
+@tagged("-at_install", "post_install")
+class TestProductConfiStock(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set up the testing environment by initializing required records.
+        super().setUpClass()
+
+        # Create a product template for an "i7 Processor".
+        cls.test_i7_pro = cls.env["product.template"].create(
+            {
+                "name": "i7 Processor",
+                "type": "product",
+                "categ_id": cls.env.ref("product.product_category_all").id,
+            }
+        )
+
+        # Create a product attribute named "SSD" with two values: "SSD-512GB" and "SSD-1TB".
+        cls.attr_ssd = cls.env["product.attribute"].create(
+            {
+                "name": "SSD",
+                "value_ids": [
+                    (0, 0, {"name": "SSD-512GB"}),
+                    (0, 0, {"name": "SSD-1TB"}),
+                ],
+            }
+        )
+
+        # Create an additional attribute value "SSD-256GB" under the "SSD" attribute
+        # and set the context to show the price extra in the display name.
+        cls.value_ssd256 = (
+            cls.env["product.attribute.value"]
+            .with_context(show_price_extra=True)
+            .create({"name": "SSD-256GB", "attribute_id": cls.attr_ssd.id})
+        )
+
+        # Create a product attribute named "Processor" with three values: "i3", "i5", and "i7".
+        cls.attr_processor = cls.env["product.attribute"].create(
+            {
+                "name": "Processor",
+                "value_ids": [
+                    (0, 0, {"name": "i3"}),
+                    (0, 0, {"name": "i5"}),
+                ],
+            }
+        )
+
+        # Create an attribute value "i7" under the "Processor" attribute and link it to the "i7 Processor" product variant.
+        # The context is set to show the price extra in the display name.
+        cls.attr_i7_value = (
+            cls.env["product.attribute.value"]
+            .with_context(show_price_extra=True)
+            .create(
+                {
+                    "name": "i7",
+                    "attribute_id": cls.attr_processor.id,
+                    "product_id": cls.test_i7_pro.product_variant_id.id,
+                }
+            )
+        )
+
+    def test_01_check_attribute_value_display_name(self):
+        # Test the display name for an attribute value that is linked to a product.
+        self.assertEqual(
+            self.attr_i7_value.display_name,
+            "i7 ( +1.00 ) (A:0/OH:0.0) (In Development)",
+        )
+
+        # Test the display name for an attribute value that is not linked to a product.
+        self.assertEqual(self.value_ssd256.display_name, "SSD: SSD-256GB")
