c1e57cc19977f5a1efcf8d4b18559415dbaf70b1
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2024-08-30 12:22:54 -0600

    [17.0][MIG] documents_quality (#831)

---
+++ documents_quality/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2021 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Documents - Quality",
+    "summary": "Documents for Quality Control Points, Checks and Alerts",
+    "category": "Productivity/Documents",
+    "website": "https://github.com/ursais/osi-addons",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["documents", "quality"],
+    "data": [
+        "data/documents_folder.xml",
+        "data/res_company.xml",
+        "data/documents_facet.xml",
+        "data/documents_tag.xml",
+        "views/res_config_settings.xml",
+    ],
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
+}
