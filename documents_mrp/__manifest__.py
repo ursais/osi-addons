c801199bae832c25d2d93b59db95a8196db962a5
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2024-08-30 12:22:27 -0600

    [17.0][MIG] documents_mrp (#829)

---
+++ documents_mrp/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2021 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Documents - MRP",
+    "summary": "Documents for Production Orders",
+    "category": "Productivity/Documents",
+    "website": "https://github.com/ursais/osi-addons",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["documents", "mrp"],
+    "data": [
+        "data/documents_folder.xml",
+        "data/res_company.xml",
+        "data/documents_facet.xml",
+        "data/documents_tag.xml",
+        "views/res_config_settings.xml",
+    ],
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
+}
---
+++ documents_mrp/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2021 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import res_company
+from . import mrp_production
+from . import res_config_settings
