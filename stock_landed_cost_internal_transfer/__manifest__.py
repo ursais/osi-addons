802d502776937e8c18ee2b3a3adf30fa960246ca
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2024-09-05 17:08:02 +0530

    [ADD]stock_landed_cost_internal_transfer

---
+++ stock_landed_cost_internal_transfer/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2024 Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Landed Cost Internal Transfer",
+    "summary": "Stock Landed Cost Internal Transfer",
+    "category": "Stock",
+    "website": "https://github.com/ursais/osi-addons",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["stock_landed_costs"],
+    "data": [
+        "views/stock_picking_view.xml"
+    ],
+}
