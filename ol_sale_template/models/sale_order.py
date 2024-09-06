ebd595d9434842010c133652eeb17a258ec7c094
Patrick Wilson <patrick.wilson@onlogic.com>
2024-09-06 14:30:26 -0700

    [IMP] ol_sale_template

--- ol_sale_template/models/sale_order.py
+++ ol_sale_template/models/sale_order.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 class SaleOrder(models.Model):
     """
-    Add new fields to Sale Order
+    Override sale_order_template_id changing domain.
     """
 
     _inherit = "sale.order"
@@ -12,13 +12,6 @@ class SaleOrder(models.Model):
     # COLUMNS #####
 
     sale_order_template_id = fields.Many2one(
-        comodel_name="sale.order.template",
-        string="Quotation Template",
-        compute="_compute_sale_order_template_id",
-        store=True,
-        readonly=False,
-        check_company=True,
-        precompute=True,
         domain="[('partner_id', 'in', [False, partner_id]), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
     )
 
