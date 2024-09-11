a5e61de10f1b9ed8556390f8bc22a2fc073be512
Patrick Wilson <patrick.wilson@onlogic.com>
2024-09-11 12:52:16 -0700

    [FIX] sync ol_blind_dropship/ol_mrp_bom_rebuild

--- ol_blind_dropship/tests/test_blind_drop.py
+++ ol_blind_dropship/tests/test_blind_drop.py
@@ -1,5 +1,6 @@
 # Import Python libs
 import logging
+
 # Import Odoo libs
 
 from odoo.tests.common import TransactionCase
@@ -17,7 +18,6 @@ class TestBlindDrop(TransactionCase):
     def setUp(self):
         super().setUp()
 
-        # self.blind_product = self.env.ref("ol_blind_dropship.blind_drop_ship_product")
         self.blind_product = self.env["product.product"].create(
             {
                 "name": "Product to indicate blind drop shipping",
@@ -106,74 +106,3 @@ class TestBlindDrop(TransactionCase):
         self.assertTrue(
             picking.blind_drop_ship, "Stock Picking was not marked as Blind Drop!"
         )
-
-    # def test_blind_sku_in_system(self):
-    #     """
-    #     Make sure that pickings are marked if the blind drop sku is in the Bill of Materials
-    #     """
-    #     picking = self.env["stock.picking"].create(
-    #         {
-    #             "location_id": self.stock_location.id,
-    #             "location_dest_id": self.customer_location.id,
-    #             "picking_type_id": self.ref("stock.picking_type_out"),
-    #             "partner_id": self.partner.id,
-    #         }
-    #     )
-    #
-    #     blind_order = self._create_system_sale_order(partner=self.partner)
-    #
-    #     # set one of the products in the BoM to be the blind ship product
-    #     line_products = blind_order.order_line.product_id
-    #     bom_products = blind_order.order_line.bom_id.bom_line_ids.product_id
-    #
-    #     # pick a bom product that is not on a different sale order line
-    #     component = (bom_products - line_products)[0]
-    #
-    #     # Set our product to be the blind drop product
-    #     _logger.debug(
-    #         f"Set BoM component to be blind ship product: {component.default_code} ({component})"
-    #     )
-    #     self.env["ir.config_parameter"].sudo().set_param(
-    #         "stock_move.blind_drop_ship", component.id
-    #     )
-    #     self.assertEqual(
-    #         str(component.id),
-    #         self.env["ir.config_parameter"]
-    #         .sudo()
-    #         .get_param("stock_move.blind_drop_ship"),
-    #         "Blind product setting is not correct",
-    #     )
-    #
-    #     self.assertFalse(
-    #         picking.blind_drop_ship,
-    #         "Stock Picking was incorrectly marked as Blind Drop!",
-    #     )
-    #
-    #     # Link the picking to a sale order for a blind drop product
-    #     picking.sale_id = blind_order
-    #     self.assertTrue(
-    #         picking.blind_drop_ship, "Stock Picking was not marked as Blind Drop!"
-    #     )
-    #
-    #
-    #
-    # def _create_system_sale_order(self, partner):
-    #     return self.env["sale.order"].create(
-    #         {
-    #             "partner_id": partner.id,
-    #             "pricelist_id": partner.property_product_pricelist.id,
-    #             # "order_line": [
-    #             #     (
-    #             #         0,
-    #             #         0,
-    #             #         {
-    #             #             "name": "Test line",
-    #             #             "product_id": product.id,
-    #             #             "product_uom_qty": product_qty,
-    #             #             "product_uom": product.uom_id.id,
-    #             #             "price_unit": price_unit,
-    #             #         },
-    #             #     )
-    #             # ],
-    #         }
-    #     )
