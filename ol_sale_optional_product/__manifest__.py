5736b4fffd2be15f9e553c6cb6c2d44ca4eebf4e
Patrick Wilson <patrick.wilson@onlogic.com>
2024-09-23 15:41:47 -0700

    [ADD] ol_sale_optional_product

---
+++ ol_sale_optional_product/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "OnLogic Sale Add Optional Products",
+    "summary": "Create Optional Products from Sale Lines.",
+    "author": "OnLogic",
+    "website": "https://www.onlogic.com",
+    "onlogic": True,
+    "category": "Sale",
+    "version": "17.0.0.1.0",
+    "license": "AGPL-3",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "ol_base",
+        "sale_management",
+        "product_state",
+    ],
+    # always loaded
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+}
