2ca8d74c396f6f794275e3255a302032875dd93e
Patrick Wilson <patrick.wilson@onlogic.com>
2024-10-30 11:43:54 -0700

    [UPD] sync modules with osi from onl

--- ol_mrp_bom_rebuild/models/mrp_eco.py
+++ ol_mrp_bom_rebuild/models/mrp_eco.py
@@ -1,5 +1,5 @@
 # Import Odoo libs
-from odoo import api,models
+from odoo import api, models
 
 
 class MRPEco(models.Model):
@@ -23,13 +23,24 @@ class MRPEco(models.Model):
                 eco.product_tmpl_id._reset_all_variants_bom_with_scaffold_bom()
         return result
 
-    @api.onchange('product_tmpl_id')
+    @api.onchange("product_tmpl_id")
     def onchange_product_tmpl_id(self):
+        """
+        When product_tmpl_id is changed, this method checks if there are
+        existing BoMs associated with the product template:
+            - If any BoM has the scaffolding_bom flag, it sets self.bom_id to that BoM.
+            - If no scaffolding_bom is found but there is a BoM without a specific
+              product (product_id not set), it assigns the first such BoM to
+              self.bom_id.
+            - If neither condition is met, it falls back to the default behavior
+              of onchange_product_tmpl_id using super()
+        """
         bom_ids = self.product_tmpl_id.bom_ids
-        if bom_ids and bom_ids.filtered(lambda l:l.scaffolding_bom):
-            self.bom_id = bom_ids.filtered(lambda l:l.scaffolding_bom).id
-        elif bom_ids and bom_ids.filtered(lambda l:not l.product_id):
-            self.bom_id = bom_ids.filtered(lambda l:not l.product_id).ids[0]
+        if bom_ids and bom_ids.filtered(lambda l: l.scaffolding_bom):
+            self.bom_id = bom_ids.filtered(lambda l: l.scaffolding_bom).id
+        elif bom_ids and bom_ids.filtered(lambda l: not l.product_id):
+            self.bom_id = bom_ids.filtered(lambda l: not l.product_id).ids[0]
         else:
             super().onchange_product_tmpl_id()
+
     # END #########
--- ol_sale_mrp_tags/models/stock.py
+++ ol_sale_mrp_tags/models/stock.py
@@ -4,26 +4,26 @@ from odoo import models
 
 class StockMove(models.Model):
     """
-    Override stock moves to set sale_line_id for mrp uses.
+    Override method to set mrp_so_line_id for mrp uses.
     """
 
     _inherit = "stock.move"
 
     def _prepare_procurement_values(self):
-        # Ensure sale_line_id is propagated through stock moves
-        res = super(StockMove, self)._prepare_procurement_values()
-        if self.sale_line_id:
-            # Pass sale_line_id into procurement values
-            res["sale_line_id"] = self.sale_line_id.id
+        res = super()._prepare_procurement_values()
+        # Add sale order line to values
+        res["mrp_so_line_id"] = self.sale_line_id
         return res
 
 
 class StockRule(models.Model):
     """
-    Override stock rules to set sale_order_id gets set on MO.
+    Override method to set sale_order_id for mrp uses.
     """
 
     _inherit = "stock.rule"
+
+    # METHODS #########
 
     def _prepare_mo_vals(
         self,
@@ -37,9 +37,6 @@ class StockRule(models.Model):
         values,
         bom,
     ):
-        """
-        Override the _prepare_mo_vals method to set the sale_order_id.
-        """
         res = super()._prepare_mo_vals(
             product_id,
             product_qty,
@@ -51,26 +48,13 @@ class StockRule(models.Model):
             values,
             bom,
         )
-
-        # If sale_line_id exists in values, link the MO to the sale order
-        sale_line_id = values.get("sale_line_id")
-        if sale_line_id:
-            sale_order = self.env["sale.order.line"].browse(sale_line_id).order_id
-            # Attach sale_order_id to the manufacturing order
+        # if mrp_so_line_id is in values, then add the sale order to values
+        if values.get("mrp_so_line_id"):
             res.update(
                 {
-                    "sale_order_id": sale_order.id,
+                    "sale_order_id": values.get("mrp_so_line_id").order_id.id,
                 }
             )
         return res
 
-    def _create_manufacturing_orders(self, procurements):
-        """
-        Override the _create_manufacturing_orders method to pass sale_order_id
-        from the sale_line_id to the manufacturing order, even in complex routes.
-        """
-        for procurement, rule in procurements:
-            sale_line = procurement.values.get("sale_line_id")
-            if sale_line:
-                procurement.values["mrp_so_line_id"] = sale_line
-        return super(StockRule, self)._create_manufacturing_orders(procurements)
+    # END #########
