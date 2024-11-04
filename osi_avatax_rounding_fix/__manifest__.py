d431d82034e18ad6984effec7f768acdf7d71426
tirth-osi <tpatel@opensourceintegrators.com>
2024-11-04 04:19:36 -0700

    [ADD] Generic avatax oca rounding

---
+++ osi_avatax_rounding_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{ # noqa
+    "name": "Proslat Accounting Enhancement",
+    "summary": "Proslat Accounting Enhancement",
+    "version": "17.0.1.0.0",
+    "author": "Open Source Integrators",
+    "category": "Accounting",
+    "depends": [
+        'account_avatax_sale_oca',
+    ],
+    "data": [
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False
+}
+
