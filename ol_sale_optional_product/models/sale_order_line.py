4f990a81318c9364f0716b4a261cde2b34654679
Patrick Wilson <patrick.wilson@onlogic.com>
2024-11-07 15:30:48 -0800

    [IMP] ol_sale_optional_products (Comments)

--- ol_sale_optional_product/models/sale_order_line.py
+++ ol_sale_optional_product/models/sale_order_line.py
@@ -12,6 +12,8 @@ class SaleOrderLine(models.Model):
 
     # Methods #####
     def _get_values_to_add_to_optional(self):
+        # Prepare a dictionary of values to add a product as an
+        # optional line in the sale order.
         self.ensure_one()
         return {
             "order_id": self.order_id.id,
@@ -25,16 +27,22 @@ class SaleOrderLine(models.Model):
         }
 
     def button_add_to_optional(self):
+        # Button on sale order line that creates optional product
         self.add_option_to_optional()
 
     def add_option_to_optional(self):
+        # Called from button method, adds the current product as an
+        # optional line in the related sale order.
         self.ensure_one()
 
         sale_order = self.order_id
 
+        # Check if the sale order can be edited on the portal; raise error if it cannot
         if not sale_order._can_be_edited_on_portal():
             raise UserError(_("You cannot add options to a confirmed order."))
 
+        # Retrieve values for the optional line and create a new optional line in
+        # the sale order
         values = self._get_values_to_add_to_optional()
         option_line = self.env["sale.order.option"].create(values)
 
