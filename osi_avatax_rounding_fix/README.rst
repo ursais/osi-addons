d815dc0b7d4ac982834a465ec6958092b58e6816
RLeeOSI <51208020+RLeeOSI@users.noreply.github.com>
2024-11-07 14:50:50 -0800

    [REF] pre-commit

---
+++ osi_avatax_rounding_fix/README.rst
@@ -0,0 +1,23 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+========
+Overview
+========
+
+* Correct rounding issues on avatax lines
+
+
+=======
+Credits
+=======
+
+* Open Source Integrators <http://www.opensourceintegrators.com>
+
+
+Contributors
+------------
+
+* Tirth Patel <tpatel@opensourceintegrators.com>
+* Raphael Lee <rlee@opensourceintegrators.com>
--- osi_avatax_rounding_fix/__manifest__.py
+++ osi_avatax_rounding_fix/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
-
-{ # noqa
+{  # noqa
     "name": "OSI Avatax Rounding Fix",
     "summary": "OSI Avatax Rounding Fix",
     "version": "17.0.1.0.0",
+    "license": "LGPL-3",
     "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/ursais/osi-addons",
     "category": "Accounting",
     "depends": [
-        'account_avatax_sale_oca',
+        "account_avatax_sale_oca",
     ],
-    "data": [
-    ],
+    "data": [],
     "application": False,
     "installable": True,
-    "auto_install": False
+    "auto_install": False,
 }
-
