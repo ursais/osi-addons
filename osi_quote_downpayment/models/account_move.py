2d639dad84cf81832db3872b8bbbd300fa3e6d9d
cbeddies <cbeddies@opensourceintegrators.com>
2024-11-12 11:18:48 -0500

    [ADD] osi_quote_downpayment: allows downpayments on quotes

---
+++ osi_quote_downpayment/models/account_move.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 - 2021, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    #Prevents price of downpayment from being reset on draft sale orders
+    def action_post(self):
+        downpayment_lines = {}
+        for rec in records:
+            downpayment_lines[rec.id] = {}
+            for line in rec.invoice_line_ids:
+                if "Down payment" in line.name:
+                    downpayment_lines[rec.id][line.id] = line.price_unit
+        res = super().action_post()
+        for rec in records:
+            for line in rec.invoice_line_ids:
+                if "Down payment" in line.name:
+                    downpayment_lines[rec.id][line.id] = line.price_unit
+        return res
