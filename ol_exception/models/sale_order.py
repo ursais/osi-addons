180c74b02896d9cafac9e6c0481651b73d4de7c9
Patrick Wilson <patrick.wilson@onlogic.com>
2024-11-26 11:21:58 -0800

    [IMP] ol_exceptions (Relation Field Updates)

--- ol_exception/models/sale_order.py
+++ ol_exception/models/sale_order.py
@@ -6,24 +6,50 @@ class SaleOrder(models.Model):
 
     def _fields_trigger_check_exception(self):
         config_records = self.env["exception.config"].search(
-            [("model_id.model", "=", "sale.order")]
+            [("model_id.model", "=", self._name)]
         )
-        return [
-            field.name
-            for record in config_records
-            for field in record.trigger_field_ids
-        ]
+        fields_to_check = set()
+        for config in config_records:
+            # Include directly configured fields
+            fields_to_check.update(field.name for field in config.trigger_field_ids)
+            # Include dynamically discovered related fields
+            related_fields = config.get_related_fields()
+            fields_to_check.update(field.name for field in related_fields)
+        return list(fields_to_check)
 
     def write(self, vals):
-        """Since Stock Pickings & Manufacturing Orders have a related
-        field to the locked field, changing it doesn't flow through write
-        method therfore we have to trick it here passing through vals."""
-        res = super(SaleOrder, self).write(vals)
-        if "locked" in vals:
+        trigger_fields = self._fields_trigger_check_exception()
+        relevant_changes = {
+            field: vals[field] for field in trigger_fields if field in vals
+        }
+
+        # Get field mappings from exception.config
+        config_records = self.env["exception.config"].search(
+            [("model_id.model", "=", "sale.order")]
+        )
+        field_mapping = {}
+        for config in config_records:
+            mapping = config.get_field_mapping()
+            field_mapping.update(mapping)
+
+        # Map changes to related fields
+        related_changes = {}
+        for field, value in relevant_changes.items():
+            if field in field_mapping:
+                for related_field in field_mapping[field]:
+                    related_changes[related_field] = value
+
+        # Call super to apply the write
+        res = super().write(vals)
+
+        # Handle exception checks for related fields
+        if related_changes:
             pickings = self.env["stock.picking"].search([("sale_id", "in", self.ids)])
-            pickings._check_stock_check_exception({"sale_locked": vals["locked"]})
+            pickings._check_stock_check_exception(related_changes)
+
             mrp_productions = self.env["mrp.production"].search(
                 [("sale_order_id", "in", self.ids)]
             )
-            mrp_productions._check_mrp_check_exception({"sale_locked": vals["locked"]})
+            mrp_productions._check_mrp_check_exception(related_changes)
+
         return res
