a5a51090352d7455e7420faae9678bbf1492af55
Patrick Wilson <patrick.wilson@onlogic.com>
2024-12-04 10:30:46 -0800

    [FIX] estimate (ECO/Product Open, ECO Stage)

--- ol_job_cost_estimate_customer/wizard/create_product_eco_wizard.py
+++ ol_job_cost_estimate_customer/wizard/create_product_eco_wizard.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductCreationWizard(models.TransientModel):
@@ -6,11 +6,23 @@ class ProductCreationWizard(models.TransientModel):
     _description = "Wizard to create product, BoM, and ECO"
 
     product_name = fields.Char(string="Product Name", required=True)
+    type_id = fields.Many2one("mrp.eco.type", "ECO Type")
+
+    @api.model
+    def default_get(self, fields):
+        result = super().default_get(fields)
+        eco_type = self.env["mrp.eco.type"].search([("name", "=", "New SKU")], limit=1)
+        if eco_type:
+            if "type_id" in fields:
+                result["type_id"] = eco_type.id
+        return result
 
     def action_create_product_bom_eco(self):
         self.ensure_one()
         sale_estimate_job = self.env["sale.estimate.job"].browse(
             self._context.get("active_id")
         )
-        action = sale_estimate_job.action_create_eco_and_product(self.product_name)
+        action = sale_estimate_job.action_create_eco_and_product(
+            self.product_name, self.type_id
+        )
         return action
