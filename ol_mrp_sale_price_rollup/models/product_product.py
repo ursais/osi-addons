6e01621d02d9826b19297194005888c74049da4d
Patrick Wilson <patrick.wilson@onlogic.com>
2024-12-05 09:57:41 -0800

    [IMP] ol_mrp_sale_price_rollup (Add Total Cost Rollup)

--- ol_mrp_sale_price_rollup/models/product_product.py
+++ ol_mrp_sale_price_rollup/models/product_product.py
@@ -124,6 +124,9 @@ class ProductProduct(models.Model):
             product._set_sale_price_from_bom()
             product.lst_price = list_price + product.price_extra
 
+            # Trigger to set the total cost from bom
+            product._set_total_cost_from_bom()
+
         return res
 
     def button_bom_sale_price(self):
@@ -134,6 +137,7 @@ class ProductProduct(models.Model):
         it triggers the recomputation of the BoM sale price.
         """
         self._compute_product_lst_price()
+        self._set_total_cost_from_bom()
 
     def action_bom_sale_price(self):
         """
@@ -154,6 +158,7 @@ class ProductProduct(models.Model):
 
         for product in self:
             product._set_sale_price_from_bom(boms_to_recompute)
+            product._set_total_cost_from_bom(boms_to_recompute)
 
     def _set_sale_price_from_bom(self, boms_to_recompute=False):
         """
@@ -199,24 +204,24 @@ class ProductProduct(models.Model):
             return 0
         if not boms_to_recompute:
             boms_to_recompute = []
-        total = 0
+        sale_total = 0
 
         for line in bom.bom_line_ids:
             if line._skip_bom_line(self):
                 continue
 
             if line.child_bom_id and line.child_bom_id in boms_to_recompute:
-                child_total = line.product_id._compute_bom_sale_price(
+                child_sale_total = line.product_id._compute_bom_sale_price(
                     line.child_bom_id, boms_to_recompute=boms_to_recompute
                 )
-                total += (
+                sale_total += (
                     line.product_id.uom_id._compute_price(
-                        child_total, line.product_uom_id
+                        child_sale_total, line.product_uom_id
                     )
                     * line.product_qty
                 )
             else:
-                total += (
+                sale_total += (
                     line.product_id.uom_id._compute_price(
                         line.product_id.lst_price, line.product_uom_id
                     )
@@ -235,15 +240,105 @@ class ProductProduct(models.Model):
                 )
             byproduct_cost_share = sum(byproduct_lines.mapped("cost_share"))
             if byproduct_cost_share and product_uom_qty:
-                return total * byproduct_cost_share / 100 / product_uom_qty
+                return sale_total * byproduct_cost_share / 100 / product_uom_qty
         else:
             byproduct_cost_share = sum(bom.byproduct_ids.mapped("cost_share"))
             if byproduct_cost_share:
-                total *= float_round(
+                sale_total *= float_round(
                     1 - byproduct_cost_share / 100, precision_rounding=0.0001
                 )
             return bom.product_uom_id._compute_price(
-                total / bom.product_qty, self.uom_id
+                sale_total / bom.product_qty, self.uom_id
+            )
+
+    def _set_total_cost_from_bom(self, boms_to_recompute=False):
+        """
+        Set the BoM total cost for the product.
+
+        This method calculates and sets the total cost of the product based on its
+        associated BoM. It handles both normal and byproduct cases.
+        """
+        self.ensure_one()
+        bom = self.env["mrp.bom"]._bom_find(self)[self]
+        if bom:
+            self.total_cost = self._compute_bom_custom_cost(
+                bom, boms_to_recompute=boms_to_recompute
+            )
+        else:
+            bom = self.env["mrp.bom"].search(
+                [("byproduct_ids.product_id", "=", self.id)],
+                order="sequence, product_id, id",
+                limit=1,
+            )
+            if bom:
+                cost = self._compute_bom_custom_cost(
+                    bom, boms_to_recompute=boms_to_recompute, byproduct_bom=True
+                )
+                if cost:
+                    self.total_cost = cost
+
+    def _compute_bom_custom_cost(
+        self,
+        bom,
+        boms_to_recompute=False,
+        byproduct_bom=False,
+    ):
+        """
+        Compute the total cost based on the BoM, including child BoMs and byproducts.
+
+        This method computes the total cost for a product based on its BoM.
+        It takes into account all BoM lines, including nested child BoMs and any byproducts
+        that affect the pricing.
+        """
+        self.ensure_one()
+        if not bom:
+            return 0
+        if not boms_to_recompute:
+            boms_to_recompute = []
+        cost_total = 0
+
+        for line in bom.bom_line_ids:
+            if line._skip_bom_line(self):
+                continue
+
+            if line.child_bom_id and line.child_bom_id in boms_to_recompute:
+                child_cost_total = line.product_id._compute_bom_custom_cost(
+                    line.child_bom_id, boms_to_recompute=boms_to_recompute
+                )
+                cost_total += (
+                    line.product_id.uom_id._compute_price(
+                        child_cost_total, line.product_uom_id
+                    )
+                    * line.product_qty
+                )
+            else:
+                cost_total += (
+                    line.product_id.uom_id._compute_price(
+                        line.product_id.total_cost, line.product_uom_id
+                    )
+                    * line.product_qty
+                )
+        # Handle cost allocation for byproducts
+        if byproduct_bom:
+            byproduct_lines = bom.byproduct_ids.filtered(
+                lambda b: b.product_id == self and b.cost_share != 0
+            )
+            product_uom_qty = 0
+            for line in byproduct_lines:
+                product_uom_qty += line.product_uom_id._compute_quantity(
+                    line.product_qty, self.uom_id, round=False
+                )
+            byproduct_cost_share = sum(byproduct_lines.mapped("cost_share"))
+            if byproduct_cost_share and product_uom_qty:
+                return cost_total * byproduct_cost_share / 100 / product_uom_qty
+        else:
+            byproduct_cost_share = sum(bom.byproduct_ids.mapped("cost_share"))
+            if byproduct_cost_share:
+                cost_total *= float_round(
+                    1 - byproduct_cost_share / 100, precision_rounding=0.0001
+                )
+            return bom.product_uom_id._compute_price(
+                cost_total / bom.product_qty, self.uom_id
             )
 
     def _compute_product_price_extra(self):
@@ -255,7 +350,7 @@ class ProductProduct(models.Model):
         bom's with qty and compute from bom,
         extra prices with boms and qty.
         """
-        standard_products = self.filtered(lambda product: not product.config_ok)
+        standard_products = self.filtered(lambda product: not product.bom_ids)
         config_products = self - standard_products
 
         if standard_products:
