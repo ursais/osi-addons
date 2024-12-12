211b720e043726eb3f27970ec774aacef387b9be
Patrick Wilson <patrick.wilson@onlogic.com>
2024-12-12 15:57:41 -0800

    [ADD] ol_mrp_plm_purchase

---
+++ ol_mrp_plm_purchase/wizard/create_purchase_wizard.py
@@ -0,0 +1,24 @@
+from odoo import api, fields, models
+
+
+class PurchaseCreationWizard(models.TransientModel):
+    _name = "purchase.creation.wizard"
+    _description = "Wizard to create RFQ"
+
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Vendor",
+        required=True,
+    )
+    product_id = fields.Many2one(
+        "product.product",
+        string="Product",
+        required=True,
+        default=lambda self: self.env.context.get("default_product_id"),
+    )
+
+    def action_create_purchase(self):
+        self.ensure_one()
+        purchase = self.env["mrp.eco"].browse(self._context.get("active_id"))
+        action = purchase.action_create_purchase(self.partner_id, self.product_id)
+        return action
