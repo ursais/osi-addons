b2423889a7203e0748188bed06247978129640de
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2024-12-23 14:40:30 +0530

    [ADD] Add expected volumne to opportunity,Add sales teams and stages.

--- ol_crm/__manifest__.py
+++ ol_crm/__manifest__.py
@@ -16,6 +16,8 @@
     ],
     # Data Loaded.
     "data": [
+        "data/sale_team_data.xml",
+        "data/crm_stage_data.xml",
         "views/crm_lead_views.xml",
         "views/sale_estimate_views.xml",
     ],
