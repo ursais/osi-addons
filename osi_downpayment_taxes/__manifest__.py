08c2ee426b2c4e8713377bea24932df4ba103090
Patrick Wilson <patrick.wilson@onlogic.com>
2025-01-27 08:33:16 -0800

    [ADD] osi_fifo_serialized_fix/osi_avatax_rounding_fix/osi_downpayment_taxes

---
+++ osi_downpayment_taxes/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sales Downpayment Tax Fixes",
+    "summary": "Extends Sales Downpayments to fix sales lines made for multiple tax rates",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/ursais/osi-addons",
+    "category": "Accounting",
+    "depends": [
+        "account_accountant",
+        "sale",
+    ],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
