54bfe24e355774aff6da98a596ff1f17a3e7c496
Patrick Wilson <patrick.wilson@onlogic.com>
2025-02-07 15:18:27 -0800

    Remove Bom py

--- ol_mrp_traveler/models/__init__.py
+++ ol_mrp_traveler/models/__init__.py
@@ -1,8 +1,6 @@
 from . import mrp_assembly_check
 from . import mrp_assembly_stage
 from . import mrp_bom_line
-
-# from . import mrp_bom
 from . import product_attribute_classification
 from . import product_product
 from . import product_template
