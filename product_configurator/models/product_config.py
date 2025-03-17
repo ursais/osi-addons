7fcbe291177a0258257d085532cf23ea049c2f61
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-03-18 00:19:12 +0530

    [IMP]product_configurator: IMP code for search_variant method.

--- product_configurator/models/product_config.py
+++ product_configurator/models/product_config.py
@@ -1491,7 +1491,7 @@ class ProductConfigSession(models.Model):
         # in values, but it might have more attributes!  These are NOT
         # matches
         more_attrs = products.filtered(
-            lambda p: len(p.product_template_attribute_value_ids) != len(value_ids)
+            lambda p: len(p.product_template_attribute_value_ids.mapped("attribute_line_id").filtered(lambda l: l.active)) != len(value_ids)
         )
         products -= more_attrs
         return products
