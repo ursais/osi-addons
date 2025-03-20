e7be1e9fa5b2b0871cd799b1089d8e4adb84615b
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-03-20 17:14:28 +0530

    [IMP]UIMP Code.

--- ol_base/__manifest__.py
+++ ol_base/__manifest__.py
@@ -63,6 +63,7 @@
         "data/res_users_role.xml",
         "data/warehouse_data.xml",
         "data/m2x_create_edit_option_data.xml",
+        "data/stock.location.csv",
         "security/res_groups.xml",
         "views/res_company.xml",
         "views/stock_picking.xml",
