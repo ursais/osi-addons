9f15fa3625c1a6557056e4a5229469d4ebaa8ab9
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2025-04-01 10:18:42 -0600

    [FIX] purchase_import_supplier_cost_price

--- purchase_import_supplier_cost_price/__manifest__.py
+++ purchase_import_supplier_cost_price/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Update Supplier Cost Price from Spreadsheet",
     "version": "11.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "author": "Open Source Integrators",
     "maintainer": "Open Source Integrators",
-    "website": "https://www.opensourceintegrators.com",
+    "website": "https://github.com/ursais/osi-addons",
     "category": "Purchase",
     "images": [],
     "depends": [
