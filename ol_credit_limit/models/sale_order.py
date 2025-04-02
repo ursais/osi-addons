db8d9caa45f91a7d4751d7ca4a68a11c01fc016d
Patrick Wilson <patrick.wilson@onlogic.com>
2025-04-02 10:21:54 -0700

    [FIX] ol_credit_limit (Singleton Error)
    https://osi.mavenlink.com/workspaces/44078089/#tracker/919954424

--- ol_credit_limit/models/sale_order.py
+++ ol_credit_limit/models/sale_order.py
@@ -25,22 +25,23 @@ class SaleOrder(models.Model):
 
     # END #########
     # METHODS #####
-    def _get_open_sale_order(self, partner_id):
-        so_obj = self.env['sale.order']
-        if not partner_id:
-            return so_obj
 
+    def _get_open_sale_order(self, partner_ids):
+        so_obj = self.env["sale.order"]
+        if not partner_ids:
+            return so_obj
         query = """
             SELECT id
             FROM sale_order
-            WHERE partner_id = %s
+            WHERE partner_id IN %s
             AND invoice_status != 'invoiced'
             AND state != 'cancel'
         """
-        self.env.cr.execute(query, (partner_id.id,))
+        self.env.cr.execute(
+            query, (tuple(partner_ids.ids),)
+        )  # Ensure tuple format for SQL IN clause
         so_list = [so[0] for so in self.env.cr.fetchall()]
         return so_obj.browse(so_list)
-
 
     @api.depends(
         "partner_id.remaining_credit",
@@ -48,7 +49,7 @@ class SaleOrder(models.Model):
         "override_credit_limit_hold",
     )
     def _compute_credit_hold(self):
-        open_saleorders = self._get_open_sale_order(self.partner_id)
+        open_saleorders = self._get_open_sale_order(self.mapped("partner_id"))
         counter_total = 0
         credit_hold = False
         all_child = (
