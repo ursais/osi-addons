42a07f3af304bef09423a7bb4e8a478e7cb009e7
Patrick Wilson <patrick.wilson@onlogic.com>
2025-04-04 16:32:56 -0700

    [IMP] ol_purchase (Code Formatting)

--- ol_purchase/models/purchase_order.py
+++ ol_purchase/models/purchase_order.py
@@ -21,6 +21,7 @@ class PurchaseOrder(models.Model):
     # END ##########
 
     """Get reporting information from a purchase order"""
+
     def get_purchase_order_doc_data(self):
         """
         Get data for the purchase order doc report
@@ -33,15 +34,23 @@ class PurchaseOrder(models.Model):
         res = {
             # TODO: NC : "self.shipping_notes" field not found
             # 'shipping_notes': newline_to_br(self.shipping_notes) if self.shipping_notes else '',
-            'shipping_notes': '',
-            'supplier_notes': newline_to_br(self.notes) if self.notes else '',
-            'amount_untaxed': formatLang(self.env, self.amount_untaxed, currency_obj=self.currency_id),
-            'amount_tax': formatLang(self.env, self.amount_tax, currency_obj=self.currency_id),
-            'amount_total': formatLang(self.env, self.amount_total, currency_obj=self.currency_id),
-            'billing_name': billing_partner.commercial_company_name or billing_partner.name or '',
-            'billing_email': ap_contacts[0].email if ap_contacts else '',
-            'billing_address': newline_to_br(billing_partner.contact_address),
-            'shipping_address': newline_to_br(
+            "shipping_notes": "",
+            "supplier_notes": newline_to_br(self.notes) if self.notes else "",
+            "amount_untaxed": formatLang(
+                self.env, self.amount_untaxed, currency_obj=self.currency_id
+            ),
+            "amount_tax": formatLang(
+                self.env, self.amount_tax, currency_obj=self.currency_id
+            ),
+            "amount_total": formatLang(
+                self.env, self.amount_total, currency_obj=self.currency_id
+            ),
+            "billing_name": billing_partner.commercial_company_name
+            or billing_partner.name
+            or "",
+            "billing_email": ap_contacts[0].email if ap_contacts else "",
+            "billing_address": newline_to_br(billing_partner.contact_address),
+            "shipping_address": newline_to_br(
                 self.dest_address_id.contact_address
                 if self.dest_address_id
                 else billing_partner.contact_address
@@ -50,33 +59,43 @@ class PurchaseOrder(models.Model):
         terms = self.payment_term_id
         if not terms:
             terms = self.partner_id.property_supplier_payment_term_id
-        res['payment_terms'] = terms.name if terms else ''
+        res["payment_terms"] = terms.name if terms else ""
 
-        if self.state in ('draft', 'sent'):
+        if self.state in ("draft", "sent"):
             # If this is being sent as part of a RFQ email, this context is set to True
-            if self.env.context.get('send_rfq', False):
+            if self.env.context.get("send_rfq", False):
                 # the order is not in a confirmed state, but we're creating the doc as if it were
-                res['order_date'] = fields.Date.context_today(self)
-                res['submitted_by'] = self.env.user.name
+                res["order_date"] = fields.Date.context_today(self)
+                res["submitted_by"] = self.env.user.name
 
-        res['lines'] = []
+        res["lines"] = []
         for line in self.order_line:
             cur_tariff_code = line.product_id.tariff_code_id
-            tariff_code_line = ''
+            tariff_code_line = ""
             if cur_tariff_code.code:
-                tariff_code_line = f'{cur_tariff_code.type_id.label}: {cur_tariff_code.code}'
+                tariff_code_line = (
+                    f"{cur_tariff_code.type_id.label}: {cur_tariff_code.code}"
+                )
 
-            res['lines'].append(
+            res["lines"].append(
                 {
-                    'name': line.product_id.name,
-                    'sku': line.product_id.get_supplier_code(supplier=self.partner_id),
-                    'qty': line.product_qty,
-                    'unit_price': formatLang(self.env, line.price_unit, currency_obj=self.currency_id),
-                    'line_subtotal': formatLang(self.env, line.price_subtotal, currency_obj=self.currency_id),
+                    "name": line.product_id.name,
+                    "sku": line.product_id.get_supplier_code(supplier=self.partner_id),
+                    "qty": line.product_qty,
+                    "unit_price": formatLang(
+                        self.env, line.price_unit, currency_obj=self.currency_id
+                    ),
+                    "line_subtotal": formatLang(
+                        self.env, line.price_subtotal, currency_obj=self.currency_id
+                    ),
                     # TODO: NC : "line.note" field not found
                     # 'line_note': newline_to_br(line.note) if line.note else '',
-                    'line_note': newline_to_br(line.name) if line.display_type == 'line_note' else "",
-                    'product_tariff_code': tariff_code_line,
+                    "line_note": (
+                        newline_to_br(line.name)
+                        if line.display_type == "line_note"
+                        else ""
+                    ),
+                    "product_tariff_code": tariff_code_line,
                 }
             )
 
