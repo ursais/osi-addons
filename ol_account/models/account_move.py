8c5ab49181ee1e586f171061894ac4033cde52c9
Patrick Wilson <patrick.wilson@onlogic.com>
2025-04-04 16:06:41 -0700

    [IMP] ol_account (Code Formatting)

--- ol_account/models/account_move.py
+++ ol_account/models/account_move.py
@@ -85,14 +85,15 @@ class AccountMove(models.Model):
         self._auto_reconcile_deposits()
         return result
 
-
     def action_invoice_print(self):
         """
         Override original invoice template
         """
         self.ensure_one()
         self.is_move_sent = True
-        return self.env.ref('ol_account.action_generic_invoice_report').report_action(self)
+        return self.env.ref("ol_account.action_generic_invoice_report").report_action(
+            self
+        )
 
     def get_report_invoice_data(self):
         data = {}
@@ -103,17 +104,17 @@ class AccountMove(models.Model):
             if invoice.line_ids.sale_line_ids:
                 order_data = invoice.get_invoice_report_data_by_sale_order_lines()
 
-            data[invoice.id]['sale_order'] = order_data
+            data[invoice.id]["sale_order"] = order_data
 
         return data
 
     def get_invoice_report_data_by_sale_order_lines(self):
 
         order_data = {
-            'product_lines': [],
+            "product_lines": [],
         }
 
-        product_lines = self.env['sale.order.line']
+        product_lines = self.env["sale.order.line"]
 
         for invoice_line in self.invoice_line_ids:
             for sale_order_line in invoice_line.sale_line_ids:
@@ -136,20 +137,26 @@ class AccountMove(models.Model):
                 #     sorted_quote_lines = False
 
                 order_line_data = {
-                    'invoice_line': invoice_line,
-                    'order_line': sale_order_line,
-                    'quote_config': quote_config,
-                    'quote_lines': sorted_quote_lines,
+                    "invoice_line": invoice_line,
+                    "order_line": sale_order_line,
+                    "quote_config": quote_config,
+                    "quote_lines": sorted_quote_lines,
                 }
 
-                order_data['product_lines'].append(order_line_data)
+                order_data["product_lines"].append(order_line_data)
 
         # Set the shipping lines
-        shipping_lines = self.mapped('invoice_line_ids.sale_line_ids').filtered(lambda l: l.is_delivery)
+        shipping_lines = self.mapped("invoice_line_ids.sale_line_ids").filtered(
+            lambda l: l.is_delivery
+        )
 
-        order_data['shipping_lines'] = shipping_lines
-        order_data['product_subtotal_amount'] = sum(product_lines.mapped('price_subtotal'))
-        order_data['shipping_subtotal_amount'] = sum(shipping_lines.mapped('price_subtotal'))
+        order_data["shipping_lines"] = shipping_lines
+        order_data["product_subtotal_amount"] = sum(
+            product_lines.mapped("price_subtotal")
+        )
+        order_data["shipping_subtotal_amount"] = sum(
+            shipping_lines.mapped("price_subtotal")
+        )
 
         return order_data
 
@@ -159,25 +166,26 @@ class AccountMove(models.Model):
 
             filtered_invoice = invoice
             filtered_invoice_data = {
-                'sum_amount': invoice.amount_total,
-                'out_invoice_amount': None,
-                'out_refund_amount': None,
-                'payment_received_amount': (invoice.amount_total - invoice.amount_residual),
-                'residual_amount': invoice.amount_residual,
-                'type': invoice.get_report_title(),
+                "sum_amount": invoice.amount_total,
+                "out_invoice_amount": None,
+                "out_refund_amount": None,
+                "payment_received_amount": (
+                    invoice.amount_total - invoice.amount_residual
+                ),
+                "residual_amount": invoice.amount_residual,
+                "type": invoice.get_report_title(),
             }
 
-        res = {'invoice': filtered_invoice, 'invoice_data': filtered_invoice_data}
+        res = {"invoice": filtered_invoice, "invoice_data": filtered_invoice_data}
 
         return res
 
     def get_report_title(self):
         """Return the report title defined by marketing"""
 
-        if self.move_type == 'out_refund':
-            return 'Refund'
+        if self.move_type == "out_refund":
+            return "Refund"
 
-        return 'Invoice'
-
+        return "Invoice"
 
     # END ##########
