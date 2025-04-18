c36d88ee94941b159adadb5426e2cfc07d52e614
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-04-18 14:59:46 +0530

    [FIX] Currency Base on Company.

--- ol_product_configurator/models/product_config.py
+++ ol_product_configurator/models/product_config.py
@@ -18,3 +18,13 @@ class ProductConfigSession(models.Model):
     )
 
     # END ##########
+    # METHODS #####
+    def _compute_currency_id(self):
+        super()._compute_currency_id()
+        """Super call the method to set the currency base on current company Currency."""
+        main_company = self.env["res.company"]._get_main_company()
+        for session in self:
+            if session.product_tmpl_id.currency_id:
+                session.currency_id = session.product_tmpl_id.currency_id.id
+
+    # END #########
