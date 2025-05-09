5a2f370f1968ab6e75c78d1f35bb09ed72174552
Patrick Wilson <patrick.wilson@onlogic.com>
2025-05-09 16:03:48 -0700

    [IMP] ol_account_hot_ar (Cron and Check Exceptions)

--- ol_account_hot_ar/models/account_move.py
+++ ol_account_hot_ar/models/account_move.py
@@ -45,7 +45,51 @@ class AccountMove(models.Model):
                 + timedelta(days=invoice.company_id.hot_ar_grace_period)
                 < today
             ):
-                invoice.hot_ar = True
+                invoice.write({"hot_ar": True})
+
+                # Trigger exception checks on the Sale Orders
+                sale_orders = invoice.mapped("invoice_line_ids.sale_line_ids.order_id")
+                sale_orders = sale_orders.filtered(
+                    lambda so: so.state not in ("cancel", "done")
+                )
+                sale_lines = invoice.mapped("invoice_line_ids.sale_line_ids")
+                sale_lines = sale_lines.filtered(
+                    lambda sl: sl.order_id.state not in ("cancel", "done")
+                )
+                if sale_orders:
+                    sale_orders.detect_exceptions()
+
+                # Trigger exception checks on Manufacturing Orders
+                production_orders = (
+                    self.env["mrp.production"]
+                    .sudo()
+                    .search(
+                        [
+                            "|",
+                            ("sale_order_line_id", "in", sale_lines.ids),
+                            ("sale_order_id", "in", sale_orders.ids),
+                            ("state", "not in", ("done", "cancel")),
+                        ]
+                    )
+                )
+
+                if production_orders:
+                    production_orders.detect_exceptions()
+
+                # Trigger exception checks on Stock Transfer
+                transfers = (
+                    self.env["stock.picking"]
+                    .sudo()
+                    .search(
+                        [
+                            ("sale_id", "in", sale_orders.ids),
+                            ("state", "not in", ("done", "cancel")),
+                        ]
+                    )
+                )
+
+                if transfers:
+                    transfers.detect_exceptions()
 
     @api.model
     def _update_hot_ar_invoices_cron(self):
@@ -91,5 +135,49 @@ class AccountMove(models.Model):
                     )
                 )
                 invoices.sudo().write({"hot_ar": True})
+
+                # Trigger exception checks on the Sale Orders
+                sale_orders = invoices.mapped("invoice_line_ids.sale_line_ids.order_id")
+                sale_orders = sale_orders.filtered(
+                    lambda so: so.state not in ("cancel", "done")
+                )
+                sale_lines = invoices.mapped("invoice_line_ids.sale_line_ids")
+                sale_lines = sale_lines.filtered(
+                    lambda sl: sl.order_id.state not in ("cancel", "done")
+                )
+                if sale_orders:
+                    sale_orders.detect_exceptions()
+
+                # Trigger exception checks on Manufacturing Orders
+                production_orders = (
+                    self.env["mrp.production"]
+                    .sudo()
+                    .search(
+                        [
+                            "|",
+                            ("sale_order_line_id", "in", sale_lines.ids),
+                            ("sale_order_id", "in", sale_orders.ids),
+                            ("state", "not in", ("done", "cancel")),
+                        ]
+                    )
+                )
+
+                if production_orders:
+                    production_orders.detect_exceptions()
+
+                # Trigger exception checks on Stock Transfer
+                transfers = (
+                    self.env["stock.picking"]
+                    .sudo()
+                    .search(
+                        [
+                            ("sale_id", "in", sale_orders.ids),
+                            ("state", "not in", ("done", "cancel")),
+                        ]
+                    )
+                )
+
+                if transfers:
+                    transfers.detect_exceptions()
 
     # END #########
