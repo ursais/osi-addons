22930f55d90ff0a428c3eedaf4740aac4f48fdcb
Patrick Wilson <patrick.wilson@onlogic.com>
2025-05-16 16:13:03 -0700

    [IMP] mrp_batch (Split transfers on MO and Batch)

--- mrp_batch/models/sale_order.py
+++ mrp_batch/models/sale_order.py
@@ -54,7 +54,6 @@ class SaleOrder(models.Model):
             existing_batch_id = None
 
             if batch_mode == "single":
-                # Create a single batch for all MOs in this record
                 existing_batch_id = batch_obj.create(
                     {"responsible_id": rec.env.user.id}
                 )
@@ -66,26 +65,83 @@ class SaleOrder(models.Model):
                 ):
                     qty = mo.product_qty
                     if qty > 1:
-                        # Perform the split and get new MOs
-                        new_mos = mo.sudo()._split_productions({mo: ([1] * int(qty))})[
+                        # Store original pickings BEFORE split
+                        original_pickings = mo.picking_ids.filtered(
+                            lambda p: p.state not in ["done", "cancel"]
+                        )
+
+                        # Split into MOs
+                        new_mos = mo.sudo()._split_productions({mo: [1] * int(qty)})[
                             :-1
                         ]
 
-                        # Assign the batch to newly created MOs
                         for new_mo in new_mos:
-                            new_mo.write(
-                                {
-                                    "mrp_batch_id": (
-                                        existing_batch_id.id
-                                        if batch_mode == "single"
-                                        else batch_obj.create(
-                                            {"responsible_id": rec.env.user.id}
-                                        ).id
-                                    )
-                                }
+                            new_batch_id = (
+                                existing_batch_id.id
+                                if batch_mode == "single"
+                                else batch_obj.create(
+                                    {"responsible_id": rec.env.user.id}
+                                ).id
                             )
+                            new_mo.write({"mrp_batch_id": new_batch_id})
 
-                    # Assign batch to original MO and its backorders
+                            # --- Split picking for this MO ---
+                            for picking in original_pickings:
+                                # Create new picking
+                                new_picking = picking.copy(
+                                    {
+                                        "origin": new_mo.name,
+                                        "group_id": new_mo.procurement_group_id.id,
+                                        "move_ids": [],
+                                        "move_ids_without_package": False,
+                                    }
+                                )
+
+                                for move in picking.move_ids:
+                                    # Use the ratio of original qty to determine component qty
+                                    if not move.product_uom_qty or not mo.product_qty:
+                                        continue
+
+                                    per_unit_qty = move.product_uom_qty / mo.product_qty
+
+                                    new_move = self.env["stock.move"].create(
+                                        {
+                                            "name": move.name,
+                                            "product_id": move.product_id.id,
+                                            "product_uom": move.product_uom.id,
+                                            "product_uom_qty": per_unit_qty,
+                                            "picking_id": new_picking.id,
+                                            "location_id": move.location_id.id,
+                                            "location_dest_id": move.location_dest_id.id,
+                                            "raw_material_production_id": new_mo.id,
+                                            "state": "draft",
+                                        }
+                                    )
+
+                                new_picking.action_confirm()
+                                new_picking.action_assign()
+
+                                # Add to stock picking batch
+                                batch = self.env["stock.picking.batch"].search(
+                                    [
+                                        (
+                                            "picking_type_id",
+                                            "=",
+                                            new_picking.picking_type_id.id,
+                                        ),
+                                        ("state", "=", "draft"),
+                                    ],
+                                    limit=1,
+                                )
+                                if not batch:
+                                    batch = self.env["stock.picking.batch"].create(
+                                        {
+                                            "picking_type_id": new_picking.picking_type_id.id,
+                                        }
+                                    )
+                                batch.picking_ids = [(4, new_picking.id)]
+
+                    # Assign batch to original MO and backorders
                     mo.write(
                         {
                             "mrp_batch_id": (
@@ -98,6 +154,61 @@ class SaleOrder(models.Model):
                         }
                     )
                     mo.backorder_ids.write({"mrp_batch_id": mo.mrp_batch_id.id})
+
+    # def split_mo(self):
+    #     batch_obj = self.env["mrp.production.batch"]
+    #     batch_mode = (
+    #         self.env["ir.config_parameter"].sudo().get_param("mrp_batch.batch_mode")
+    #     )
+
+    #     for rec in self:
+    #         existing_batch_id = None
+
+    #         if batch_mode == "single":
+    #             # Create a single batch for all MOs in this record
+    #             existing_batch_id = batch_obj.create(
+    #                 {"responsible_id": rec.env.user.id}
+    #             )
+
+    #         for mo in rec.mrp_production_ids:
+    #             if (
+    #                 mo.product_id.tracking == "serial"
+    #                 and mo.product_id.is_allow_split_mo
+    #             ):
+    #                 qty = mo.product_qty
+    #                 if qty > 1:
+    #                     # Perform the split and get new MOs
+    #                     new_mos = mo.sudo()._split_productions({mo: ([1] * int(qty))})[
+    #                         :-1
+    #                     ]
+
+    #                     # Assign the batch to newly created MOs
+    #                     for new_mo in new_mos:
+    #                         new_mo.write(
+    #                             {
+    #                                 "mrp_batch_id": (
+    #                                     existing_batch_id.id
+    #                                     if batch_mode == "single"
+    #                                     else batch_obj.create(
+    #                                         {"responsible_id": rec.env.user.id}
+    #                                     ).id
+    #                                 )
+    #                             }
+    #                         )
+
+    #                 # Assign batch to original MO and its backorders
+    #                 mo.write(
+    #                     {
+    #                         "mrp_batch_id": (
+    #                             existing_batch_id.id
+    #                             if batch_mode == "single"
+    #                             else batch_obj.create(
+    #                                 {"responsible_id": rec.env.user.id}
+    #                             ).id
+    #                         )
+    #                     }
+    #                 )
+    #                 mo.backorder_ids.write({"mrp_batch_id": mo.mrp_batch_id.id})
 
     # Methods for Batch Smart Button
     def _compute_mrp_production_batch_id_count(self):
