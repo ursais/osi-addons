7e4eba650d193d7d4dba4414503b268599f65a91
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-05-20 14:39:10 +0530

    [IMP] Code.

--- osi_ol_decryption/models/ir_server_action.py
+++ osi_ol_decryption/models/ir_server_action.py
@@ -42,9 +42,9 @@ class IrActionsServer(models.Model):
                 AND tab.table_name NOT LIKE 'mail%'
                 AND col.table_name NOT LIKE 'report%'
                 --and col.column_name in ('vat')
-                -- AND col.table_name in ('res_partner')
+                -- AND col.table_name in ()
                 --AND col.table_name ~* '^[p-zP-Z]'
-                AND col.table_name NOT IN ('res_config_settings', 'res_groups', 'res_lang','account_invoice_extract_words')
+                AND col.table_name NOT IN ('res_config_settings', 'res_lang','account_invoice_extract_words', 'mrp_bom_line','stock_lot')
                 ORDER BY col.table_name, col.ordinal_position;"""
         )
         table_columns_list_set = self.env.cr.fetchall()
@@ -55,11 +55,10 @@ class IrActionsServer(models.Model):
                 table_columns_dict[table].append(column)
             else:
                 table_columns_dict[table] = [column]
-        
+
         # Line use to find the non decrepated data and use to decrpitions
         if all_data:
             table_columns_dict = self.get_non_decrpted_data()
-        
 
         runningLog += "\n MASTER TABLE GROUPED DICT: %s" % (str(table_columns_dict))
         excluded_tables_columns = {
@@ -69,10 +68,7 @@ class IrActionsServer(models.Model):
             "res_partner": [
                 "contact_address_complete",
             ],
-            "account_move": [
-                "sequence_prefix",
-                "invoice_partner_display_name"
-            ],
+            "account_move": ["sequence_prefix", "invoice_partner_display_name"],
         }
         columns_missing = []
         tables_missing = []
@@ -152,9 +148,11 @@ class IrActionsServer(models.Model):
                 # )
                 continue
             batch_size = 100000
-            if table in ('project_task'):
+            if table in ("project_task"):
                 batch_size = 10000
             offset = 0
+            if table == 'account_move_line':
+                offset = 7400000
             self.env.cr.execute("select count(*) from %s" % (table))
             records = self.env.cr.fetchall()
             _logger.info(
@@ -162,6 +160,7 @@ class IrActionsServer(models.Model):
                 % (table, str(columns_exists), records[0][0])
             )
             offset_limit = records[0][0] + 1000
+            _logger.info("\n offset_limitoffset_limit %s" % (offset_limit))
             while True:
                 query = "SELECT id, {}  FROM {} ORDER BY id  LIMIT {} OFFSET {}".format(
                     ", ".join(['"%s"' % c for c in columns_exists]),
@@ -178,7 +177,7 @@ class IrActionsServer(models.Model):
                 # if not table_data:
                 #     break  # Exit the loop if there are no more records to process
                 # print ("\n table_data", table_data)
-                if table == 'stock_move':
+                if table == "stock_move":
                     if offset > offset_limit:
                         break
                 else:
@@ -470,9 +469,9 @@ class IrActionsServer(models.Model):
             "res_partner": [
                 "contact_address_complete",
             ],
-             "account_move": [
-                 "sequence_prefix",
-             ],
+            "account_move": [
+                "sequence_prefix",
+            ],
         }
         columns_missing = []
         tables_missing = []
