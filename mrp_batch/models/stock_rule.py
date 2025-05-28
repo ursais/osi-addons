2b185d65797de786bfa3e7771c4c52541ed78037
Chankya Soni <csoni@opensourceintegrators.com>
2025-05-28 21:02:15 +0000

    [IMP] Separate pick components for all split MOs

--- mrp_batch/models/stock_rule.py
+++ mrp_batch/models/stock_rule.py
@@ -80,4 +80,12 @@ class StockRule(models.Model):
                     sale_order.split_mo()
         return res
                 
+    def _should_auto_confirm_procurement_mo(self, p):        
+        auto_confirm_mo = self.env['ir.config_parameter'].sudo().get_param(
+            'auto_confirm_mo', ''
+        )
+        if auto_confirm_mo == "True":
+           return super()._should_auto_confirm_procurement_mo(p)
+        else: 
+           return False
     # # END #########
