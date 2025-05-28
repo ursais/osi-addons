83624e74698ec4924b8ff1366c5d84e24b4a29d9
Chankya Soni <csoni@opensourceintegrators.com>
2025-05-28 21:28:30 +0000

    [REM] Removed auto confirm MO code

--- mrp_batch/models/stock_rule.py
+++ mrp_batch/models/stock_rule.py
@@ -79,13 +79,5 @@ class StockRule(models.Model):
                 else:
                     sale_order.split_mo()
         return res
-                
-    def _should_auto_confirm_procurement_mo(self, p):        
-        auto_confirm_mo = self.env['ir.config_parameter'].sudo().get_param(
-            'auto_confirm_mo', ''
-        )
-        if auto_confirm_mo == "True":
-           return super()._should_auto_confirm_procurement_mo(p)
-        else: 
-           return False
+
     # # END #########
