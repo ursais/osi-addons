e1350c278a73c8dc7776d879947152b63f2cf5d0
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-05-30 17:34:52 +0530

    [IMP]Allow net terms checkout.

--- ol_account/models/res_partner.py
+++ ol_account/models/res_partner.py
@@ -12,5 +12,18 @@ class ResPartner(models.Model):
         string="Payment Preference",
         company_dependent=True,
     )
+    net_terms_allowed = fields.Boolean(compute="_compute_net_terms_allowed")
+    net_terms_active = fields.Boolean(string="Frontend Net Terms Active")
+
+    # END #########
+    # Method #########
+
+    def _compute_net_terms_allowed(self):
+        """Enable the partner is allowed to the net terms; This is only Used on Frontend eCommerce."""
+        for partner in self:
+            net_terms_allowed = False
+            if partner.commercial_partner_id.property_payment_term_id:
+                net_terms_allowed = True
+            partner.net_terms_allowed = net_terms_allowed
 
     # END #########
