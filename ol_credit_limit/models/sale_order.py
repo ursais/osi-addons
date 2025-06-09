d2a7ba7ea102dd26cdf20a92b622ac9c87a291f6
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-06-09 17:52:40 +0530

    [IMP]IMP Code.

--- ol_credit_limit/models/sale_order.py
+++ ol_credit_limit/models/sale_order.py
@@ -73,108 +73,109 @@ class SaleOrder(models.Model):
         "override_credit_limit_hold",
     )
     def _compute_credit_hold(self):
-        open_saleorders = self._get_open_sale_order(self.mapped("partner_id"))
-        _logger.info("_compute_credit_hold %s", self.mapped("partner_id"))
-        open_so_invoices = self.env['sale.order']
-        paid_so_invoices = self.env['sale.order']
-        cr = self.env.cr
-        counter_total = 0
-        credit_hold = False
-        all_child = (
-            self.env["res.partner"]
-            .with_context(active_test=False)
-            .search([("id", "child_of", self.partner_id.ids)])
-        )
-        query = """
-            SELECT id
-            FROM account_move
-            WHERE move_type = 'out_invoice'
-            AND partner_id IN %s
-            AND state != 'cancel'
-        """
-        cr.execute(query, (tuple(all_child.ids),))  # Wrap in tuple
-        datas = cr.fetchall()
-        not_paid_invoices = [row[0] for row in datas]
-
-        not_paid_amls = False
-        sols = False
-
-        if not_paid_invoices:
-            open_so_invoices = self._get_sale_order_id(not_paid_invoices)
-        query = """
-            SELECT id
-            FROM account_move
-            WHERE move_type = 'out_invoice'
-            AND state != 'cancel'
-            AND partner_id IN %s
-            AND payment_state IN %s
-        """
-        move_ids = tuple(not_paid_invoices) or (0,)
-        partns = tuple(all_child.ids)
-        payment_states = ('in_payment', 'paid')
-        cr.execute(query, (partns, payment_states))
-        datas = cr.fetchall()
-        paid_invoices = [row[0] for row in datas]
-        if paid_invoices:
-            paid_so_invoices = self._get_sale_order_id(paid_invoices)
-
-        saleorders = open_saleorders + open_so_invoices
-        sorted_orders_asc = (
-            self.env["sale.order"]
-            .browse(saleorders.ids)
-            .filtered(lambda l: l.original_request_date)
-            .sorted("original_request_date")
-        )
-        counter_total = 0
-        for order in sorted_orders_asc:
-            if order.id not in paid_so_invoices.ids:
-                counter_total += order.amount_total
+        for sale in self:
+            open_saleorders = self._get_open_sale_order(sale.mapped("partner_id"))
+            _logger.info("_compute_credit_hold %s", self.mapped("partner_id"))
+            open_so_invoices = self.env['sale.order']
+            paid_so_invoices = self.env['sale.order']
+            cr = self.env.cr
+            counter_total = 0
             credit_hold = False
-            if counter_total > order.partner_id.credit_limit:
-                credit_hold = True
-            if order.override_credit_limit_hold:
+            all_child = (
+                self.env["res.partner"]
+                .with_context(active_test=False)
+                .search([("id", "child_of", self.partner_id.ids)])
+            )
+            query = """
+                SELECT id
+                FROM account_move
+                WHERE move_type = 'out_invoice'
+                AND partner_id IN %s
+                AND state != 'cancel'
+            """
+            cr.execute(query, (tuple(all_child.ids),))  # Wrap in tuple
+            datas = cr.fetchall()
+            not_paid_invoices = [row[0] for row in datas]
+
+            not_paid_amls = False
+            sols = False
+
+            if not_paid_invoices:
+                open_so_invoices = self._get_sale_order_id(not_paid_invoices)
+            query = """
+                SELECT id
+                FROM account_move
+                WHERE move_type = 'out_invoice'
+                AND state != 'cancel'
+                AND partner_id IN %s
+                AND payment_state IN %s
+            """
+            move_ids = tuple(not_paid_invoices) or (0,)
+            partns = tuple(all_child.ids)
+            payment_states = ('in_payment', 'paid')
+            cr.execute(query, (partns, payment_states))
+            datas = cr.fetchall()
+            paid_invoices = [row[0] for row in datas]
+            if paid_invoices:
+                paid_so_invoices = self._get_sale_order_id(paid_invoices)
+
+            saleorders = open_saleorders + open_so_invoices
+            sorted_orders_asc = (
+                self.env["sale.order"]
+                .browse(saleorders.ids)
+                .filtered(lambda l: l.original_request_date)
+                .sorted("original_request_date")
+            )
+            counter_total = 0
+            for order in sorted_orders_asc:
+                if order.id not in paid_so_invoices.ids:
+                    counter_total += order.amount_total
                 credit_hold = False
-            if order.id in paid_so_invoices.ids:
-                credit_hold = False
-            order.credit_hold = credit_hold
+                if counter_total > order.partner_id.credit_limit:
+                    credit_hold = True
+                if order.override_credit_limit_hold:
+                    credit_hold = False
+                if order.id in paid_so_invoices.ids:
+                    credit_hold = False
+                order.credit_hold = credit_hold
 
 
-        # not_paid_invoices = self.env["account.move"].search(
-        #     [
-        #         ("move_type", "=", "out_invoice"),
-        #         ("partner_id", "in", all_child.ids),
-        #         ("state", "!=", "cancel"),
-        #     ]
-        # )
-        # open_so_invoices = not_paid_invoices.mapped("line_ids.sale_line_ids.order_id")
-        # paid_invoices = self.env["account.move"].search(
-        #     [
-        #         ("move_type", "=", "out_invoice"),
-        #         ("partner_id", "in", all_child.ids),
-        #         ("state", "!=", "cancel"),
-        #         ("payment_state", "in", ["in_payment", "paid"]),
-        #     ]
-        # )
-        # paid_so_invoices = paid_invoices.mapped("line_ids.sale_line_ids.order_id")
-        # saleorders = open_saleorders + open_so_invoices
-        # sorted_orders_asc = (
-        #     self.env["sale.order"]
-        #     .browse(saleorders.ids)
-        #     .filtered(lambda l: l.original_request_date)
-        #     .sorted("original_request_date")
-        # )
-        # counter_total = 0
-        # for order in sorted_orders_asc:
-        #     if order.id not in paid_so_invoices.ids:
-        #         counter_total += order.amount_total
-        #     credit_hold = False
-        #     if counter_total > order.partner_id.credit_limit:
-        #         credit_hold = True
-        #     if order.override_credit_limit_hold:
-        #         credit_hold = False
-        #     if order.id in paid_so_invoices.ids:
-        #         credit_hold = False
-        #     order.credit_hold = credit_hold
+            # not_paid_invoices = self.env["account.move"].search(
+            #     [
+            #         ("move_type", "=", "out_invoice"),
+            #         ("partner_id", "in", all_child.ids),
+            #         ("state", "!=", "cancel"),
+            #     ]
+            # )
+            # open_so_invoices = not_paid_invoices.mapped("line_ids.sale_line_ids.order_id")
+            # paid_invoices = self.env["account.move"].search(
+            #     [
+            #         ("move_type", "=", "out_invoice"),
+            #         ("partner_id", "in", all_child.ids),
+            #         ("state", "!=", "cancel"),
+            #         ("payment_state", "in", ["in_payment", "paid"]),
+            #     ]
+            # )
+            # paid_so_invoices = paid_invoices.mapped("line_ids.sale_line_ids.order_id")
+            # saleorders = open_saleorders + open_so_invoices
+            # sorted_orders_asc = (
+            #     self.env["sale.order"]
+            #     .browse(saleorders.ids)
+            #     .filtered(lambda l: l.original_request_date)
+            #     .sorted("original_request_date")
+            # )
+            # counter_total = 0
+            # for order in sorted_orders_asc:
+            #     if order.id not in paid_so_invoices.ids:
+            #         counter_total += order.amount_total
+            #     credit_hold = False
+            #     if counter_total > order.partner_id.credit_limit:
+            #         credit_hold = True
+            #     if order.override_credit_limit_hold:
+            #         credit_hold = False
+            #     if order.id in paid_so_invoices.ids:
+            #         credit_hold = False
+            #     order.credit_hold = credit_hold
 
     @api.depends("amount_total", "invoice_status")
     def _compute_uninvoiced_balance(self):
