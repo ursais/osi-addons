e0a18b26d4947ee666b226f18a5b9ddb7eee6c9d
Patrick Wilson <patrick.wilson@onlogic.com>
2025-06-19 16:59:14 -0700

    [UPD] operations_multi (Exclude Field)

--- ol_mrp_operations_multi_company/models/mrp_routing_workcenter.py
+++ ol_mrp_operations_multi_company/models/mrp_routing_workcenter.py
@@ -32,3 +32,36 @@ class MrpRoutingWorkcenter(models.Model):
     )
 
     # END #########
+    # METHODS #####
+
+    def write(self, vals):
+        # Only trigger outdated BOM if fields other than time_cycle_manual were changed
+        # Add other fields as needed
+        safe_fields = {
+            "time_cycle_manual",
+        }
+        trigger_bom_flag = not set(vals.keys()).issubset(safe_fields)
+
+        # If bom_id is changed, clear operation links before write
+        if "bom_id" in vals:
+            for op in self:
+                op.bom_id.bom_line_ids.filtered(
+                    lambda line: line.operation_id == op
+                ).operation_id = False
+                op.bom_id.byproduct_ids.filtered(
+                    lambda byproduct: byproduct.operation_id == op
+                ).operation_id = False
+                op.bom_id.operation_ids.filtered(
+                    lambda operation: operation.blocked_by_operation_ids == op
+                ).blocked_by_operation_ids = False
+
+        # Perform the actual write
+        res = super().write(vals)
+
+        # Mark MOs outdated *only if necessary*
+        if trigger_bom_flag:
+            self.bom_id._set_outdated_bom_in_productions()
+
+        return res
+
+    # END #########
