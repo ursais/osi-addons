5c5732ec13aaad0afb9b0bf295d10266b007f821
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-06-20 16:27:10 +0530

    [IMP]set_visibility_based_on_sale_description

--- ol_sale/models/product_template_attribute_line.py
+++ ol_sale/models/product_template_attribute_line.py
@@ -24,4 +24,23 @@ class ProductTemplateAttributeLine(models.Model):
             # Set the value from the related product.attribute field
             self.used_in_sale_description = self.attribute_id.used_in_sale_description
 
+    def _set_visibility_based_on_sale_description(self):
+        # Utility method to set visibility based on 'used_in_sale_description'
+        visible = self.used_in_sale_description
+        self.product_template_value_ids.write({"visible_to_user": visible})
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        results = super().create(vals_list)
+        for line in results:
+            line._set_visibility_based_on_sale_description()
+        return results
+
+    def write(self, values):
+        results = super().write(values)
+        # Apply changes only if 'used_in_sale_description' has been modified
+        if "used_in_sale_description" in values:
+            self._set_visibility_based_on_sale_description()
+        return results
+
     # END #########
