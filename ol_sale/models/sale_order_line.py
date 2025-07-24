e06980f9104ac9abaf856450abef85e59666b7fe
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-07-24 16:50:33 +0530

    [ECH] Ability to duplicate a line item on an SO and BO.

--- ol_sale/models/sale_order_line.py
+++ ol_sale/models/sale_order_line.py
@@ -141,12 +141,26 @@ class SaleOrderLine(models.Model):
         self.with_context(from_unlink=True).update_crm_tag_sale_order()
         return super().unlink()
 
-
     def get_line_serial_numbers(self):
         """
         Get a list of serial numbers for this sale line
         """
         self.ensure_one()
 
-        return self.move_ids.move_line_ids.mapped('lot_id.name')
+        return self.move_ids.move_line_ids.mapped("lot_id.name")
+
+    def action_duplicate_line(self):
+        """Method that allows a user to duplicate the sale order line."""
+        for line in self:
+            values = line.copy_data(
+                {
+                    "order_id": line.order_id.id,
+                    "config_session_id": (
+                        line.config_session_id.id if line.config_session_id else False
+                    ),
+                    "bom_id": line.bom_id.id if line.bom_id else False,
+                }
+            )
+            self.create(values)
+
     # END #########
