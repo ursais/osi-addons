be237850a0c9766b2c429a8b694640fc079827be
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-08-19 08:18:46 +0000

    [IMP] Imp the code

--- osi_ol_decryption/models/ir_server_action_1.py
+++ osi_ol_decryption/models/ir_server_action_1.py
@@ -63,9 +63,9 @@ class IrActionsServer(models.Model):
             else:
                 name = payment_methods.get(data.get('method_id'))
             
-            if name == 'Custom':
-                payment = new_payment_data.filtered(lambda l: l.name == 'Custom')
-            elif name == 'Net Terms':
+            # if name == 'Custom':
+            #     payment = new_payment_data.filtered(lambda l: l.name == 'Custom')
+            if name == 'Net Terms':
                 payment = new_payment_data.filtered(lambda l: l.name == 'Payment Terms')
             elif name in ('Credit Card Prepayment', 'Credit Card'):
                 payment = new_payment_data.filtered(lambda l: l.name == 'Card')
