ef5767b5e569a8739865a4a73b1ab604a40d21b1
Patrick Wilson <patrick.wilson@onlogic.com>
2025-08-21 13:51:32 -0700

    ol_stock code formatting

--- ol_stock/models/stock_orderpoint.py
+++ ol_stock/models/stock_orderpoint.py
@@ -1,14 +1,19 @@
+# Import Odoo Libs
 from odoo import fields, models
 
 
 class StockOrderpoint(models.Model):
+    """Inherit Order Point to add overflow bool and cron."""
+
     _inherit = "stock.warehouse.orderpoint"
 
     # COLUMNS #####
-    is_overflow = fields.Boolean(string="Is Overflow")
-    # END #########
 
+    is_overflow = fields.Boolean(string="Is Overflow")
+
+    # END #########
     # METHODS ######
+
     def _cron_run_overflow_replenishments(self):
         domain = [("is_overflow", "!=", False), ("active", "=", True)]
         orderpoints = self.search(domain)
