45baf7963a5c49c4751284d649f6c57acadfde9d
Patrick Wilson <patrick.wilson@onlogic.com>
2025-09-05 16:28:51 -0700

    [FIX] ol_bank_transfer_email (Workflow Fix)
    https://osi.mavenlink.com/workspaces/44078089/#tracker/932422256

--- ol_bank_transfer_email/models/sale_order.py
+++ ol_bank_transfer_email/models/sale_order.py
@@ -14,21 +14,43 @@ class SaleOrder(models.Model):
     # END #########
     # METHOD #####
 
+    # Exceptions that are "ok" and should not block email sending
+    ALLOWED_EXCEPTIONS = [
+        "ol_sale.exception_so_bank_tranfer_payment",
+    ]
+
     def write(self, vals):
+        """Override write to send email if no blocking exceptions."""
         res = super().write(vals)
         order_review = self.env.ref(
             "ol_sale_substate.base_substate__order_review", raise_if_not_found=True
         )
+
         for order in self.filtered(
             lambda l: not l.payment_method_mail_send
             and l.substate_id.id == order_review.id
             and l.sale_payment_method_id.sale_email_template_id
         ):
-            if not order.detect_exceptions():
+            rule_ids = order.detect_exceptions() or []
+
+            # Browse the exception rules
+            rules = self.env["exception.rule"].browse(rule_ids)
+
+            # Map rule_id -> xml_id
+            xml_ids_map = rules.get_external_id()
+
+            # Check which rules are NOT in whitelist
+            blocking = [
+                r_id
+                for r_id in rule_ids
+                if xml_ids_map.get(r_id) not in self.ALLOWED_EXCEPTIONS
+            ]
+
+            if not blocking:
                 template = order.sale_payment_method_id.sale_email_template_id
                 template.send_mail(order.id, force_send=True)
-
                 order.write({"payment_method_mail_send": True})
+
         return res
 
     # END #########
