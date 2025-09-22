abb10481d9c9fbc459eb811f343ced8571f6d7fd
Vandan-OSI <vpandeji@opensourceintegrators.com>
2025-09-22 15:29:09 +0530

    [IMP]Accounts Receivable type contacts on a VENDOR receive payment remittance emails
    [IMP]IMP Code.

---
+++ ol_account/models/account_batch_payment.py
@@ -0,0 +1,23 @@
+# Copyright 2025, AUTHOR(S)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class AccountBatchPayment(models.Model):
+    _inherit = "account.batch.payment"
+
+
+    def action_send_detailed_payment_emails(self):
+        for batch in self:
+            for payment in batch.payment_ids:
+                if payment.partner_id and not payment.partner_id.ar:
+                    raise ValidationError(
+                        _(
+                            "AR is not set on : %s in batch: %s"
+                        )
+                        % (payment.partner_id.display_name, batch.name)
+                    )
+
+        return super().action_send_detailed_payment_emails()
