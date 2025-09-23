9739ace16662f747b7de38df43f46d350b5c3b8a
Patrick Wilson <patrick.wilson@onlogic.com>
2025-09-23 09:21:36 -0700

    Update onlogic_schema.py

--- ol_graphql/schema/onlogic_schema.py
+++ ol_graphql/schema/onlogic_schema.py
@@ -64,7 +64,7 @@ class GQLLoader:
     def __init__(self):
         os.chdir("../")
         # self.onlogic_modules = f"/odoo/addons/private-addons/onlogic-addons"
-        self.onlogic_modules = f"/odoo/addons/"  # OSI DEMO ONLY
+        self.onlogic_modules = f"/opt/odoo/osi-addons"  # OSI DEMO ONLY
         self.directories_to_exclude = ["__pycache__", "_base"]
         self.modules = self.get_modules()
         # Set up the base data
