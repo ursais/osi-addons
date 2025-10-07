e21d6ea0fc13555c9a38c2005844c0724b5ac52f
Patrick Wilson <patrick.wilson@onlogic.com>
2025-10-07 11:41:49 -0700

    Update attribute_attribute.py

--- ol_pim/models/attribute_attribute.py
+++ ol_pim/models/attribute_attribute.py
@@ -1,5 +1,6 @@
 # Import Odoo libs
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class AttributeAttribute(models.Model):
@@ -15,21 +16,19 @@ class AttributeAttribute(models.Model):
         string="Code",
         required=True,
     )
-
     attribute_type = fields.Selection(
-        [
-            ("char", "Char"),
-            ("text", "Text"),
-            ("select", "Select"),
-            ("multiselect", "Multiselect"),
-            ("boolean", "Boolean"),
-            ("nullable_integer", "Integer"),
-            ("date", "Date"),
-            ("datetime", "Datetime"),
-            ("binary", "Binary"),
-            ("nullable_float", "Float"),
+        selection_add=[
+            ("nullable_integer", "Nullable Integer"),
+            ("nullable_float", "Nullable Float"),
         ],
+        ondelete={
+            "nullable_integer": "set integer",
+            "nullable_float": "set float",
+        },
     )
+
+    # END ##########
+    # CONSTRAINTS ##########
 
     _sql_constraints = [
         (
@@ -38,4 +37,22 @@ class AttributeAttribute(models.Model):
             "The Attribute Code must be unique. Please choose a different code.",
         )
     ]
+
+    @api.constrains("attribute_type", "model_id")
+    def _check_attribute_type_for_products(self):
+        """Prevent product.template and product.product from using non-nullable integer/float types."""
+        for rec in self:
+            if (
+                rec.model_id
+                and rec.model_id.model in ["product.template", "product.product"]
+                and rec.attribute_type in ["integer", "float"]
+            ):
+                raise ValidationError(
+                    _(
+                        "Attributes of type 'Integer' or 'Float' are not allowed "
+                        "for product templates or variants.\n"
+                        "Please use 'Nullable Integer' or 'Nullable Float' instead."
+                    )
+                )
+
     # END ##########
