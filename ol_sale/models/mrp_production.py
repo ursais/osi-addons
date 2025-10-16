304ec2c2df2eb2ce3ecf8982d61bd1a86babb2c1
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-10-16 14:59:48 +0530

    [FIX] SO Confirm Issue.

--- ol_sale/models/mrp_production.py
+++ ol_sale/models/mrp_production.py
@@ -14,7 +14,15 @@ class MrpProduction(models.Model):
         related="sale_order_id.mrp_note",
         store=True,
     )
-    date_change_exception = fields.Boolean(string="Customer Request Date Change Proposed", default=False)
+    date_change_exception = fields.Boolean(
+        string="Customer Request Date Change Proposed", default=False
+    )
 
+    def write(self, vals):
+        res = super().write(vals)
+        for mo in self:
+            if ("mrp_batch_id" in vals or "state" in vals) and mo.sale_order_id:
+                mo.sale_order_id._compute_current_estimate_ship_date()
+        return res
 
     # END #########
