784155d4bb63557f1c1cf41044d8bd5f63dcda0e
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2025-10-31 14:19:24 +0530

    [FIX] FIX the followup.

--- ol_account/wizard/followup_manual_reminder.py
+++ ol_account/wizard/followup_manual_reminder.py
@@ -1,4 +1,5 @@
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 
 class FollowupManualReminder(models.TransientModel):
@@ -9,4 +10,25 @@ class FollowupManualReminder(models.TransientModel):
         for wizard in self:
             partner = wizard.partner_id
             template = wizard.template_id
+
             wizard.email_recipient_ids = partner._get_all_followup_contacts()
+
+    def process_followup(self):
+        options = self._get_wizard_options()
+        for wizard in self:
+            if options.get("email"):
+                followup_contacts = self.env["res.partner"].search(
+                    [
+                        ("id", "child_of", wizard.partner_id.id),
+                        ("followup", "=", True),
+                    ]
+                )
+                if not followup_contacts:
+                    raise UserError(
+                        _(
+                            """No ‘Followup’ contacts are found for %s, """
+                            """please add or set the ‘Followup’ setting on a contact and try again.""",
+                            wizard.partner_id.display_name,
+                        )
+                    )
+        return super().process_followup()
